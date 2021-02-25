3a6bcafe3304b95290514ed9ab1777573d095433
Ceiling_roof <ted10014@hotmail.com>
2021-02-25 19:36:02 +0800

    Committed 2021/2/25

--- ball_plot2.py
+++ ball_plot2.py
@@ -10,8 +10,11 @@ g = 9.81;
 
 # linspace(a, b, n) shows when n evenly spaced floating point numbers are sought on an interval [a,b]
 t = linspace(0, 1, 1001);
+# The object returned from linspace is an array of number.
 
+# When we start computing with t in the right hand side in the following expression,
 y = v0 * t - 0.5 * g * t ** 2;
+# the right hand side is computed for every number in array t, yielding the results stored as an array.
 
 plt.plot(t, y);  # plots all y coordinates vs. all t coordinates
 plt.xlabel('t (s)');  # places the text t (s) on x-axis
---
+++ volume3cube.py
@@ -0,0 +1,9 @@
+from numpy.core.function_base import linspace
+
+l = 4;
+v = l ** 3;
+
+array = linspace([1, 3, 3]);
+
+print(array);
+print(v);
