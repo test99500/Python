20193b5b645f4e110f690ff10d9c74e002000de6
Ceiling_roof <ted10014@hotmail.com>
2021-02-25 15:17:30 +0800

    Committed 2021/2/25

---
+++ ball_plot2.py
@@ -0,0 +1,17 @@
+# To see the plot on JetBrain IDEA, see https://www.jetbrains.com/help/pycharm/matplotlib-support.html#sm
+
+from numpy.core.function_base import linspace
+
+# The standard import statement, including "nickname", is as follows.
+import matplotlib.pyplot as plt;  # Refer to <Programming for Computations - Python, Second Edition> page on 19.
+
+v0 = 5;
+g = 9.81;
+t = linspace(0, 1, 1001);
+
+y = v0 * t - 0.5 * g * t ** 2;
+
+plt.plot(t, y);  # plots all y coordinates vs. all t coordinates
+plt.xlabel('t (s)');  # places the text t (s) on x-axis
+plt.ylabel('y (m)');  # places the text y (m) on y-axis
+plt.show();  # displays the figure
