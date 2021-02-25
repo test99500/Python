9aed21dd0f2b089bd1d262e894da301a2b37aefa
Ceiling_roof <ted10014@hotmail.com>
2021-02-25 14:29:56 +0800

    Committed 2021/2/25

---
+++ check_functions.py
@@ -0,0 +1,12 @@
+from math import cos
+
+# To install numpy, see https://www.jetbrains.com/help/idea/installing-uninstalling-and-upgrading-packages.html
+
+import numpy
+
+x = numpy.exp([0, 1, 2]);
+
+print(x);
+
+y = cos(0);
+print(y);
