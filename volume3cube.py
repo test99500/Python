f4d46cfab1e2141556e757e6ca0e0f45bff7da17
Ceiling_roof <ted10014@hotmail.com>
2021-02-25 20:03:44 +0800

    Committed 2021/2/25

--- volume3cube.py
+++ volume3cube.py
@@ -10,4 +10,10 @@ array = linspace(1, 3, 3);
 print(array);
 print(v);
 
-plt.plot(v, l);
+l2 = linspace(1, 3, 3);
+v2 = 1*2*3;
+
+plt.plot(v2, l2);
+plt.xlabel('Volume');
+plt.ylabel('Length');
+plt.show();
