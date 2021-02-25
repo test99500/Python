fc00fdfee007de0798afa96354072c83e9e982f1
Ceiling_roof <ted10014@hotmail.com>
2021-02-25 19:51:04 +0800

    Committed 2021/2/25

--- volume3cube.py
+++ volume3cube.py
@@ -1,5 +1,7 @@
 from numpy.core.function_base import linspace
 
+import matplotlib.pyplot as plt;
+
 l = 4;
 v = l ** 3;
 
@@ -7,3 +9,5 @@ array = linspace(1, 3, 3);
 
 print(array);
 print(v);
+
+plt.plot(v, l);
