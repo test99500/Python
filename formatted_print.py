9f9128b59fe2da5255fdee2a72216c6b4233c743
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 17:18:51 +0800

    Committed 2021/3/1

--- formatted_print.py
+++ formatted_print.py
@@ -6,3 +6,7 @@ s = "some message";   # string  (equivalent: 's = 'some message')
 
 print("real = {:.3f}, integer = {:d}, string = {:s}".format(r, i, s));
 print("real = {:9.3e}, integer = {:5d}, string = {:s}".format(r, i, s));
+
+# 9.3e means a scientific notation (e) with 3 decimals, in a field that has a width of 9 characters.
+
+# 5d means an integer in a field that has a width of 5 characters.
