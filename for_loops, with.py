1a4799392b655227ce90ffac66da8aea49f7d1ed
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 12:48:30 +0800

    Committed 2021/3/1

---
+++ for_loops, with.py
@@ -0,0 +1,5 @@
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print("{:d}*5 = {:d}".format(i, i*5));   # Note indent
+
+# The loop variable i takes on each of the values 1 to 10 appear in square brackets,
+# they constitute a special structure called a list.
---
+++ for_loops, without.py
@@ -0,0 +1,14 @@
+# Example on page 60 of the textbook
+
+# Natively printing the 5 times table.
+
+print("{:d}*5 = {:d}".format(1, 1*5));
+print("{:d}*5 = {:d}".format(2, 2*5));
+print("{:d}*5 = {:d}".format(3, 3*5));
+print("{:d}*5 = {:d}".format(4, 4*5));
+print("{:d}*5 = {:d}".format(5, 5*5));
+print("{:d}*5 = {:d}".format(6, 6*5));
+print("{:d}*5 = {:d}".format(7, 7*5));
+print("{:d}*5 = {:d}".format(8, 8*5));
+print("{:d}*5 = {:d}".format(9, 9*5));
+print("{:d}*5 = {:d}".format(10, 10*5));
