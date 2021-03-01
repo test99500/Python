b1e5b1a32d1bc8fa39173f03f486ed7553c30a14
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 19:19:33 +0800

    Committed 2021/3/1

---
+++ average_int.py
@@ -0,0 +1,9 @@
+# Exercise on page 37 of the textbook
+
+sum = 0;
+for i in range(1, 6, 1):
+    sum = sum + i;
+
+average = sum / 5;
+
+print("The average of 1+2+3+4+5 is {:.2f}".format(average));
---
+++ formatted_print2.py
@@ -0,0 +1,9 @@
+# Exercise on page 38 of the textbook.
+
+pi = 3.14159;
+x = pi;
+y = 2;
+
+product_z = x * y;
+
+print("Multiplying {} and 2 gives {:.3f}".format(pi, product_z));
