88a9d7aecd3cffe0bc4eb220f04d2c985421da32
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 19:06:58 +0800

    Committed 2021/3/1

---
+++ circumference_and_area.py
@@ -0,0 +1,9 @@
+# Exercise on page 37.
+
+radius = 2.00;
+pi = 3.14;
+circumference = 2.00 * radius * pi;
+area = pi * radius**2.00;
+
+print("The circumference and area of a circle with a radius of 2 cm "
+      "are {:.4f} cm and {} cm2*pi, respectively.".format(circumference, area));
