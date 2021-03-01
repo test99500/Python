4369909a52c6545930e39ca751677dac89fa2db1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 13:21:58 +0800

    Committed 2021/3/1

---
+++ for_loop2.py
@@ -0,0 +1,7 @@
+# Example on page 62 of the textbook
+
+for i, j, k in (1, 2, 3), (4, 5, 6), (7, 8, 9):
+    print(i, j, k);
+
+# Here, in the first iteration, the loop variables i, j and k will become 1, 2 and 3, respectively.
+# In the second iteration, they will become 4, 5 and 6, respectively, and so on.
