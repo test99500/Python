8735ad87bf53e3740c4931cfd45eefbe853ff7f4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 12:23:12 +0800

    Committed 2021/3/1

---
+++ formatted_print.py
@@ -0,0 +1,8 @@
+# Example on page 29 of the textbook
+
+r = 12.89643;         # Real number or float
+i = 42;               # integer
+s = "some message";   # string  (equivalent: 's = 'some message')
+
+print("real = {:.3f}, integer = {:d}, string = {:s}".format(r, i, s));
+print("real = {:9.3e}, integer = {:5d}, string = {:s}".format(r, i, s));
