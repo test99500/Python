7fe3d3d6ac19a75579f19f3ae589118225651be8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-01 17:59:42 +0800

    Committed 2021/3/1

---
+++ Printing Strings that Span Multiple Lines.py
@@ -0,0 +1,8 @@
+# Example on page 32 of the textbook
+
+print("This is a long string that will run over several lines "
+      "if we just manage to fill in enough words!");
+print(" ");
+print("""This is a long string that will run over several lines 
+         if we just manage to fill in 
+         enough words. """);
