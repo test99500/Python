4b3d756f0020448d466c1cd9bc00e65b13d9c6e2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-05 13:25:35 +0800

    Committed 2021/3/5

---
+++ Boolean expression _Even or Odd Number.py
@@ -0,0 +1,10 @@
+# Exercise 2.5, page on 57 of the textbook.
+
+x = int(input("Enter a number to see if it is an even number or an odd number: "));
+
+y = x % 2;
+
+if y == 0:
+    print(True);
+else:
+    print(False);
