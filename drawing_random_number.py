8980716ba448412d8ae46255a24f8174d3daa2d3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-05 19:18:53 +0800

    Committed 2021/3/5

---
+++ drawing_random_number.py
@@ -0,0 +1,10 @@
+# Exercise on page 57 of the textbook.
+
+import random
+
+from numpy import array
+
+random_array =\
+    array([random.uniform(0, 10), random.uniform(0, 10), random.uniform(0, 10),
+           random.uniform(0, 10)]);
+print(random_array);
