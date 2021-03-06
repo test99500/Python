d2cbe3aeec101826559c3803fb395142bbe7c4e2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-06 21:55:44 +0800

    Committed 2021/3/6

--- Chapter3 - Loops and Branching/while_instead_of_for.py
+++ Chapter3 - Loops and Branching/while_instead_of_for.py
@@ -1,2 +1,18 @@
 # Exercise on page 74 of the textbook.
 
+n = 5;
+h = [1.60, 1.85, 1.75, 1.80, 0.50];
+
+i = 0;
+sum = 0;
+
+# As to logical operators in python, see: https://www.w3schools.com/python/python_operators.asp
+while ( i < 5) :
+    sum += h[i];
+    i += 1;
+    # Python doesn't support ++, but you can do: number += 1
+    # Reference: https://stackoverflow.com/questions/2632677/python-integer-incrementing-with
+
+average = sum / n;
+
+print("Average is {:.2f} centimeter.".format(average));
