96aa8f7c6139cf6a1babb1c191f1383d071359d4
Ceiling_roof <ted10014@hotmail.com>
2021-03-09 09:23:04 +0800

    Committed 2021/3/9

---
+++ Chapter3 - Loops and Branching/compare_a_and_b.py
@@ -0,0 +1,11 @@
+# Exercise 3.5 on page 74 of the textbook
+
+a = int(input("Give an integer a: "));
+b = int(input("Give an integer b: "));
+
+if a < b:
+    print("\na is the smallest of the two number.");
+elif a == b:
+    print("\na and b are equal.");
+else:
+    print("\na is the largest of the two numbers.");
