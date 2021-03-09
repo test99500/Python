233e04c8cfae98932a948ccc61ea227297a8bda4
Ceiling_roof <ted10014@hotmail.com>
2021-03-09 09:00:12 +0800

    Committed 2021/3/9

--- Chapter3 - Loops and Branching/for_loop_errors.py
+++ Chapter3 - Loops and Branching/for_loop_errors.py
@@ -2,7 +2,7 @@
 
 sum = 0;
 
-for i in range(1, 10, 1):
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
     sum += i;
 
 print("Sum is {:d}.".format(sum));
---
+++ Chapter3 - Loops and Branching/range_function.py
@@ -0,0 +1,8 @@
+# Example on page 73 of the textbook
+
+sum = 0;
+
+for i in range(2, 12, 2):
+    sum += i;
+
+print("Sum is {:d}.".format(sum));
