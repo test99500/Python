164fbc98b1c68caa09edaad99ffa68e45a88a198
Ceiling_roof <ted10014@hotmail.com>
2021-03-10 15:00:45 +0800

    Committed 2021/3/10

---
+++ Chapter3 - Loops and Branching/area_rectangle_vs_circle.py
@@ -0,0 +1,16 @@
+# Exercise 3.6 on page 75 of the textbook.
+
+radius = 10.6;
+pi = 3.14;
+rectangle_side_1_of_2 = 1.3;
+rectangle_side_2_of_2 = 0;
+
+area_of_circle = radius**2 * pi;
+
+print("Area of circle is {:g}".format(area_of_circle));
+
+ratio = area_of_circle / rectangle_side_1_of_2;
+
+print("The ratio of area of the circle to one side of the rectangle is {:g}".format(ratio));
+
+
