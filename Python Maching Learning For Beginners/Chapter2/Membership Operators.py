2d2d17debbb48f283563e7d691717a2d5fa903ce
Ceiling_roof <ted10014@hotmail.com>
2021-03-12 20:24:45 +0800

    Committed 2021/3/12

---
+++ Python Maching Learning For Beginners/Chapter2/Membership Operators.py
@@ -0,0 +1,8 @@
+# Script7 on page 42.
+
+# "in" and "not in" are membership operators used to find
+# if an item is a member of a collection of items or not.
+
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+print("Sunday" in days);
+
