a308ba6a8d3794be1d8a6b9408ef067491b4772d
Ceiling_roof <ted10014@hotmail.com>
2021-03-12 13:41:58 +0800

    Committed 2021/3/12

---
+++ Python Maching Learning For Beginners/Chapter2/Data types.py
@@ -0,0 +1,31 @@
+# Script 2 on page 39.
+
+# A string Variable
+first_name = "Joseph";
+print(type(first_name));
+
+# An integer variable
+age = 20;
+print(type(age));
+
+# A floating point variable
+weight = 70.35;
+print(type(weight));
+
+# A boolean variable
+married = False;
+print(type(married));
+
+# List
+cars = ["Honda", "Toyota", "Suzuki"];
+print(type(cars));
+
+# Tuples / a structure of data that has several parts.
+# Reference: https://dictionary.cambridge.org/dictionary/english/tuple
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+print(type(days));
+
+# Dictionaries
+days2 = {1: "Sunday", 2: "Monday", 3: "Tuesday", 4: "Wednesday", 5: "Thursday",
+         6: "Friday", 7: "Saturday"};
+print(type(days2));
