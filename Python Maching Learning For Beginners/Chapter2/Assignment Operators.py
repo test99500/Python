12b93964804ae405b28e4b1562d768cc8b234ef9
Ceiling_roof <ted10014@hotmail.com>
2021-03-12 14:57:51 +0800

    Committed 2021/3/12

---
+++ Python Maching Learning For Beginners/Chapter2/Assignment Operators.py
@@ -0,0 +1,17 @@
+# Script6 on page 46.
+
+X = 20;
+Y = 10;
+
+R = X + Y;
+print(R);
+
+X += Y;
+print(X);
+
+# X now equals 30
+
+X -= Y;
+print(X);
+
+
---
+++ Python Maching Learning For Beginners/Chapter2/Assignment Operators2.py
@@ -0,0 +1,43 @@
+# Script6 on page 46.
+
+X = 20;
+Y = 10;
+
+R = X + Y;
+print(R);
+
+X = 20;
+Y = 10;
+
+X += Y;
+print(X);
+
+X = 20;
+Y = 10;
+
+X -= Y;
+print(X);
+
+X = 20;
+Y = 10;
+
+X *= Y;
+print(X);
+
+X = 20;
+Y = 10;
+
+X /= Y;
+print(X);
+
+X = 20;
+Y = 10;
+
+X %= Y;
+print(X);
+
+X = 20;
+Y = 10;
+
+X **= Y;
+print(X);
