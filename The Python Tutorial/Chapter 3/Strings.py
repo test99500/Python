cdeb1bb6e5ea069c82a4d88d930ffcae4926dd53
Ceiling_roof <ted10014@hotmail.com>
2021-03-13 16:23:19 +0800

    Committed 2021/3/13

--- The Python Tutorial/Chapter 3/Strings.py
+++ The Python Tutorial/Chapter 3/Strings.py
@@ -20,3 +20,43 @@ print(double_quote2);
 
 single_quote3 = '"Isn\'t", they said.';
 print(single_quote3);
+
+s = 'First line. \nSecond line';
+print(s);
+
+# If you don't want characters prefaced by \ to be interpreted as special characters,
+# you can use raw strings by adding an r before the first quote.
+print('C:\some\name');   # here \n means newLine!
+print("C:\some\name");   # here \n means new Line!
+
+print(r'C:\some\name');
+print(r"C:\some\name");
+
+# String literals can span multiple lines.
+# One way is using triple-quotes """...""" or '''...'''
+
+print("""
+Usage: thingy [OPTIONS]
+    -h                           Display this usage message
+    -H                           Hostname to connect to 
+""")
+
+# End of lines are automatically included in the string, but it's possible to prevent this
+# by adding a \ (back-slash) at the end of the line.
+print("""\ 
+Usage: thingy [OPTIONS]
+    -h                           Display this usage message
+    -H                           Hostname to connect to 
+""");
+
+# print(""\
+# Usage: thingy [OPTIONS]
+#    -h                           Display this usage message
+#    -H                           Hostname to connect to
+# "");
+
+my_sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 +\
+    19 + 20;
+
+print(my_sum);  # Example on page 36 of Programming for Computations - Python.
+print( (1 + 20) * 20 / 2 );
