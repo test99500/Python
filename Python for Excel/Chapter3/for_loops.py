f6ccaee120742394124e803d4a4cf038c2e7e2de
Ceiling_roof <ted10014@hotmail.com>
2021-03-13 12:45:24 +0800

    Committed 2021/3/13

---
+++ Python for Excel/Chapter3/for_loops.py
@@ -0,0 +1,10 @@
+# Example on page 76
+
+# Create a for loop that takes each element of the currencies list,
+# assigns it to the variable currency, and prints it - one after another until there are
+# no more elements in the list.
+
+currencies = ["USD", "HKD", "AUD"];
+
+for currency in currencies:
+    print(currency);
