24ff2af7797be981006934dc06c031cd68bb4c82
Ceiling_roof <ted10014@hotmail.com>
2021-03-13 15:15:30 +0800

    Committed 2021/3/13

--- The Python Tutorial/Chapter 3/Lists.py
+++ The Python Tutorial/Chapter 3/Lists.py
@@ -5,3 +5,5 @@
 
 squares = [1, 4, 9, 16, 25];
 print(squares);
+
+# Like strings
---
+++ The Python Tutorial/Chapter 3/Strings.py
@@ -0,0 +1,13 @@
+# Reference: https://docs.python.org/3/tutorial/introduction.html
+
+single_quote = 'spam eggs';
+double_quote = "spam eggs";
+
+print(single_quote);
+print(double_quote);
+
+escape = "doesn't";
+Escape = 'doesn\'t';
+
+print(escape);
+print(Escape);
