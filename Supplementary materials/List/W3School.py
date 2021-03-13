cbaca286e321468b5cddd673079b06a33ce90085
Ceiling_roof <ted10014@hotmail.com>
2021-03-13 15:04:42 +0800

    Committed 2021/3/13

---
+++ Supplementary materials/List/W3School.py
@@ -0,0 +1,9 @@
+# Reference: https://www.w3schools.com/python/python_lists.asp
+
+# Lists are created using square brackets.
+this_list = ["apple", "banana", "cherry"];
+
+print(this_list);
+
+# List elements are indexed, the first element has index [0], the second element has
+# index [1] etc.
---
+++ The Python Tutorial/Chapter 3/Lists.py
@@ -0,0 +1,7 @@
+# Reference: https://docs.python.org/3/tutorial/introduction.html
+
+# Python knows a number of compound data type, used to group together other values.
+# The most versatile is the list, which might contain items of different types.
+
+squares = [1, 4, 9, 16, 25];
+print(squares);
