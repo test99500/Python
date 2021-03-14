88275d2f6ed5242afe4bac3b0e63126eb2758455
Ceiling_roof <ted10014@hotmail.com>
2021-03-14 19:59:26 +0800

    Committed 2021/3/14

---
+++ Python for Excel/Chapter3/List.py
@@ -0,0 +1,9 @@
+file_names = ["one.xlsx", "two.xlsx", "three.xlsx"];
+numbers = [1, 2, 3];
+print(file_names + numbers);
+
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+cells = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+print(cells[1]);
