b54808fdd1daa75868f97cb0d5612f5abb7ad35e
Ceiling_roof <ted10014@hotmail.com>
2021-03-15 13:49:38 +0800

    Committed 2021/3/15

--- The Python Tutorial/Chapter 3/Strings.py
+++ The Python Tutorial/Chapter 3/Strings.py
@@ -110,3 +110,10 @@ print(word[0]);
 # Slicing
 print(word[0:2]); # characters from position 0 (included) to 2 (excluded)
 print(word[2:5]);
+
+# Slice indices have useful defaults;
+# an omitted first index defaults to zero,
+# an omitted second index defaults to the size of the string being sliced.
+word[:2] ;  # character from the beginning to position 2 (excluded)
+word[4:] ;  # characters from position 4 (included) to the end
+word[-2:];  # characters from the second-last (included) to the end
