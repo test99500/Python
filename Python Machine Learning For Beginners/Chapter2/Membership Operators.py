47906bb28da247cf542f9f87d6da43bc6fd2c16e
Ceiling_roof <ted10014@hotmail.com>
2021-03-15 16:00:35 +0800

    Committed 2021/3/15

--- Python Machine Learning For Beginners/Chapter2/Membership Operators.py
+++ Python Machine Learning For Beginners/Chapter2/Membership Operators.py
@@ -6,3 +6,7 @@
 days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 print("Sunday" in days);
 
+# Script8 on page 42
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+print("Xunday" not in days);
+
