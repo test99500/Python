5ec2d7acbd04b7100a260feb4186e481c4891820
Ceiling_roof <ted10014@hotmail.com>
2021-03-16 13:57:52 +0800

    Committed 2021/3/16

---
+++ Python for Excel/Chapter3/Dictionaries.py
@@ -0,0 +1,19 @@
+exchange_rates = {"EURUSD": 1.1152, "GBPUSD": 1.2454, "AUDUSD": 0.6161};
+print(exchange_rates["EURUSD"]);
+# print(exchange_rates{"EURUSD"});
+
+# Change an existing value.
+exchange_rates["EURUSD"] = 1.2;
+
+print(exchange_rates);
+
+# Add a new key/value pair.
+exchange_rates["CADUSD"] = 0.714;
+
+print(exchange_rates);
+
+# To merge two or more dictionaries is by using two leading asterisks,
+# which unpacks them into a new dictionary.
+print( {** exchange_rates, ** {"SGDUSD": 0.7004, "GBPUSD": 1.2222}} );
+# If the second dictionary contatins keys from the first one, the values from the first
+# will be overridden.
