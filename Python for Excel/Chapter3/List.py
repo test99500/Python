b18a55a2c356c2dc2a632d65d93df3c6aca063e1
Ceiling_roof <ted10014@hotmail.com>
2021-03-16 13:32:21 +0800

    Committed 2021/3/16

--- Python for Excel/Chapter3/List.py
+++ Python for Excel/Chapter3/List.py
@@ -14,3 +14,23 @@ users = ["Linda", "Brian"];
 users.append("Jennifer"); # Most commonly you add to the end.
 
 print(users);
+
+users.insert(0, "Kim"); # insert "Kim" at index 0
+print(users);
+
+# To delete an element, use either pop or del. While pop is a method, del is implemented
+# as a statement in Python.
+print(users.pop()); # Removes and returns the last element by default.
+print(users);
+
+del users[0]; # del removes an element at the given index.
+print(users);
+
+print(len(users)); # Length
+print("Linda" in users); # Check if users contains "Lindas".
+
+print(sorted(users)); # Returns a new sorted list.
+print(users); # The original list is unchanged.
+
+users.sort(); # Sort the original list.
+print(users);
