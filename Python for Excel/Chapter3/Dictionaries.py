0123d102d551a2089f108abd0986be83cc4e9912
Ceiling_roof <ted10014@hotmail.com>
2021-03-16 14:31:21 +0800

    Committed 2021/3/16

--- Python for Excel/Chapter3/Dictionaries.py
+++ Python for Excel/Chapter3/Dictionaries.py
@@ -17,3 +17,7 @@ print(exchange_rates);
 print( {** exchange_rates, ** {"SGDUSD": 0.7004, "GBPUSD": 1.2222}} );
 # If the second dictionary contatins keys from the first one, the values from the first
 # will be overridden.
+
+# Python 3.9 introduced the pipe character as a dedicated merge operator for dictionaries,
+# which allows you to simplify the previous expression to this:
+# print(exchange_rates | {"SGDUSD":0.70, "GBPUSD":1.20});
