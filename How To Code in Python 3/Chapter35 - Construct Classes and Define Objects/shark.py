d934c34a6b3e1dd7a9a4c4d9146ac2f0adaf88b0
Ceiling_roof <ted10014@hotmail.com>
2021-03-18 03:26:41 +0800

    Committed 2021/3/17

---
+++ How To Code in Python 3/Chapter35 - Construct Classes and Define Objects/shark.py
@@ -0,0 +1,14 @@
+# Define a class called Shark that has two functions associated with it, one for swimming
+# and one for being awesome:
+
+class Shark:
+    def swim(self):
+        print("The shark is swimming.");
+
+    def be_awesome(self):
+        print("This shark is being awesome.");
+
+def main():
+    sammy = Shark();
+    sammy.swim();
+    sammy.be_awesome();
---
+++ Python Machine Learning For Beginners/Chapter2/Objects and Classes.py
@@ -0,0 +1,14 @@
+# Script 17
+
+class Fruit:
+
+    name = "apple";
+    price = 10;
+
+    def eat_fruit(self):
+        print("Fruit has been eaten.");
+
+f = Fruit();
+f.eat_fruit();
+print(f.name);
+print(f.price);
