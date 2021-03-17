b1c86e6979879728eea8d92e079262b76990332a
Ceiling_roof <ted10014@hotmail.com>
2021-03-17 19:50:07 +0800

    Committed 2021/3/17

--- Chapter3 - Loops and Branching/compute_pi.py
+++ Chapter3 - Loops and Branching/compute_pi.py
@@ -1,2 +1,18 @@
 # Exercise 3.11 on page 76 of the textbook.
+from math import sqrt
 
+pi_of_Leibniz = 0;
+
+pi_of_Euler = 0;
+
+N = int(input("Input the number of iteration N: "));
+
+for k in range(0, N, 1):
+
+    pi_of_Leibniz = ( 1 / (4 * k + 1) * (4 * k + 3) ) * 8;
+
+for K in range(1, N, 1):
+
+    squared = 6 * (1 / K ** 2);
+
+    pi_of_Euler = sqrt(squared);
