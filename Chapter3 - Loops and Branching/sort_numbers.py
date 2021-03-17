dae4975a1bcf82b3dd61bfdd50fa3c7811a02547
Ceiling_roof <ted10014@hotmail.com>
2021-03-17 19:03:41 +0800

    Committed 2021/3/17

---
+++ Chapter3 - Loops and Branching/sort_numbers.py
@@ -0,0 +1,16 @@
+# Exercise 3.10 on page 76 of the textbook.
+
+# Use the uniform function from the random module to generate an array of 6 random numbers
+# between 0 and 10. The program should then sort the array so that numbers appear in
+# increasing order.
+# Let the program make a formatted print of the array to screen before and after sorting.
+
+import numpy as np
+
+r = np.random.uniform(0, 11, 6);
+print("Before sorting: ");
+print(r);
+
+r = np.sort(r);
+print("After sorting: ");
+print(r);
