9b061c5851d67ec0e90f964a2ab71c0982298d54
Ceiling_roof <ted10014@hotmail.com>
2021-03-17 13:36:51 +0800

    Committed 2021/3/17

---
+++ The Python Tutorial/Chapter 3/Comment.py
@@ -0,0 +1,9 @@
+# Reference: https://docs.python.org/3/tutorial/introduction.html
+
+# This is the first comment.
+spam = 1; # and this is the second comment.
+          # and now a third!
+
+text = "# This is not a comment because it's inside quotes.";
+
+print(text);
