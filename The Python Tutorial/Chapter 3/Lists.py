7d2ec27c3f4fc8e29d544be54c3df0d9d19f9c34
Ceiling_roof <ted10014@hotmail.com>
2021-03-17 16:45:57 +0800

    Committed 2021/3/17

--- The Python Tutorial/Chapter 3/Lists.py
+++ The Python Tutorial/Chapter 3/Lists.py
@@ -1,9 +1,13 @@
 # Reference: https://docs.python.org/3/tutorial/introduction.html
 
-# Python knows a number of compound data type, used to group together other values.
-# The most versatile is the list, which might contain items of different types.
+# Python knows a number of "compound" data type, used to group together other values.
+# The most versatile is the list, which might contain items of different types,
+# but usually the items all have the same type.
 
 squares = [1, 4, 9, 16, 25];
 print(squares);
 
-# Like strings
+# Like strings (and all other built-in sequence types), lists can be indexed and sliced.
+print(squares[0]);
+print(squares[-1]);
+print(squares[-3:]); # Slicing returns a new list.
