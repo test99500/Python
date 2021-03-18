9cddaf7fe34774ddd9f90187cedd5e0cbaca0a5c
Ceiling_roof <ted10014@hotmail.com>
2021-03-19 01:23:40 +0800

    Committed 2021/3/18

---
+++ How To Code in Python 3/Chapter35 - Construct Classes and Define Objects/Shark8.py
@@ -0,0 +1,9 @@
+class Shark:
+    animal_type = "fish";
+    location = "ocean";
+    followers = 5;
+
+new_shark = Shark();
+print(new_shark.animal_type);
+print(new_shark.location);
+print(new_shark.followers);
