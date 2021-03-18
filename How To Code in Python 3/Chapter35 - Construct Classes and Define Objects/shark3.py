88e094fc9d44aafba63b1620c5fe6dba427fed68
Ceiling_roof <ted10014@hotmail.com>
2021-03-18 09:50:40 +0800

    Committed 2021/3/18

---
+++ How To Code in Python 3/Chapter35 - Construct Classes and Define Objects/shark3.py
@@ -0,0 +1,18 @@
+# Define a class called Shark that has two functions associated with it, one for swimming
+# and one for being awesome:
+
+class Shark:
+
+    def __init__(self, name):
+        print("This is the constructor method.");
+        self.name = name;
+
+    def swim(self):
+        print("The shark is swimming.");
+
+    def be_awesome(self):
+        print("This shark is being awesome.");
+
+sammy = Shark();
+sammy.swim();
+sammy.be_awesome();
