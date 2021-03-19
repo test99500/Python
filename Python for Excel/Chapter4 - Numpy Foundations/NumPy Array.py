6aba44d31b4b1542abe83d3c097983e55f83d6a3
Ceiling_roof <ted10014@hotmail.com>
2021-03-19 17:39:36 +0800

    Committed 2021/3/19

---
+++ Python for Excel/Chapter4 - Numpy Foundations/NumPy Array.py
@@ -0,0 +1,13 @@
+# First, let's import NumPy
+import numpy as np
+
+# Constructing an array with a simple list results in a 1d array
+array1 = np.array([10, 100, 1000.]);
+
+# Constructing an array with a nested list results in a 2d array
+array2 = np.array([[1., 2., 3.],
+                   [4., 5., 6.]]);
+
+print(array1.dtype);
+
+print(array2 + 1);
