71a93ac0f3f8e3e12eb8d5bdfb294dcd933f1671
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 01:13:44 +0800

    Committed 2021/3/23

---
+++ Hands on Machine Learning2/Tutorial for pandas/Series.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# The pandas library contains these useful data structures:
+#
+# Series objects, that we will discuss now. A Series object is 1D array, similar to a column in a spreadsheet (with a
+# column name and row labels).
+# DataFrame objects. This is a 2D table, similar to a spreadsheet (with column names and
+# row labels).
+
+# Creating a Series
+series = pd.Series([2, -1, 3, 5]);
+print(series);
+
+# Reference:
+# https://github.com/ageron/handson-ml2/blob/master/tools_pandas.ipynb
