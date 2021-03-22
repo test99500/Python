4a67acf97b364ecd3e64a07adabb2b878b3d0b49
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 00:50:30 +0800

    Committed 2021/3/23

---
+++ Pandas User Guide/10 minutes to pandas/Object creation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8]);
+
+print(s);
+
+# Reference:
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/10min.html#min
