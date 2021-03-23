3d0d1b557c50b3c0ed9a15faf9e9fa13fbdc8b61
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 09:54:18 +0800

    Committed 2021/3/23

---
+++ Python Machine Learning For Beginners/Chapter3/arange().py
@@ -0,0 +1,7 @@
+# Script 3
+
+import numpy as np;
+
+# With the arrange() method, you can create a NumPy array that contains a range of integers.
+nums_a_range = np.arange(5, 11);
+print(nums_a_range);
