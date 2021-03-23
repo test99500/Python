f989fac73a3f1ca1ecf399af103c1b551a5ed710
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 11:38:11 +0800

    Committed 2021/3/23

--- Python Machine Learning For Beginners/Chapter3/3/4 Array Indexing And Slicing/Script14.py
+++ Python Machine Learning For Beginners/Chapter3/3/4 Array Indexing And Slicing/Script14.py
@@ -6,3 +6,9 @@ print(series);
 print(series[1]);
 
 print(series[1:9]);
+
+# Slice indices have useful defaults;
+# an omitted first index defaults to zero,
+# an omitted second index defaults to the size of the string being sliced.
+print(series[:5]);
+print(series[5:]);
