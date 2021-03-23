f8c9284f27b40cd284acc65f393f613d42fadc5b
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 08:59:13 +0800

    Committed 2021/3/23

---
+++ Python Machine Learning For Beginners/Chapter3/Creating NumPy Arrays.py
@@ -0,0 +1,11 @@
+# Script1
+
+import numpy as np
+
+nums_list = [10, 12, 14, 16, 20];
+nums_array = np.array(nums_list);
+
+print(nums_list);
+print(type(nums_list));
+print(nums_array);
+print(type(nums_array));
