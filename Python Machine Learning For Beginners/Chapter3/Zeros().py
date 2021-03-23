ff4a7dbc590b9d01c37dab84fe45811fbf1b6f09
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 11:07:37 +0800

    Committed 2021/3/23

---
+++ Python Machine Learning For Beginners/Chapter3/Zeros().py
@@ -0,0 +1,6 @@
+# Script 7
+
+import numpy as np
+
+zeros_array = np.zeros(6);
+print(zeros_array);
---
+++ Python Machine Learning For Beginners/Chapter3/Zeros()2.py
@@ -0,0 +1,6 @@
+# Script 8
+
+import numpy as np
+
+zeros_array = np.zeros((6, 4));
+print(zeros_array);
