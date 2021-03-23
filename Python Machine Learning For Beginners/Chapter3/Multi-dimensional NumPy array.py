f0be934c8e2f154cd9e9ddbd5b79a3a5ccf953c3
Ceiling_roof <ted10014@hotmail.com>
2021-03-23 09:26:29 +0800

    Committed 2021/3/23

---
+++ Python Machine Learning For Beginners/Chapter3/Multi-dimensional NumPy array.py
@@ -0,0 +1,13 @@
+# Script 2
+
+import numpy as np;
+
+row1 = [10, 12, 13];
+row2 = [45, 32, 16];
+row3 = [45, 32, 16];
+
+nums_2d = np.array([row1, row3, row3]);
+
+print(nums_2d);
+print(nums_2d.shape);
+
---
+++ Python Machine Learning For Beginners/Chapter3/Multi-dimensional NumPy array2.py
@@ -0,0 +1,9 @@
+# Create a list of list
+# Convert list to NumPy array
+
+import  numpy as np;
+
+list_of_list = [[10, 12, 13], [45, 32, 16], [45, 32, 16]];
+array = np.array(list_of_list);
+
+print(array);
