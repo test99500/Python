6dc0560ea72071c5dccabd070b1121f5ab654f2b
Ceiling_roof <ted10014@hotmail.com>
2021-03-25 13:08:50 +0800

    Committed 2021/3/25

---
+++ Python Machine Learning For Beginners/Chapter3/3/6 NumPy for Linear Algebra Operations/multiply().py
@@ -0,0 +1,13 @@
+# Script 26
+
+import numpy as np
+
+row1 = [10, 12, 13];
+row2 = [45, 32, 16];
+row3 = [45, 32, 16];
+
+numpy_2d = np.array([row1, row2, row3]);
+
+multiply = np.multiply(numpy_2d, numpy_2d);
+
+print(multiply);
