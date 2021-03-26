c077ad7c5267ab82c9b25a56b24882b504b9a65d
Ceiling_roof <ted10014@hotmail.com>
2021-03-26 19:22:58 +0800

    Committed 2021/3/26

--- NumPy User Guide/Indexing, Slicing, and Iterating/Multi-dimensional.py
+++ NumPy User Guide/Indexing, Slicing, and Iterating/Multi-dimensional.py
@@ -10,3 +10,16 @@ b = np.fromfunction(f, (5, 4), dtype=int);
 print(b);
 
 print(b[2, 3]);
+
+print(b[0 : 5, 1]);  # each row in the second column of b.
+
+print(b[ : , 1]);    # equivalent to the previous example.
+
+print(b[1 : 3, : ]); # each column in the second and third row of b.
+
+print(b[-1, :]);
+
+print(b[-1])  # the last row. Equivalent to b[-1, : ].
+
+# Reference:
+# https://numpy.org/devdocs/user/quickstart.html#indexing-slicing-and-iterating
