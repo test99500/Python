bf11c43f31a1e220846f8ef3e9810f09ac195475
Ceiling_roof <ted10014@hotmail.com>
2021-03-26 19:08:54 +0800

    Committed 2021/3/26

--- NumPy User Guide/Indexing, Slicing, and Iterating/One-dimensional.py
+++ NumPy User Guide/Indexing, Slicing, and Iterating/One-dimensional.py
@@ -1,4 +1,25 @@
+# One-dimensional arrays can be indexed, sliced and iterated over,
+# much like lists and other Python sequences.
+
 import numpy as np
 
 a = np.arange(10)**3;
 print(a);
+
+print(a[2]);
+
+print(a[2:5]);
+
+# equivalent to a[0:6:2] = 1000;
+# from start to position 6, exclusive, set every 2nd element to 1000.
+a[ : 6 : 2] = 1000;
+print(a);
+
+b = a[ : : -1]; # reversed a
+print(b, a);
+
+for i in a:
+    print(i ** (1 / 3.));
+
+# Reference:
+# https://numpy.org/devdocs/user/quickstart.html#indexing-slicing-and-iterating
