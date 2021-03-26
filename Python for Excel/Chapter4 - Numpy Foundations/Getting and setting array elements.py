c3c099dc5559c2071cb8c06bea5e94dab3568709
Ceiling_roof <ted10014@hotmail.com>
2021-03-26 13:48:13 +0800

    Committed 2021/3/26

---
+++ Python for Excel/Chapter4 - Numpy Foundations/Getting and setting array elements.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+array1 = np.array([10, 100, 1000]);
+
+row1 = [1, 2, 3];
+row2 = [4, 5, 6];
+array2 = np.array([row1, row2]);
+
+print(array1[2]); # Returns a scalar.
+print(array2[0][0]); # Returns a scalar
+print(array2[0, 0]); # Returns a scalar
+
+print(array2[1][2]);
+print(array2[1, 2]);
+
+print(array2[ : ][1 : ]);
+print(array2[ : , 1 : ]); # Returns a 2d array
