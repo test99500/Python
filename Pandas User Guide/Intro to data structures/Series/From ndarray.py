e87b266029f0b94a0ea1dc8b0698a4e1de2fa806
Ceiling_roof <ted10014@hotmail.com>
2021-03-28 15:43:19 +0800

    Committed 2021/3/28

--- Pandas User Guide/Intro to data structures/Series/From ndarray.py
+++ Pandas User Guide/Intro to data structures/Series/From ndarray.py
@@ -4,7 +4,13 @@ import pandas as pd
 s = pd.Series(data=np.random.randn(5), index=['a', 'b', 'c', 'd', 'e'],
               name="List of random number");
 
-print(s);
+print(s, "\n");
+
+print(s.index, "\n");
+
+#  If no index is passed, one will be created having values [0, ..., len(data) - 1].
+s2 = pd.Series(data=np.random.randn(10), name="A list of 10 random numbers");
+print(s2);
 
 # Reference:
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/dsintro.html#series
