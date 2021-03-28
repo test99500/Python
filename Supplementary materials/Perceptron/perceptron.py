a91a51df49bc3b06e896060e12594eb8b3b9d6dc
Ceiling_roof <ted10014@hotmail.com>
2021-03-28 23:45:56 +0800

    Committed 2021/3/28

--- Supplementary materials/Perceptron/perceptron.py
+++ Supplementary materials/Perceptron/perceptron.py
@@ -7,11 +7,32 @@ class Perceptron:
         self.lr = learning_rate;
         self.n_iters = number_of_iteration;
         self.activation_func = self._unit_step_func;
-        self.weight = None;
+        self.weights = None;
         self.bias = None;
 
-    def fit(self, x, y):
-        pass;
+    def fit(self, X, y):
+        n_samples, n_features = X.shape;
+
+        # init weights
+        self.weights = np.zeros(n_features);
+        self.bias = 0;
+
+        y_ = np.array([1 if i > 0 else 0 for i in y]);
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(x):
+                linear_output = np.dot(x_i, self.weights) + self.bias;
+                y_predicted = self.activation_func(linear_output);
+
+                update = self.lr * (y_[idx] - y_predicted);
+                self.weights += update * x_i;
+                self.bias += update;
+
+    def predict(self, X):
+        linear_output = np.dot(x, self.weights) + self.bias;
+        y_predicted = self.activation_func(linear_output);
+        return y_predicted;
+
 
     def _unit_step_function(self, x):
         if x >= 0:
---
+++ Supplementary materials/Perceptron/perceptron_tests.py
@@ -0,0 +1,6 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+from perceptron import
