99462f3241d5162e3d0e160030b026e93165e2a2
Ceiling_roof <ted10014@hotmail.com>
2021-03-29 00:57:14 +0800

    Committed 2021/3/28

--- Supplementary materials/Perceptron/perceptron.py
+++ Supplementary materials/Perceptron/perceptron.py
@@ -1,3 +1,5 @@
+# Reference: https://www.youtube.com/watch?v=t2ym2a3pb_Y
+
 import numpy as np
 
 
@@ -20,7 +22,7 @@ class Perceptron:
         y_ = np.array([1 if i > 0 else 0 for i in y]);
 
         for _ in range(self.n_iters):
-            for idx, x_i in enumerate(x):
+            for idx, x_i in enumerate(X):
                 linear_output = np.dot(x_i, self.weights) + self.bias;
                 y_predicted = self.activation_func(linear_output);
 
@@ -29,7 +31,7 @@ class Perceptron:
                 self.bias += update;
 
     def predict(self, X):
-        linear_output = np.dot(x, self.weights) + self.bias;
+        linear_output = np.dot(X, self.weights) + self.bias;
         y_predicted = self.activation_func(linear_output);
         return y_predicted;
 
