003302ca2b179271bd4fdbaf9f77a119bd72ecc3
Ceiling_roof <ted10014@hotmail.com>
2021-03-29 20:01:53 +0800

    Committed 2021/3/29

--- Python Machine Learning/Chapter2/Iris dataset.py
+++ Python Machine Learning/Chapter2/Iris dataset.py
@@ -9,8 +9,8 @@ print("URL:", s);
 df = pd.read_csv(filepath_or_buffer=s, header=None, encoding="utf-8");
 print(df.tail(5));
 
-y = df.iloc[0:100, 4].values;
-y = np.where(y == "Iris-setosa", -1, 1);
+y = df.iloc[0:100, 4].values; # Extract the first 100 class labels that correspond to the 50 Iris-versicolor flower.
+y = np.where(y == "Iris-setosa", -1, 1); # Convert the class labels into the two integer class labels, 1 (versicolor) and -1 (setosa), that we assign to a vector y, where the values method of a pandas yields the corresponding NumPy representation.
 
 # References:
 # 1. https://github.com/rasbt/python-machine-learning-book-3rd-edition/commit/6aa856219c1b85092df28c2a88d2bf85ebbfdfa8
