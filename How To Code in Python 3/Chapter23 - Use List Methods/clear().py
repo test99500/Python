94bd090cb8ee16efcbd65571e68f46a58eedfac1
Ceiling_roof <ted10014@hotmail.com>
2021-03-29 18:06:50 +0800

    Committed 2021/3/29

---
+++ How To Code in Python 3/Chapter23 - Use List Methods/clear().py
@@ -0,0 +1,9 @@
+fish = ["barracuda", "cod", "devil ray", "eel"];
+fish2 = fish.clear();
+print(fish2);
+print(fish);
+
+fish3 = ["barracuda", "cod", "devil ray",  "eel"];
+print(fish3);
+fish3.clear();  # clear(self);
+print(fish3);
