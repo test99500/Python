a2d926e8d18f8ff251db94e53ea8b9d6b420127d
Ceiling_roof <ted10014@hotmail.com>
2021-03-29 19:51:19 +0800

    Committed 2021/3/29

--- Python Machine Learning/Chapter2/Iris dataset.py
+++ Python Machine Learning/Chapter2/Iris dataset.py
@@ -1,5 +1,16 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-s = os.path.join("https://archive.ics.uci.edu", "ml", "machine-learning-databases", "iris", "iris.data");
+s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'; # [1]
 print("URL:", s);
+
+df = pd.read_csv(filepath_or_buffer=s, header=None, encoding="utf-8");
+print(df.tail(5));
+
+y = df.iloc[0:100, 4].values;
+y = np.where(y == "Iris-setosa", -1, 1);
+
+# References:
+# 1. https://github.com/rasbt/python-machine-learning-book-3rd-edition/commit/6aa856219c1b85092df28c2a88d2bf85ebbfdfa8
