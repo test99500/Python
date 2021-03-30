b122371bbc7aafc2c96518384b31dc4fc78dbe6b
Ceiling_roof <ted10014@hotmail.com>
2021-03-30 20:34:26 +0800

    Committed 2021/3/30

--- Python for Data Analysis/NumPy/where().py
+++ Python for Data Analysis/NumPy/where().py
@@ -2,4 +2,13 @@ import numpy as np
 
 # Supposing you have a matrix of randomly generated data and you want to replace all
 # positive values with 2 and all negative values with -2.
-arr = np.random.randn(4, 4);
+
+arr = np.random.default_rng(5);
+
+matrix = arr.integers(low=-10, high=10, size=(4, 4), dtype=int, endpoint=True);
+print(matrix);
+print(matrix > 0);
+
+amended_matrix = np.where(matrix > 0, 2, -2);
+print(amended_matrix);
+print(matrix);
