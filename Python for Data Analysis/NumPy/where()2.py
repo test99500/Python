8abba81588bf5cd91d2087f488162b71af40fca3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-03-31 14:13:03 +0800

    Committed 2021/3/31

--- Python for Data Analysis/NumPy/where()2.py
+++ Python for Data Analysis/NumPy/where()2.py
@@ -6,6 +6,9 @@ print(type(rng));
 matrix = rng.random(size=(4, 4)); # [1][2]
 print(matrix);
 
+amended_matrix = np.where(matrix > 0, 2, None);
+print(amended_matrix);
+
 # References:
 # 1. https://numpy.org/doc/stable/reference/random/generator.html#simple-random-data
 # 2. https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.random.html#numpy.random.Generator.random
