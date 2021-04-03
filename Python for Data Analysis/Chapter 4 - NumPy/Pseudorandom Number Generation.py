a55b06c866a2bfef602b12a20a65740d1f12d563
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-03 19:41:48 +0800

    Committed 2021/4/03

---
+++ Python for Data Analysis/Chapter 4 - NumPy/Pseudorandom Number Generation.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+np.random.seed(1234);
+samples = np.random.normal(size=(4, 4));
+print(samples);
---
+++ Python for Data Analysis/Chapter 4 - NumPy/Pseudorandom Number Generation2.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+rng = np.random.RandomState(1234);
+print(rng.randn(10));
