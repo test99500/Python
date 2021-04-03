7b1f0484fa9606766af7f82d723970e988180327
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-03 13:45:49 +0800

    Committed 2021/4/03

---
+++ Pandas User Guide/Indexing and selecting data/Different choices for indexing5.py
@@ -0,0 +1,14 @@
+# The Python and NumPy indexing operators [] and attribute operator .
+# provide quick and easy access to pandas data structures across a wide range of use cases.
+
+# This makes interactive work intuitive, as there’s little new to learn if you already know
+# how to deal with Python dictionaries and NumPy arrays.
+
+# However, since the type of the data to be accessed isn’t known in advance,
+# directly using standard operators has some optimization limits.
+
+# For production code, we recommended that you take advantage of
+# the optimized pandas data access methods exposed in this chapter.
+
+# Source:
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#indexing-and-selecting-data
