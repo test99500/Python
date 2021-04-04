48ffb020eb8a16c6c363f831d1c85823d0a93bb4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-04 14:16:54 +0800

    Committed 2021/4/04

--- Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/From dictionary.py
+++ Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/From dictionary.py
@@ -6,7 +6,7 @@ data = {"state": ["Ohio", "Ohio", "Ohio", "Nevada", "Nevada", "Nevada"],
         "pop": [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]};
 
 # If axis labels are not passed, they will be constructed from the input data based on
-# common sense rules. [1]
+# common sense rules. [1][3]
 frame = pd.DataFrame(data);
 print(frame);
 
@@ -21,3 +21,4 @@ print(frame3);
 # Reference:
 # 1. https://pandas.pydata.org/pandas-docs/stable/user_guide/dsintro.html#dataframe
 # 2. The input[47] of Textbook, page on 175.
+# 3. Pandas User Guide/Intro to data structures/DataFrame/From dict of Series or dicts2.py
