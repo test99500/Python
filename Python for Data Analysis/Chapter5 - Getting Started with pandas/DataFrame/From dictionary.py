7145c7af9a52b40ec571a7899121486f134fe171
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-04 13:46:42 +0800

    Committed 2021/4/04

--- Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/From dictionary.py
+++ Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/From dictionary.py
@@ -5,6 +5,16 @@ data = {"state": ["Ohio", "Ohio", "Ohio", "Nevada", "Nevada", "Nevada"],
         "year": [2000, 2001, 2002, 2001, 2002, 2003],
         "pop": [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]};
 
+# If axis labels are not passed, they will be constructed from the input data based on
+# common sense rules. [1]
 frame = pd.DataFrame(data);
-
 print(frame);
+
+# If you specify a sequence of columns, the DataFrame's columns will be arranged
+# in that order: [2]
+frame2 = pd.DataFrame(data=data, columns=["year", "state", "pop"]);
+print(frame2);
+
+# Reference:
+# 1. https://pandas.pydata.org/pandas-docs/stable/user_guide/dsintro.html#dataframe
+# 2. The input[47] of Textbook, page on 175.
---
+++ Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/From dictionary2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+data = {"state": "Ohio",
+        "year": 2000,
+        "pop": 1.5};
+
+frame = pd.DataFrame(data);
+
+print(frame);
