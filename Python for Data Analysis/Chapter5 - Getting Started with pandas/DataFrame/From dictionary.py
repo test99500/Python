987b936c5693aa81705e52b0c909378c0085e2bf
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-04 13:28:13 +0800

    Committed 2021/4/04

---
+++ Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/From dictionary.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+data = {"state": ["Ohio", "Ohio", "Ohio", "Nevada", "Nevada", "Nevada"],
+        "year": [2000, 2001, 2002, 2001, 2002, 2003],
+        "pop": [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]};
+
+frame = pd.DataFrame(data);
+
+print(frame);
