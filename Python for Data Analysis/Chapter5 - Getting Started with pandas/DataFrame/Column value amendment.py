f60a9866df917ac1b7ac0801c8dbe70a30a2c612
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-05 15:31:53 +0800

    Committed 2021/4/04

--- Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/Column value amendment.py
+++ Python for Data Analysis/Chapter5 - Getting Started with pandas/DataFrame/Column value amendment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 data = {"state": ["Ohio", "Ohio", "Ohio", "Nevada", "Nevada", "Nevada"],
         "year": [2000, 2001, 2002, 2001, 2002, 2003],
@@ -15,3 +16,25 @@ print(frame["year"]);
 frame["debt"] = 16.5;
 
 print(frame);
+
+frame["debt"] = np.arange(6.);
+print(frame);
+
+# Numpy.arange([start, ]stop, [step, ], dtype=None, *, like=None)
+#     Parameters
+#           start : integer or real, optional
+#               Start of interval.
+#               The interval includes this value. The default start value is 0.
+#
+#           stop : integer or real
+#                End of interval. The interval does not include this value,
+#                except in some cases where step is not an integer and floating point
+#                round-off affects the length of out.
+#
+#           step : integer or real, optional
+#                Spacing between values. For any output out,
+#                this is the distance between two adjacent values, out[i+1] - out[i].
+#                The default step size is 1.
+#                If step is specified as a position argument, start must also be given.
+# Reference:
+# https://numpy.org/doc/stable/reference/generated/numpy.arange.html
