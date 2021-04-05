b33fcbb1d96beb6585e1382d97a5cd1419881e3e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-05 18:12:48 +0800

    Committed 2021/4/5

---
+++ NumPy User Guide/Shape and size of an array/array's transpose() method.py
@@ -0,0 +1,12 @@
+# ndarray.transpose(*axes)
+
+#   Returns a view of the array with axes transposed.
+#
+#   For a 1-D array this has no effect, as a transposed vector is simply the same vector.
+#
+#   Parameters:
+#       axes : None, tuple of ints, or n ints
+#           None or no argument: reverses the order of the axes.
+#
+# Reference:
+# https://numpy.org/doc/stable/reference/generated/numpy.ndarray.transpose.html
