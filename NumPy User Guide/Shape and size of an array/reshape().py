db854c3da20a611e79f9024e4f46899b02bfc219
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-05 16:47:34 +0800

    Committed 2021/4/5

--- NumPy User Guide/Shape and size of an array/reshape().py
+++ NumPy User Guide/Shape and size of an array/reshape().py
@@ -1,10 +1,10 @@
-# numpy.reshape(a, newshape, order='C')
+# numpy.reshape(a, new_shape, order='C')
 #       Gives a new shape to an array without changing its data.
 #           Parameters:
 #               a : array_like
 #                   Array to be reshaped.
 #
-#               newshape : int or tuple of ints
+#               new_shape : int or tuple of ints
 #                   The new shape should be compatible with the original shape.
 #                   If an integer, then the result will be a 1-D array of that length.
 #                   One shape dimension can be -1.
