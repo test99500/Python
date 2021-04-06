de5ece6f1eeb289398f3f9c2518d10c791d9ea98
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-06 14:56:38 +0800

    Committed 2021/4/6

---
+++ Pandas User Guide/Indexing and selecting data/Attribute access5.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+dates = pd.date_range(start="2000-01-01", periods=8);
+df = pd.DataFrame(data=np.random.randn(8, 4), index=dates, columns=['A', 'B', 'C', 'D']);
+print(df);
+
+dfa = df.copy();
+print(dfa);
+
+print(dfa.A);
+
+dfa.A = np.arange(len(dfa.index));
+
+print(dfa);
