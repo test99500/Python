9731c863dcae06778e8249b41a73d7caa00d6ac1
Ceiling_roof <ted10014@hotmail.com>
2021-04-07 00:39:55 +0800

    Committed 2021/4/6

---
+++ Python for Data Analysis/Chapter5 - Getting Started with pandas/fill NAN.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+df1 = pd.DataFrame(data=np.arange(12.).reshape((3, 4)), columns=list("abcd"));
+print(df1);
+
+df2 = pd.DataFrame(data=np.arange(20.).reshape((4, 5)), columns=list("abcde"));
+print(df2);
