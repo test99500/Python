59e1d8f1b91d2e5076e6520b38dafc9401aa6df6
Ceiling_roof <ted10014@hotmail.com>
2021-04-08 22:51:10 +0800

    Committed 2021/4/8

--- Python Machine Learning/Chapter2/Iris dataset2.py
+++ Python Machine Learning/Chapter2/Iris dataset2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 url = \
     "https://raw.githubusercontent.com/rasbt/python-machine-learning-book-3rd-edition/master/ch02/iris.data";
@@ -24,6 +25,19 @@ y3 = np.where(y == "Iris-setosa", -1, 1);
 # the third feature column (petal length) of those 100 training examples and assign them
 # to a feature matrix, X.
 X = df.iloc[0:100, [0, 2]].to_numpy();
+print(X);
+
+# plot data
+plt.scatter(x=X[:50, 0], y=X[:50, 1], c="red", marker='o', label="setosa");
+plt.scatter(x=X[50:100, 0], y=X[50:100, 1], c="blue", marker='x', label="versicolor");
+
+plt.xlabel("sepal length [cm]");
+plt.ylabel("petal length [cm]");
+plt.legend(loc="upper left");
+
+plt.savefig('iris_dataset.jpg');
+
+plt.show();
 
 # References:
 # 1. https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#column-and-index-locations-and-names
---
+++ Python Machine Learning/Chapter2/Train.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import Perceptron as Perceptron
+import
+
+ppn = Perceptron.Perceptron(eta=0.1, n_iter=10);
+ppn.fit(X=)
