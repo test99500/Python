83abf9d9716b4942c1f7b41155faef673de22aec
Ceiling_roof <ted10014@hotmail.com>
2021-04-08 12:14:39 +0800

    Committed 2021/4/8

--- Python Machine Learning/Chapter2/Iris dataset2.py
+++ Python Machine Learning/Chapter2/Iris dataset2.py
@@ -4,7 +4,13 @@ import pandas as pd
 url = \
     "https://raw.githubusercontent.com/rasbt/python-machine-learning-book-3rd-edition/master/ch02/iris.data";
 
+#  if no names are passed the behavior is identical to header=0 and
+#  column names are inferred from the first line of the file.[1]
 df = pd.read_csv(filepath_or_buffer=url, header=None);
 print(df);
 
 # select setosa and versicolor
+
+
+# References:
+# 1. https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#column-and-index-locations-and-names
