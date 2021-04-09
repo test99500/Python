fa45b3299c38210f056e48065083812952fd3e5c
Ceiling_roof <ted10014@hotmail.com>
2021-04-10 02:15:16 +0800

    Committed 2021/4/9

--- Python Machine Learning For Beginners/Chapter6 - Supervised Learning/Regression algorithm.py
+++ Python Machine Learning For Beginners/Chapter6 - Supervised Learning/Regression algorithm.py
@@ -1,8 +1,58 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 print(sns.get_dataset_names());
 
 tips_df = sns.load_dataset("tips");
 print(tips_df.head());
+
+# Copy obj where its index is dropped/removed.
+X = tips_df.drop(axis=1, labels="tip");
+
+# feature set
+print(X);
+
+y = tips_df["tip"];
+
+# record set
+print(y);
+
+print(type(X));
+numerical = X.drop(axis=1, labels=["sex", "smoker", "day", "time"]);
+print(numerical);
+
+# Create a dataframe that contains only categorical columns.
+categorical = X.filter(axis=1, items=["sex", "smoker", "day", "time"]);
+print(categorical);
+
+categorical_numerical = pd.get_dummies(data=categorical, drop_first=True);
+print(categorical_numerical);
+
+X1 = pd.concat([numerical, categorical_numerical], axis=1);
+print(X1);
+
+X_train, X_test, y_train, y_test = train_test_split(X1, y, test_size=0.20, random_state=0);
+
+sc = StandardScaler();
+
+# scaling the training set
+X_train = sc.fit_transform(X=X_train);
+
+# scaling the test set
+X_test = sc.transform(X=X_test);
+
+# training the algorithm
+lin_reg = LinearRegression();
+regressor = lin_reg.fit(X=X_train, y=y_train);
+
+# making predictions on test set
+y_prediction = regressor.predict(X=X_test);
+
+print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_prediction));
+print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_prediction));
+print("Root Mean Squared Error:", np.sqrt(metrics.mean_squared_error(y_test, y_prediction)));
