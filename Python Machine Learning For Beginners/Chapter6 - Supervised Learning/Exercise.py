0deb03cf04be97e75d96f58d1f287cd29afd9dd0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-11 02:19:54 +0800

    Committed 2021/4/10

--- Python Machine Learning For Beginners/Chapter6 - Supervised Learning/Exercise.py
+++ Python Machine Learning For Beginners/Chapter6 - Supervised Learning/Exercise.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+
+print(sns.get_dataset_names());
+
+diamond_df = sns.load_dataset("diamonds");
+print(diamond_df);
+
+# Train a regression algorithm of your choice, which predicts the price of the diamond.
+# Perform all the preprocessing steps.
+
+# Dividing Data into Features and Labels
