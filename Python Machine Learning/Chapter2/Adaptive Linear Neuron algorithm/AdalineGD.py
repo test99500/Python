94dee51245d65cfda3df78ba50c04c659f097d2a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-11 17:12:26 +0800

    Committed 2021/4/11

--- Python Machine Learning/Chapter2/Adaptive Linear Neuron algorithm/AdalineGD.py
+++ Python Machine Learning/Chapter2/Adaptive Linear Neuron algorithm/AdalineGD.py
@@ -1,4 +1,6 @@
-class AdalineGD:
+import numpy as np
+
+class AdalineGD(object):
     """ADAptive LInear NEuron classifier
 
     :parameter
@@ -43,3 +45,28 @@ class AdalineGD:
          :return
             self: object
         """
+
+        rgen = np.random.RandomState(self.randome_state);
+
+        self.w = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1]);
+
+        self.cost_ = [];
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X);
+            output = self.activation(net_input);
+            errors = (y - output);
+            self.w_[1:] += self.eta * X.T.dot(errors);
+            self.w_[0] += self.eta * errors.sum();
+            cost = (errors ** 2).sum() / 2.0;
+            self.cost_.append(cost);
+            return self;
+
+    def net_input(self, X):
+        """Calculate net input"""
+
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X=self.net_input(X=X)) >= 0.0, 1, -1);
