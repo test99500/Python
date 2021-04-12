5815c7646ea9d89e925511f9fb86db416bb1a869
Ceiling_roof <ted10014@hotmail.com>
2021-04-12 08:57:16 +0800

    Committed 2021/4/11

--- Python Machine Learning/Chapter3/Perceptron with scikit.py
+++ Python Machine Learning/Chapter3/Perceptron with scikit.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 iris = datasets.load_iris();
 print(iris);
@@ -15,4 +16,17 @@ y = iris.target;
 
 print("Class labels: ", np.unique(y));
 
-# Split the dataset into seperate training and test dataset
+# Split the dataset into separate training and test dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1,
+                                                    stratify=y);
+
+print("Labels counts in y:", np.bincount(y));
+print("Labels counts in y_train:", np.bincount(y_train));
+print("Labels counts in y_test:", np.bincount(y_test));
+
+# feature scaling
+sc = StandardScaler();
+sc.fit(X=X_train);
+X_train_std = sc.transform(X=X_train);
+
+X_test_std = sc.transform(X=X_test);
