e9f203d2989ebe2a1444a66f83668feb04ce4ad7
Ceiling_roof <ted10014@hotmail.com>
2021-04-14 02:25:52 +0800

    Committed 2021/4/13

--- Python Machine Learning/Chapter3/Logistic_regression_model_with_scikit.py
+++ Python Machine Learning/Chapter3/Logistic_regression_model_with_scikit.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+import plot
 
 from sklearn import datasets
 import numpy as np
@@ -39,4 +40,10 @@ X_test_std = sc.transform(X=X_test);
 lr = LogisticRegression(C=100.0, random_state=1, solver="lbfgs", multi_class="ovr");
 
 lr.fit(X=X_train_std, y=y_train);
-visualization.plot_decision_regions(X=X_)
+visualization.plot_decision_regions(X=plot.X_combined_std, y=plot.y_combined, classifier=lr, test_idx=range(105, 150));
+plt.xlabel("petal length [standardized]");
+plt.ylabel("petal wideth [standardized]");
+plt.legend(loc="upper left");
+plt.tight_layout();
+
+plt.show();
