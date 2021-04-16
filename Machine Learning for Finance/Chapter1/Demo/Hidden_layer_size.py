ac3ed61eee7cb956504098c7694e8a3b5a67c97c
Ceiling_roof <ted10014@hotmail.com>
2021-04-16 20:03:15 +0800

    Committed 2021/4/16

--- Machine Learning for Finance/Chapter1/Demo/Hidden_layer_size.py
+++ Machine Learning for Finance/Chapter1/Demo/Hidden_layer_size.py
@@ -21,10 +21,11 @@ def sigmoid(x):
     Calculates the sigmoid activation of a given input x
     See: https://en.wikipedia.org/wiki/Sigmoid_function
     '''
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
-#Log Loss function
-def bce_loss(y,y_hat):
+
+# Log Loss function
+def bce_loss(y, y_hat):
     '''
     Calculates the logistic loss between a prediction y_hat and the labels y
     See: http://wiki.fast.ai/index.php/Log_Loss
@@ -35,18 +36,20 @@ def bce_loss(y,y_hat):
     '''
     minval = 0.000000000001
     N = y.shape[0]
-    l = -1/N * np.sum(y * np.log(y_hat.clip(min=minval)) + (1-y) * np.log((1-y_hat).clip(min=minval)))
+    l = -1 / N * np.sum(y * np.log(y_hat.clip(min=minval)) + (1 - y) * np.log((1 - y_hat).clip(min=minval)))
     return l
 
+
 # Log loss derivative
-def bce_loss_derivative(y,y_hat):
+def bce_loss_derivative(y, y_hat):
     '''
     Calculates the gradient (derivative) of the log loss between point y and y_hat
     See: https://stats.stackexchange.com/questions/219241/gradient-for-logistic-loss-function
     '''
-    return (y_hat-y)
+    return (y_hat - y)
 
-def forward_prop(model,a0):
+
+def forward_prop(model, a0):
     '''
     Forward propagates through the model, stores results in cache.
     See: https://stats.stackexchange.com/questions/147954/neural-network-forward-propagation
@@ -67,8 +70,9 @@ def forward_prop(model,a0):
 
     # Second activation function
     a2 = sigmoid(z2)
-    cache = {'a0':a0,'z1':z1,'a1':a1,'z1':z1,'a2':a2}
+    cache = {'a0': a0, 'z1': z1, 'a1': a1, 'z1': z1, 'a2': a2}
     return cache
+
 
 def tanh_derivative(x):
     '''
@@ -77,7 +81,8 @@ def tanh_derivative(x):
     '''
     return (1 - np.power(x, 2))
 
-def backward_prop(model,cache,y):
+
+def backward_prop(model, cache, y):
     '''
     Backward propagates through the model to calculate gradients.
     Stores gradients in grads dictionary.
@@ -87,11 +92,11 @@ def backward_prop(model,cache,y):
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
 
     # Load forward propagation results
-    a0,a1, a2 = cache['a0'],cache['a1'],cache['a2']
+    a0, a1, a2 = cache['a0'], cache['a1'], cache['a2']
 
     # Backpropagation
     # Calculate loss derivative with respect to output
-    dz2 = bce_loss_derivative(y=y,y_hat=a2)
+    dz2 = bce_loss_derivative(y=y, y_hat=a2)
 
     # Calculate loss derivative with respect to second layer weights
     dW2 = (a1.T).dot(dz2)
@@ -109,22 +114,26 @@ def backward_prop(model,cache,y):
     db1 = np.sum(dz1, axis=0)
 
     # Store gradients
-    grads = {'dW2':dW2,'db2':db2,'dW1':dW1,'db1':db1}
+    grads = {'dW2': dW2, 'db2': db2, 'dW1': dW1, 'db1': db1}
     return grads
 
 
 # Helper function to plot a decision boundary.
 # If you don't fully understand this function don't worry, it just generates the contour plot below.
 def plot_decision_boundary(pred_func):
+
     # Set min and max values and give it some padding
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     h = 0.01
+
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
     # Predict the function value for the whole gid
     Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
+
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.scatter(X[:, 0], X[:, 1], c=y.flatten(), cmap=plt.cm.Spectral)
@@ -133,17 +142,18 @@ def plot_decision_boundary(pred_func):
 # Generate a dataset and plot it
 np.random.seed(0)
 X, y = sklearn.datasets.make_moons(200, noise=0.15)
-y = y.reshape(200,1)
-plt.scatter(X[:,0], X[:,1], s=40, c=y.flatten(), cmap=plt.cm.Spectral)
+y = y.reshape(200, 1)
+plt.scatter(X[:, 0], X[:, 1], s=40, c=y.flatten(), cmap=plt.cm.Spectral)
 
 
 def predict(model, x):
     '''
     Predicts y_hat as 1 or 0 for a given input X
     '''
+
     # Do forward pass
-    c = forward_prop(model,x)
-    #get y_hat
+    c = forward_prop(model, x)
+    # get y_hat
     y_hat = c['a2']
 
     # Turn values to either 1 or 0
@@ -151,31 +161,36 @@ def predict(model, x):
     y_hat[y_hat < 0.5] = 0
     return y_hat
 
-def calc_accuracy(model,x,y):
+
+def calc_accuracy(model, x, y):
     '''
     Calculates the accuracy of the model given an input x and a correct output y.
     The accuracy is the percentage of examples our model classified correctly
     '''
     # Get total number of examples
     m = y.shape[0]
+
     # Do a prediction with the model
-    pred = predict(model,x)
+    pred = predict(model, x)
+
     # Ensure prediction and truth vector y have the same shape
     pred = pred.reshape(y.shape)
+
     # Calculate the number of wrong examples
-    error = np.sum(np.abs(pred-y))
+    error = np.sum(np.abs(pred - y))
+
     # Calculate accuracy
-    return (m - error)/m * 100
+    return (m - error) / m * 100
 
 
-def initialize_parameters(nn_input_dim,nn_hdim,nn_output_dim):
+def initialize_parameters(nn_input_dim, nn_hdim, nn_output_dim):
     '''
     Initializes weights with random number between -1 and 1
     Initializes bias with 0
     Assigns weights and parameters to model
     '''
     # First layer weights
-    W1 = 2 *np.random.randn(nn_input_dim, nn_hdim) - 1
+    W1 = 2 * np.random.randn(nn_input_dim, nn_hdim) - 1
 
     # First layer bias
     b1 = np.zeros((1, nn_hdim))
@@ -187,11 +202,11 @@ def initialize_parameters(nn_input_dim,nn_hdim,nn_output_dim):
     b2 = np.zeros((1, nn_output_dim))
 
     # Package and return model
-    model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+    model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
     return model
 
 
-def update_parameters(model,grads,learning_rate):
+def update_parameters(model, grads, learning_rate):
     '''
     Updates parameters accoarding to gradient descent algorithm
     See: https://en.wikipedia.org/wiki/Gradient_descent
@@ -206,30 +221,28 @@ def update_parameters(model,grads,learning_rate):
     b2 -= learning_rate * grads['db2']
 
     # Store and return parameters
-    model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+    model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
     return model
 
 
-
-def train(model,X_,y_,learning_rate, num_passes=20000, print_loss=False):
+def train(model, X_, y_, learning_rate, num_passes=20000, print_loss=False):
     # Gradient descent. For each batch...
     for i in range(0, num_passes):
 
         # Forward propagation
-        cache = forward_prop(model,X_)
-        #a1, probs = cache['a1'],cache['a2']
+        cache = forward_prop(model, X_)
+        # a1, probs = cache['a1'],cache['a2']
         # Backpropagation
 
-        grads = backward_prop(model,cache,y)
+        grads = backward_prop(model, cache, y)
         # Gradient descent parameter update
         # Assign new parameters to the model
-        model = update_parameters(model=model,grads=grads,learning_rate=learning_rate)
+        model = update_parameters(model=model, grads=grads, learning_rate=learning_rate)
 
         # Pring loss & accuracy every 100 iterations
         if print_loss and i % 100 == 0:
             y_hat = cache['a2']
-            print('Loss after iteration',i,':',bce_loss(y,y_hat))
-            print('Accuracy after iteration',i,':',calc_accuracy(model,X_,y_),'%')
+            print('Loss after iteration', i, ':', bce_loss(y, y_hat))
+            print('Accuracy after iteration', i, ':', calc_accuracy(model, X_, y_), '%')
 
     return model
-
