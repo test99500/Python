c32deee4ef32cbf4fc1fea87294d4d8703ab8588
Ceiling_roof <ted10014@hotmail.com>
2021-04-16 13:00:48 +0800

    Committed 2021/4/16

---
+++ Machine Learning for Finance/Chapter1/A_two_layer_model_in_Keras.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+
+# Building a neural network in the sequential API works as follows.
+
+## Firstly, we create an empty sequential model with no layers:
+model = Sequential();
+
+## Then we can add layers to this model, just like stacking a layer cake, with model.add().
+model.add();
+
+## For the first layer, we have to specify the input dimensions of the layer.
+### Two features (the coordinates of the point)
+model.add(Dense(units=3, input_dim=2));
+
+## Add a tanh activation function:
+model.add(Activation("tanh"));
+
+## Add the linear step and the activation function of the output layer:
+model.add(Dense(1));
+model.add(Activation("sigmoid"));
+
+## Get an overview of all the layers:
+print(model.summary());
