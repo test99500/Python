682291cd7dd3dc457fef7a386f3f02ac1e9d5540
Ceiling_roof <ted10014@hotmail.com>
2021-04-16 16:50:32 +0800

    Committed 2021/4/16

---
+++ Machine Learning for Finance/Chapter1/Demo/Hidden_layer_size.py
@@ -0,0 +1,235 @@
+# Package imports
+# Matplotlib is a matlab like plotting library
+import matplotlib
+import matplotlib.pyplot as plt
+# Numpy handles matrix operations
+import numpy as np
+# SciKitLearn is a useful machine learning utilities library
+import sklearn
+# The sklearn dataset module helps generating datasets
+import sklearn.datasets
+import sklearn.linear_model
+
+# Display plots inline and change default figure size
+matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
+
+
+# Just some helper functions we moved over from the last chapter
+# sigmoid function
+def sigmoid(x):
+    '''
+    Calculates the sigmoid activation of a given input x
+    See: https://en.wikipedia.org/wiki/Sigmoid_function
+    '''
+    return 1/(1+np.exp(-x))
+
+#Log Loss function
+def bce_loss(y,y_hat):
+    '''
+    Calculates the logistic loss between a prediction y_hat and the labels y
+    See: http://wiki.fast.ai/index.php/Log_Loss
+
+    We need to clip values that get too close to zero to avoid zeroing out.
+    Zeroing out is when a number gets so small that the computer replaces it with 0.
+    Therefore, we clip numbers to a minimum value.
+    '''
+    minval = 0.000000000001
+    N = y.shape[0]
+    l = -1/N * np.sum(y * np.log(y_hat.clip(min=minval)) + (1-y) * np.log((1-y_hat).clip(min=minval)))
+    return l
+
+# Log loss derivative
+def bce_loss_derivative(y,y_hat):
+    '''
+    Calculates the gradient (derivative) of the log loss between point y and y_hat
+    See: https://stats.stackexchange.com/questions/219241/gradient-for-logistic-loss-function
+    '''
+    return (y_hat-y)
+
+def forward_prop(model,a0):
+    '''
+    Forward propagates through the model, stores results in cache.
+    See: https://stats.stackexchange.com/questions/147954/neural-network-forward-propagation
+    A0 is the activation at layer zero, it is the same as X
+    '''
+
+    # Load parameters from model
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+
+    # Linear step
+    z1 = a0.dot(W1) + b1
+
+    # First activation function
+    a1 = np.tanh(z1)
+
+    # Second linear step
+    z2 = a1.dot(W2) + b2
+
+    # Second activation function
+    a2 = sigmoid(z2)
+    cache = {'a0':a0,'z1':z1,'a1':a1,'z1':z1,'a2':a2}
+    return cache
+
+def tanh_derivative(x):
+    '''
+    Calculates the derivative of the tanh function that is used as the first activation function
+    See: https://socratic.org/questions/what-is-the-derivative-of-tanh-x
+    '''
+    return (1 - np.power(x, 2))
+
+def backward_prop(model,cache,y):
+    '''
+    Backward propagates through the model to calculate gradients.
+    Stores gradients in grads dictionary.
+    See: https://en.wikipedia.org/wiki/Backpropagation
+    '''
+    # Load parameters from model
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+
+    # Load forward propagation results
+    a0,a1, a2 = cache['a0'],cache['a1'],cache['a2']
+
+    # Backpropagation
+    # Calculate loss derivative with respect to output
+    dz2 = bce_loss_derivative(y=y,y_hat=a2)
+
+    # Calculate loss derivative with respect to second layer weights
+    dW2 = (a1.T).dot(dz2)
+
+    # Calculate loss derivative with respect to second layer bias
+    db2 = np.sum(dz2, axis=0, keepdims=True)
+
+    # Calculate loss derivative with respect to first layer
+    dz1 = dz2.dot(W2.T) * tanh_derivative(a1)
+
+    # Calculate loss derivative with respect to first layer weights
+    dW1 = np.dot(a0.T, dz1)
+
+    # Calculate loss derivative with respect to first layer bias
+    db1 = np.sum(dz1, axis=0)
+
+    # Store gradients
+    grads = {'dW2':dW2,'db2':db2,'dW1':dW1,'db1':db1}
+    return grads
+
+
+# Helper function to plot a decision boundary.
+# If you don't fully understand this function don't worry, it just generates the contour plot below.
+def plot_decision_boundary(pred_func):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y.flatten(), cmap=plt.cm.Spectral)
+
+
+# Generate a dataset and plot it
+np.random.seed(0)
+X, y = sklearn.datasets.make_moons(200, noise=0.15)
+y = y.reshape(200,1)
+plt.scatter(X[:,0], X[:,1], s=40, c=y.flatten(), cmap=plt.cm.Spectral)
+
+
+def predict(model, x):
+    '''
+    Predicts y_hat as 1 or 0 for a given input X
+    '''
+    # Do forward pass
+    c = forward_prop(model,x)
+    #get y_hat
+    y_hat = c['a2']
+
+    # Turn values to either 1 or 0
+    y_hat[y_hat > 0.5] = 1
+    y_hat[y_hat < 0.5] = 0
+    return y_hat
+
+def calc_accuracy(model,x,y):
+    '''
+    Calculates the accuracy of the model given an input x and a correct output y.
+    The accuracy is the percentage of examples our model classified correctly
+    '''
+    # Get total number of examples
+    m = y.shape[0]
+    # Do a prediction with the model
+    pred = predict(model,x)
+    # Ensure prediction and truth vector y have the same shape
+    pred = pred.reshape(y.shape)
+    # Calculate the number of wrong examples
+    error = np.sum(np.abs(pred-y))
+    # Calculate accuracy
+    return (m - error)/m * 100
+
+
+def initialize_parameters(nn_input_dim,nn_hdim,nn_output_dim):
+    '''
+    Initializes weights with random number between -1 and 1
+    Initializes bias with 0
+    Assigns weights and parameters to model
+    '''
+    # First layer weights
+    W1 = 2 *np.random.randn(nn_input_dim, nn_hdim) - 1
+
+    # First layer bias
+    b1 = np.zeros((1, nn_hdim))
+
+    # Second layer weights
+    W2 = 2 * np.random.randn(nn_hdim, nn_output_dim) - 1
+
+    # Second layer bias
+    b2 = np.zeros((1, nn_output_dim))
+
+    # Package and return model
+    model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+    return model
+
+
+def update_parameters(model,grads,learning_rate):
+    '''
+    Updates parameters accoarding to gradient descent algorithm
+    See: https://en.wikipedia.org/wiki/Gradient_descent
+    '''
+    # Load parameters
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+
+    # Update parameters
+    W1 -= learning_rate * grads['dW1']
+    b1 -= learning_rate * grads['db1']
+    W2 -= learning_rate * grads['dW2']
+    b2 -= learning_rate * grads['db2']
+
+    # Store and return parameters
+    model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+    return model
+
+
+
+def train(model,X_,y_,learning_rate, num_passes=20000, print_loss=False):
+    # Gradient descent. For each batch...
+    for i in range(0, num_passes):
+
+        # Forward propagation
+        cache = forward_prop(model,X_)
+        #a1, probs = cache['a1'],cache['a2']
+        # Backpropagation
+
+        grads = backward_prop(model,cache,y)
+        # Gradient descent parameter update
+        # Assign new parameters to the model
+        model = update_parameters(model=model,grads=grads,learning_rate=learning_rate)
+
+        # Pring loss & accuracy every 100 iterations
+        if print_loss and i % 100 == 0:
+            y_hat = cache['a2']
+            print('Loss after iteration',i,':',bce_loss(y,y_hat))
+            print('Accuracy after iteration',i,':',calc_accuracy(model,X_,y_),'%')
+
+    return model
+
---
+++ Machine Learning for Finance/Chapter1/Demo/Too_small_hidden_size.py
@@ -0,0 +1,35 @@
+import numpy as np
+import Hidden_layer_size as h
+import matplotlib.pyplot as plt
+import sklearn
+
+# Now with more noise
+# Generate a dataset and plot it
+np.random.seed(0)
+
+# The data generator allows us to regulate the noise level
+X, y = sklearn.datasets.make_moons(200, noise=0.3)
+y = y.reshape(200,1)
+plt.scatter(X[:, 0], X[:, 1], s=40, c=y.flatten(), cmap=plt.cm.Spectral)
+
+
+# Hyper parameters
+hiden_layer_size = 1
+
+# I picked this value because it showed good results in my experiments
+learning_rate = 0.01
+
+# Initialize the parameters to random values. We need to learn these.
+np.random.seed(0)
+
+# This is what we return at the end
+model = h.initialize_parameters(nn_input_dim=2, nn_hdim= hiden_layer_size, nn_output_dim= 1)
+model = h.train(model, X, y, learning_rate=learning_rate, num_passes=1000, print_loss=True)
+
+# Plot the decision boundary
+h.plot_decision_boundary(lambda x: h.predict(model, x))
+plt.title("Decision Boundary for hidden layer size 1")
+
+plt.savefig("Too_small_hidden_size.jpg");
+
+plt.show();
