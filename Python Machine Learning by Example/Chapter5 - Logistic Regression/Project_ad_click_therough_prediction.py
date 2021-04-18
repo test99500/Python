14e963d06102dc5c0dbc01a639f6c30fd9abf3ec
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-04-18 16:25:23 +0800

    Committed 2021/4/18

--- Python Machine Learning by Example/Chapter5 - Logistic Regression/Project_ad_click_therough_prediction.py
+++ Python Machine Learning by Example/Chapter5 - Logistic Regression/Project_ad_click_therough_prediction.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 n_rows = 300000
 df = pd.read_csv()
+
+# Project aborted because the training set from Kaggle is too big and requires membership download,
+# which is very time-consuming.
