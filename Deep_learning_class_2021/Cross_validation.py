cf1ae2dd2329e76e5cc5a06971f67b6886a4df3e
Ceiling_roof <ted10014@hotmail.com>
2021-04-20 12:22:13 +0800

    Committed 2021/4/20

--- Deep_learning_class_2021/Cross_validation.py
+++ Deep_learning_class_2021/Cross_validation.py
@@ -1,1 +1,8 @@
 from sklearn.model_selection import cross_val_score
+import KNN
+
+scores = cross_val_score(estimator=KNN.knn, X=KNN.train_data, y=KNN.train_label, cv=5,
+                         scoring="accuracy")
+
+print("score: ", scores)
+print("mean: ", scores.mean())
