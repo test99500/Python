b6ef2fb2af48b656ee874a82877b902ef8ca2efe
Ceiling_roof <ted10014@hotmail.com>
2021-04-22 18:19:11 +0800

    Committed 2021/4/22

--- Hands on Machine Learning2/Chapter10/Simple_image_classifier_with_keras.py
+++ Hands on Machine Learning2/Chapter10/Simple_image_classifier_with_keras.py
@@ -5,6 +5,9 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
 
+print(y_train_full)
+print(y_test)
+
 print(X_train_full.shape)
 print(X_train_full.dtype)
 
@@ -15,7 +18,7 @@ y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
 
 # List the class names to know what we are dealing with:
 class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Scandal", "Shirt",
-              "Sneaker", "Bag", "Angle boot"]
+               "Sneaker", "Bag", "Angle boot"]
 
 ## The first image in the training set represents a coat:
 print(class_names[y_train[0]])
