efe3325251c0faab3503ea6e23ad336326d3eae6
Ceiling_roof <ted10014@hotmail.com>
2021-04-24 12:21:05 +0800

    Committed 2021/4/24

--- Python Machine Learning/Chapter4/Handling categorical data/Mapping_ordinal_features.py
+++ Python Machine Learning/Chapter4/Handling categorical data/Mapping_ordinal_features.py
@@ -1,4 +1,5 @@
 import Categorical_data_encoding_with_pandas as dataset
+from sklearn.preprocessing import LabelEncoder
 
 size_mapping = {"XL": 3, 'L': 2, 'M': 1}
 
@@ -9,3 +10,11 @@ print(dataset.df)
 inv_size_mapping = {v: k for k, v in size_mapping.items()}
 
 print(dataset.df["size"].map(inv_size_mapping))
+
+class_le = LabelEncoder()
+
+y = class_le.fit_transform(y=dataset.df["class_label"])
+print(y)
+
+y_inverse = class_le.inverse_transform(y)
+print(y_inverse)
