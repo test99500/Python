d32a4aef18ed046c29d36e7fc4642fd157e8f7cd
Ceiling_roof <ted10014@hotmail.com>
2021-04-24 13:02:23 +0800

    Committed 2021/4/24

--- Python Machine Learning/Chapter4/Handling categorical data/Mapping_ordinal_features.py
+++ Python Machine Learning/Chapter4/Handling categorical data/Mapping_ordinal_features.py
@@ -1,5 +1,7 @@
 import Categorical_data_encoding_with_pandas as dataset
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
 
 size_mapping = {"XL": 3, 'L': 2, 'M': 1}
 
@@ -18,3 +20,20 @@ print(y)
 
 y_inverse = class_le.inverse_transform(y)
 print(y_inverse)
+
+print(dataset.df)
+
+numerical = dataset.df.drop(["color", "class_label"], axis=1)
+print(numerical)
+
+categorical = dataset.df.filter(["color", "class_label"], axis=1)
+print(categorical)
+
+cat_numerical = pd.get_dummies(data=categorical, drop_first=True)
+print(cat_numerical)
+
+X = pd.concat([numerical, cat_numerical], axis=1)
+print(X)
+
+X = X.drop(["class_label_class2"], axis=1)
+print(X)
