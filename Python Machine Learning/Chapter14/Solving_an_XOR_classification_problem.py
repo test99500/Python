721ac3e0e0fd01f60df413e10b53a484ea4baff2
Ceiling_roof <ted10014@hotmail.com>
2021-04-26 22:39:25 +0800

    Committed 2021/4/26

--- Python Machine Learning/Chapter14/Solving_an_XOR_classification_problem.py
+++ Python Machine Learning/Chapter14/Solving_an_XOR_classification_problem.py
@@ -10,4 +10,22 @@ np.random.seed(1)
 x = np.random.uniform(low=-1, high=1, size=(200, 2))
 print(x)
 
+# Assign the ground truth label
 y = np.ones(len(x))
+print(y)
+
+## Applying the threshold rule:
+y[x[:, 0] * x[:, 1] < 0] = 0
+
+# Use half of the data (100 training examples) for training and the remaining half for
+# validation
+## feature set (0 - 99)
+x_train = x[:100, :]
+## label set (0- 99)
+y_train = y[:100]
+
+## the feature set for validation (100 - 199)
+x_valid = x[100:, :]
+
+## the label set for validation (100 - 199)
+y_valid = y[100:]
