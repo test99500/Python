3dd1e27dcf3ebe84fe1c6c6b4b9e9147fa484de5
Ceiling_roof <ted10014@hotmail.com>
2021-04-28 13:03:02 +0800

    Committed 2021/4/28

---
+++ Python Machine Learning/Handy_tools/plot_decision_regions()6.py
@@ -0,0 +1,26 @@
+from mlxtend.plotting import plot_decision_regions
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.svm import SVC
+import pandas as pd
+
+# Loading some example data
+iris = datasets.load_iris()
+
+print(iris)
+
+# Put data into data frame to improve readability
+iris_df = pd.DataFrame(data=datasets.load_iris().data)
+print(iris_df)
+
+# Rename the column
+new_iris_df = iris_df.rename(columns={0: "sepal length ", 1:"sepal width", 2: "petal length",
+                                      3: "petal width"})
+
+print(new_iris_df)
+
+# Concatenate the target column to new_iris_df in order to see the full
+new_iris_df["target"] = datasets.load_iris().target
+print(new_iris_df)
+
+#
