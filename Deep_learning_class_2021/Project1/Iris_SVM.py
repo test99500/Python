e21270a785495ad9bb3bd53176ff6301b84e5058
Ceiling_roof <ted10014@hotmail.com>
2021-04-30 14:00:28 +0800

    Committed 2021/4/30

---
+++ Deep_learning_class_2021/Project1/Iris_SVM.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+iris = load_iris()
+iris_data=iris.data
+print(iris_data)
+X_train, X_test, y_train, y_test = train_test_split(iris_data, iris.target, test_size=0.33);
+
+print(X_train.shape)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
