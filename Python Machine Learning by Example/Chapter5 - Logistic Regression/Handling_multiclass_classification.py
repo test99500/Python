bc180716c93ecd91c0f9a788923a0881c702b157
Ceiling_roof <ted10014@hotmail.com>
2021-04-30 18:12:54 +0800

    Committed 2021/4/30

---
+++ Python Machine Learning by Example/Chapter5 - Logistic Regression/Handling_multiclass_classification.py
@@ -0,0 +1,8 @@
+from sklearn import datasets
+
+digits = datasets.load_digits()
+
+n_samples = len(digits.images)
+
+# As the image data is stored in 8 x 8 matrices, we need to flatten them.
+X = digits.images.reshape((n_samples, -1))
