5045a03201920d925ab0842cf689bbe8c8fcdcc1
Ceiling_roof <ted10014@hotmail.com>
2021-05-01 18:09:50 +0800

    Committed 2021/05/01

--- Deep_learning_class_2021/Project1/Iris_SVM.py
+++ Deep_learning_class_2021/Project1/Iris_SVM.py
@@ -4,10 +4,13 @@ from sklearn import svm
 from sklearn import metrics
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 iris = load_iris()
 iris_data=iris.data
 print(iris_data)
+
 X_train, X_test, y_train, y_test = train_test_split(iris_data, iris.target, test_size=0.33);
 
 print(X_train.shape)
@@ -17,13 +20,22 @@ print(X_test)
 print(y_train)
 print(y_test)
 
+pipeline = make_pipeline(StandardScaler(), svm.SVC())
+pipeline.fit(X=X_train, y=y_train)
+
 svm_reg = svm.SVR()
 regressor = svm_reg.fit(X=X_train, y=y_train)
 
 y_prediction = svm_reg.predict(X=X_test)
+y_prediction_Scaled = pipeline.predict(X=X_test)
 
 print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_prediction));
 print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_prediction));
 print("Root Mean Squared Error:", np.sqrt(metrics.mean_squared_error(y_test, y_prediction)));
 
-score = cross_val_score(estimator=svm.SVC(random_state=1))
+print("Mean Absolute Error (after scaling input data):", metrics.mean_absolute_error(y_test, y_prediction_Scaled));
+print("Mean Squared Error (after scaling input data):", metrics.mean_squared_error(y_test, y_prediction_Scaled));
+print("Root Mean Squared Error (after scaling input data):", np.sqrt(metrics.mean_squared_error(y_test, y_prediction_Scaled)));
+
+
+score = cross_val_score(estimator=svm.SVC(random_state=1), X=)
