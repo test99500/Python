ed3e2abaf03556d6915cda148b62adf240666529
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-02 21:05:19 +0800

    Committed 2021/05/02

--- Python Machine Learning/Chapter6/Validation_curves.py
+++ Python Machine Learning/Chapter6/Validation_curves.py
@@ -12,4 +12,35 @@ pipe_lr = make_pipeline(StandardScaler(), LogisticRegression(penalty='l2', rando
 
 param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
 
-train_scores, test_scores = validation_curve(estimator=pipe_lr, X=dataset.X, y=dataset.y)
+train_scores, test_scores = validation_curve(estimator=pipe_lr, X=dataset.X, y=dataset.y,
+                                             param_name='logisticregression__C',
+                                             param_range=param_range, cv=10)
+
+print(train_scores)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean, color='blue', marker='o', markersize=5,
+         label='Training accuracy')
+
+plt.fill_between(x=param_range, y1=train_mean + train_std, y2=train_mean - train_std,
+                 alpha=0.15, color='blue')
+
+plt.plot(param_range, test_mean, color='green', linestyle='--', marker='s', markersize=5,
+         label='Validation accuracy')
+
+plt.fill_between(x=param_range, y1=test_mean + test_std, y2=test_mean - test_std, alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+
+plt.savefig("Validation_curves.jpg")
+
+plt.show()
