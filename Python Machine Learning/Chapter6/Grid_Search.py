a8e913103db3295f9f63492fab6703a5f55b725e
Ceiling_roof <ted10014@hotmail.com>
2021-05-02 23:13:29 +0800

    Committed 2021/05/02

--- Python Machine Learning/Chapter6/Grid_Search.py
+++ Python Machine Learning/Chapter6/Grid_Search.py
@@ -8,8 +8,9 @@ pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
 
 param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
 
-param_grid = [{"svc_C": param_range, "svc_Kernel": ["linear"]},
-              {"svc_C": param_range, "svc_gamma": param_range, "svc_kernel": ["rbf"]}]
+# Note that 'k' should be lowercase.
+param_grid = [{"svc__C": param_range, "svc__kernel": ["linear"]},
+              {"svc__C": param_range, "svc__gamma": param_range, "svc__kernel": ["rbf"]}]
 
 gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring="accuracy",
                   cv=10, refit=True)
--- Python Machine Learning/Chapter6/Grid_Search3.py
+++ Python Machine Learning/Chapter6/Grid_Search3.py
@@ -8,7 +8,7 @@ pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
 
 param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
 
-param_grid = [{'svc__C': param_range, 'svc__Kernel': ['linear']},
+param_grid = [{'svc__C': param_range, 'svc__kernel': ['linear']},
               {'svc__C': param_range, 'svc__gamma': param_range, 'svc__kernel': ['rbf']}]
 
 gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring="accuracy",
