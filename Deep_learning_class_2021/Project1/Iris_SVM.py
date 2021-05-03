af1bccc993419515dc86e98a6bac626193033312
Ceiling_roof <ted10014@hotmail.com>
2021-05-03 08:49:12 +0800

    Committed 2021/05/03

--- Deep_learning_class_2021/Project1/Iris_SVM.py
+++ Deep_learning_class_2021/Project1/Iris_SVM.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
 
 iris = load_iris()
 iris_data = iris.data
@@ -45,6 +46,11 @@ score = cross_val_score(estimator=svm.SVC(random_state=1), X=X_train2, y=iris.ta
                         scoring="accuracy")
 print("Array of accuracy of the estimator for each run of the cross validation: ", score)  # [1]
 print("Mean accuracy: ", score.mean())
+
+print("Confusion matrix: ", confusion_matrix(y_true=y_test, y_pred=y_prediction_Scaled))
+print("Precision score: ", precision_score(y_true=y_test, y_pred=y_prediction_Scaled))
+print("Recall: ", recall_score(y_true=y_test, y_pred=y_prediction_Scaled))
+print("F1: ", f1_score(y_true=y_test, y_pred=y_prediction_Scaled))
 
 # References:
 # 1. https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_val_score.html
