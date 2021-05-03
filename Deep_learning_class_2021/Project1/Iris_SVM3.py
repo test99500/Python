820e144e56e0737e42a74b6ef4c18575d5beabfc
Ceiling_roof <ted10014@hotmail.com>
2021-05-03 14:47:30 +0800

    Committed 2021/05/03

--- Deep_learning_class_2021/Project1/Iris_SVM3.py
+++ Deep_learning_class_2021/Project1/Iris_SVM3.py
@@ -10,6 +10,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, confusion_m
 
 iris = load_iris()
 iris_data = iris.data
+iris_label = iris.target
 print(iris_data)
 
 pipeline = make_pipeline(StandardScaler(), svm.SVC(random_state=1))
@@ -17,17 +18,15 @@ pipeline.fit(X=iris_data, y=iris.target)
 
 y_prediction_Scaled = pipeline.predict(X=iris_data)
 
-
-print("Mean Absolute Error (after scaling input data):", metrics.mean_absolute_error(iris.target, y_prediction_Scaled));
-print("Mean Squared Error (after scaling input data):", metrics.mean_squared_error(iris.target, y_prediction_Scaled));
-print("Root Mean Squared Error (after scaling input data):",
-      np.sqrt(metrics.mean_squared_error(iris.target, y_prediction_Scaled)))
-
 score = cross_val_score(estimator=pipeline, X=iris_data, y=iris.target, cv=10, scoring="accuracy")
 print("Array of accuracy of the estimator for each run of the cross validation: ", score)
 print("Mean accuracy: ", score.mean())
 
-print("Confusion matrix: ", confusion_matrix(y_true=iris.target, y_pred=y_prediction_Scaled))
-print("Precision score: ", precision_score(y_true=iris.target, y_pred=y_prediction_Scaled))
-print("Recall: ", recall_score(y_true=iris.target, y_pred=y_prediction_Scaled))
-print("F1: ", f1_score(y_true=iris.target, y_pred=y_prediction_Scaled))
+X_train, y_train, X_test, y_test = train_test_split(iris_data, iris.target, test_size=0.33,
+                                                    random_state=1)
+
+
+
+target_names = ['setosa', 'veriscolor', 'virginica']
+
+print("Accuracy score: ", accuracy_score(iris.target.prediction))
