a9d03726eb374e7f2cf83173d70e4f64abd2d681
Ceiling_roof <ted10014@hotmail.com>
2021-05-05 01:38:24 +0800

    Committed 2021/05/04

---
+++ Hands on Machine Learning2/Chapter6 - Decision Trees/Train_and_Visualizing_a_decision_tree.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+
+iris = load_iris()
+X = iris.data[:, 2:]  # petal length and width
+y = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2)
+
+tree_clf.fit(X=X, y=y)
+
+export_graphviz(decision_tree=tree_clf, out_file=image_path("iris_tree.dot"),
+                feature_names=iris.feature_names[2:],
+                class_names=iris.target_names,
+                rounded=True,
+                filled=True)
