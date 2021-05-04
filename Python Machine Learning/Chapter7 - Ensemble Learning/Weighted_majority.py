17012dea34659c3b1736811a8a550da8af7528bc
Ceiling_roof <ted10014@hotmail.com>
2021-05-04 14:34:18 +0800

    Committed 2021/05/04

--- Python Machine Learning/Chapter7 - Ensemble Learning/Weighted_majority.py
+++ Python Machine Learning/Chapter7 - Ensemble Learning/Weighted_majority.py
@@ -3,4 +3,20 @@ import pandas as pd
 
 np.argmax(np.bincount([0, 0, 1], weights=[0.2, 0.2, 0.6]))
 
-df = pd.DataFrame(data=[[0.9, 0.1], [0.8, 0.2], [0.4, 0.6]])
+df = pd.DataFrame(data=[[0.9, 0.1], [0.8, 0.2], [0.4, 0.6]],
+                  index=["Classifier1", "Classifier2", "Classifier3"],
+                  columns=["Class 1", "Class 2"])
+
+print(df)
+
+p = df.multiply(other=[0.2, 0.2, 0.6], axis=0)
+print(p)
+
+print(p.iloc[:, 0].sum())
+
+s = pd.Series(data=[p.loc[:, "Class 1"].sum(), p.loc[:, "Class 2"].sum()])
+s.name = "Probability"
+print(s)
+
+total = p.append(other=s)
+print(total)
