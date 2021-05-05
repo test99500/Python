bb697d7a838c63778c6aa40a467e21167c6ea623
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-06 00:24:42 +0800

    Committed 2021/05/05

--- Hands on Machine Learning2/Chapter7/Gradient_Boosting.py
+++ Hands on Machine Learning2/Chapter7/Gradient_Boosting.py
@@ -1,25 +1,25 @@
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_moons
+import numpy as np
 
-X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+np.random.seed(42)
+X = np.random.rand(100, 1) - 0.5
+y = 3*X[:, 0]**2 + 0.05 * np.random.randn(100)
 
 # First, let's fit a DecisionTreeRegressor to the training set.
-tree_reg1 = DecisionTreeRegressor(max_depth=2)
+tree_reg1 = DecisionTreeRegressor(max_depth=2, random_state=42)
 tree_reg1.fit(X=X, y=y)
 
 # Next, we will train a second DecisionTreeRegressor on the residual errors made by the first
 # predictor.
 y2 = y - tree_reg1.predict(X=X)
 
-tree_reg2 = DecisionTreeRegressor(max_depth=2)
+tree_reg2 = DecisionTreeRegressor(max_depth=2, random_state=42)
 tree_reg2.fit(X=X, y=y2)
 
 # Then, we train a third regressor on the residual errors made by the second predictor.
 y3 = y2 - tree_reg2.predict(X=X)
 
-tree_reg3 = DecisionTreeRegressor(max_depth=2)
+tree_reg3 = DecisionTreeRegressor(max_depth=2, random_state=42)
 
 tree_reg3.fit(X=X, y=y3)
 
@@ -27,4 +27,9 @@ tree_reg3.fit(X=X, y=y3)
 
 ## This ensemble can now make predictions on a new instance simply by adding up the predictions of
 # all the trees.
+
+X_new = np.array([[0.8]])
+
 y_prediction = sum(tree.predict(X=X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
+
+print(y_prediction)
