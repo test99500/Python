8318e9b658504ca27d8e03dfa167c70a4d180ada
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-06 00:18:29 +0800

    Committed 2021/05/05

--- Hands on Machine Learning2/Chapter7/Gradient_Boosting.py
+++ Hands on Machine Learning2/Chapter7/Gradient_Boosting.py
@@ -23,3 +23,8 @@ tree_reg3 = DecisionTreeRegressor(max_depth=2)
 
 tree_reg3.fit(X=X, y=y3)
 
+# Now that we have an ensemble containing three trees.
+
+## This ensemble can now make predictions on a new instance simply by adding up the predictions of
+# all the trees.
+y_prediction = sum(tree.predict(X=X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
