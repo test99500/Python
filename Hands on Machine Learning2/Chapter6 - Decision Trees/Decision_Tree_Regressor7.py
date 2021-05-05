3b20dff3de8ad6f74213b5454b0eca49093eefa3
Ceiling_roof <ted10014@hotmail.com>
2021-05-05 15:01:17 +0800

    Committed 2021/05/05

---
+++ Hands on Machine Learning2/Chapter6 - Decision Trees/Decision_Tree_Regressor7.py
@@ -0,0 +1,45 @@
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+# Quadratic training set + noise
+
+np.random.seed(42)
+m = 200
+
+X = np.random.rand(m, 1)
+y = 4 * (X - 0.5) ** 2
+
+y = y + np.random.randn(m, 1) / 10
+
+tree_reg1 = DecisionTreeRegressor(random_state=42, max_depth=5)
+tree_reg2 = DecisionTreeRegressor(random_state=42, min_samples_leaf=10)
+tree_reg1.fit(X, y)
+tree_reg2.fit(X, y)
+
+x1 = np.linspace(0, 1, 500).reshape(-1, 1)
+y_pred1 = tree_reg1.predict(x1)
+y_pred2 = tree_reg2.predict(x1)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+
+plt.sca(axes[0])
+plt.plot(X, y, "b.")
+plt.plot(x1, y_pred1, "r.-", linewidth=2, label=r"$\hat{y}$")
+plt.axis([0, 1, -0.2, 1.1])
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", fontsize=18, rotation=0)
+plt.legend(loc="upper center", fontsize=18)
+plt.title("Max_depth=5", fontsize=14)
+
+plt.sca(axes[1])
+plt.plot(X, y, "b.")
+plt.plot(x1, y_pred2, "r.-", linewidth=2, label=r"$\hat{y}$")
+plt.axis([0, 1, -0.2, 1.1])
+plt.xlabel("$x_1$", fontsize=18)
+plt.title("min_samples_leaf={}".format(tree_reg2.min_samples_leaf), fontsize=14)
+
+plt.savefig("tree_regression_regularization_plot5.jpg")
+
+plt.show()
