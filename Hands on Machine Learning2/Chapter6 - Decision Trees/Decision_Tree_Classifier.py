ca20225a2c3576fcba091caa0771b53b513f232e
Ceiling_roof <ted10014@hotmail.com>
2021-05-05 14:05:26 +0800

    Committed 2021/05/05

--- Hands on Machine Learning2/Chapter6 - Decision Trees/Decision_Tree_Classifier.py
+++ Hands on Machine Learning2/Chapter6 - Decision Trees/Decision_Tree_Classifier.py
@@ -12,8 +12,10 @@ tree_clf.fit(X=X, y=y)
 tree.plot_tree(decision_tree=tree_clf, feature_names=iris.feature_names[2:],
                class_names=iris.target_names, rounded=True, filled=True)
 
+# A flower whose petals are 5 cm long and 1.5 cm wide.
 probability = tree_clf.predict_proba([[5, 1.5]])
 print(probability)
 
+# A flower whose petals are 5 cm long and 1.5 cm wide.
 prediction = tree_clf.predict([[5, 1.5]])
 print(prediction)
