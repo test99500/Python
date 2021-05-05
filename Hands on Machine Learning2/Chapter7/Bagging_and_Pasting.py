75603d9fa6bb9f12fc3e1f0b0d51626707732b99
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-05 18:03:28 +0800

    Committed 2021/05/05

--- Hands on Machine Learning2/Chapter7/Bagging_and_Pasting.py
+++ Hands on Machine Learning2/Chapter7/Bagging_and_Pasting.py
@@ -6,7 +6,9 @@ from sklearn.datasets import make_moons
 X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-bag_clf = BaggingClassifier(base_estimator=DecisionTreeClassifier(), n_estimators=500, max_samples=100, bootstrap=True, n_jobs=-1)
+bag_clf = BaggingClassifier(base_estimator=DecisionTreeClassifier(), n_estimators=500,
+                            max_samples=100, bootstrap=True, n_jobs=-1)
+
 bag_clf.fit(X=X_train, y=y_train)
 
 y_prediction = bag_clf.predict(X=X_test)
