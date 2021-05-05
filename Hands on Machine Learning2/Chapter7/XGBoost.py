582e6843bb0b968ffba81edc2ebcfcc13a931480
Ceiling_roof <ted10014@hotmail.com>
2021-05-06 02:34:10 +0800

    Committed 2021/05/05

--- Hands on Machine Learning2/Chapter7/XGBoost.py
+++ Hands on Machine Learning2/Chapter7/XGBoost.py
@@ -1,19 +1,21 @@
+import timeit
+
+from sklearn.ensemble import GradientBoostingRegressor
+
 try:
     import xgboost
 except ImportError as ex:
     print("Error: the xgboost library is not installed.")
     xgboost = None
 
-
 if xgboost is not None:
-
     import numpy as np
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import mean_squared_error
 
     np.random.seed(42)
     X = np.random.rand(100, 1) - 0.5
-    y = 3*X[:, 0]**2 + 0.05 * np.random.randn(100)
+    y = 3 * X[:, 0] ** 2 + 0.05 * np.random.randn(100)
 
     X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
 
@@ -28,3 +30,6 @@ if xgboost is not None:
     y_prediction2 = xgb_reg.predict(X=X_val)
     val_error = mean_squared_error(y_true=y_val, y_pred=y_prediction2)
     print("Validation MSE: ", val_error)
+
+    timeit.timeit(xgb_reg.fit(X_train, y_train))
+    timeit.timeit(GradientBoostingRegressor().fit(X_train, y_train))
