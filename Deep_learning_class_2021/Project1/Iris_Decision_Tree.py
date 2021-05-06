b5a026022800fdfae89e214e9ebc4a784f351c1b
Ceiling_roof <ted10014@hotmail.com>
2021-05-06 19:27:00 +0800

    Committed 2021/05/06

--- Deep_learning_class_2021/Project1/Iris_Decision_Tree.py
+++ Deep_learning_class_2021/Project1/Iris_Decision_Tree.py
@@ -3,6 +3,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+import matplotlib.pyplot as plt
 
 iris = load_iris()
 
@@ -15,3 +16,7 @@ print(iris_label)
 train_data, test_data, train_label, test_label = train_test_split(iris_data, iris_label,
                                                                   test_size=0.33)
 
+tree_classifier = DecisionTreeClassifier(max_depth=2)
+tree_classifier.fit(X=train_data, y=train_label)
+
+tree.plot_tree(tree_classifier)
