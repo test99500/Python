6226ab61840d152f64fcf15c62a3f2ab64e7bb5d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-07 01:49:30 +0800

    Committed 2021/05/06

--- Deep_learning_class_2021/Project1/Breast_Cancer.py
+++ Deep_learning_class_2021/Project1/Breast_Cancer.py
@@ -1,5 +1,32 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
 
+# Load dataset
 data = load_breast_cancer()
-
 print(data)
+
+# Organize our data
+label_names = data["target_names"];
+labels = data["target"];
+feature_name = data["feature_names"];
+features = data["data"];
+
+# Look at our data
+print(label_names);
+print(labels[0]);
+print(feature_name[0]);
+print(features[0]);
+
+# Split our data (divide data into Training and Test sets)
+train, test, train_label, test_labels = train_test_split(features, labels, test_size=0.33,
+                                                         random_state=42);
+
+tree_classifier = DecisionTreeClassifier(max_depth=5)
+tree_classifier.fit(X=train, y=train_label)
+
+y_prediction = tree_classifier.predict(X=test)
+
