95b7b543c2cdbc19e76ef84185dc80db3193d853
Ceiling_roof <ted10014@hotmail.com>
2021-05-06 18:01:22 +0800

    Committed 2021/05/06

---
+++ Deep_learning_class_2021/Project1/Iris_Decision_Tree.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
