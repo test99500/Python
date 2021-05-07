305200c5b7b479192102e985a89acdc0829fd700
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-07 14:53:33 +0800

    Committed 2021/05/07

--- Python Machine Learning by Example/Chapter3 - Support Vector Machine/Breast_Cancer_Diagnosis.py
+++ Python Machine Learning by Example/Chapter3 - Support Vector Machine/Breast_Cancer_Diagnosis.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 cancer_data = load_breast_cancer()
 
@@ -7,3 +9,14 @@ Y = cancer_data.target
 
 print('Input data size: ', X.shape)
 print('Output data size: ', Y.shape)
+print('Label names: ', cancer_data.target_names)
+
+n_pos = (Y == 1).sum()
+n_neg = (Y == 0).sum()
+print(f'{n_pos} positive samples and {n_neg} negative samples.')
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=42)
+
+classifier = SVC(C=1.0, kernel='linear', random_state=42)
+
+classifier.fit(X=X_train, y=Y_train)
