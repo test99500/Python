761adcbc23bb2d54b1ed21a5b5d475051ea44877
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-07 18:03:16 +0800

    Committed 2021/05/07

--- Deep_learning_class_2021/Project1/Breast_Cancer_NNs.py
+++ Deep_learning_class_2021/Project1/Breast_Cancer_NNs.py
@@ -4,6 +4,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
 import numpy as np
+import keras
 
 cancer_data = load_breast_cancer()
 
@@ -22,4 +23,3 @@ n_negative = (y == 0).sum()
 print(f'{n_positive} positive samples and {n_negative} negative samples.')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.33)
-
