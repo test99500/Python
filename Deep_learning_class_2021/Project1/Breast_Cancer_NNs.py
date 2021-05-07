1cea203132b07ca05ad0fcc71515de435bbc7d45
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-07 16:54:26 +0800

    Committed 2021/05/07

---
+++ Deep_learning_class_2021/Project1/Breast_Cancer_NNs.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+import numpy as np
+
+cancer_data = load_breast_cancer()
+
+X = cancer_data.data
+y = cancer_data.target
+
+print('Input data size: ', X.shape)
+print('Output data size: ', y.shape)
+
+# Check to see if it's a binary or multi-class classification.
+print('Label names: ', cancer_data.target_names)
+
+# Determine if the data distribution among the dataset is balanced.
+n_positive = (y == 1).sum()
+n_negative = (y == 0).sum()
+print(f'{n_positive} positive samples and {n_negative} negative samples.')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.33)
+
--- Deep_learning_class_2021/Project1/Iris_SVM4.py
+++ Deep_learning_class_2021/Project1/Iris_SVM4.py
@@ -11,8 +11,13 @@ iris_data = iris.data
 iris_label = iris.target
 print(iris_data)
 print(iris_label)
+print(iris_label.shape)
 
-X_train, y_train, X_test, y_test = train_test_split(iris_data, iris_label, test_size=0.33,
+reshaped_iris_label = iris_label.reshape(1, 150)
+print(reshaped_iris_label)
+print(reshaped_iris_label.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(iris_data, iris_label, test_size=0.33,
                                                     random_state=1)
 
 classifier = SVC(C=5.0)
@@ -24,4 +29,13 @@ print("Ground truth: ", y_test)
 
 print("Accuracy: ", accuracy_score(y_true=y_test, y_pred=y_prediction))
 
-print("Classification report: ", classification_report(y_true=y_test, y_pred=y_prediction))
+print("Classification report: ", '\n', classification_report(y_true=y_test, y_pred=y_prediction))
+
+scores = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, scoring='accuracy')
+
+print("Scores: ", scores)
+print("Mean Scores: ", scores.mean())
+
+y_prediction2 = classifier.predict(X=X_train)
+
+print("Report", '\n', classification_report(y_true=y_train, y_pred=y_prediction2))
