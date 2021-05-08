0e9826f5f6b9ff97fe606b5d0044cc49d31319f1
Ceiling_roof <ted10014@hotmail.com>
2021-05-08 16:53:24 +0800

    Committed 2021/05/08

---
+++ Supplementary materials/Matplotlib/figure_size.py
@@ -0,0 +1,11 @@
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(9.6, 7.2), dpi=100)
+
+# figsize = Width, height in inches. [1]
+# dpi = 100 means the figure will be 11 MB large.
+
+# References:
+# 1. https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.figure.html
+# 2. https://pixelcalculator.com/en
