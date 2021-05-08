89ec598526bb44f2bfb3af7d15f1414ccaa4bca4
Ceiling_roof <ted10014@hotmail.com>
2021-05-08 22:22:56 +0800

    Committed 2021/05/08

--- Supplementary materials/Keras/MNIST.py
+++ Supplementary materials/Keras/MNIST.py
@@ -22,3 +22,49 @@ def plot_image(image):
 
 
 plot_image(x_train[1])
+print(y_train[1])
+
+def plot_image_labels(images, labels, idx, num = 10):
+    fig = plt.gcf()
+    fig.set_size_inches(12, 14)
+    if num > 25: num = 25
+
+    for i in range(0, num):
+        ax = plt.subplot(5, 5, 1 + i)
+        ax.imshow(images[idx], cmap='binary')
+        title = "label=" + str(labels[idx])
+        ax.set_title(title, fontsize=10)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        idx += 1
+    plt.show()
+
+
+plot_image_labels(images=x_train, labels=y_train, idx=0, num=10)
+
+plot_image_labels(images=x_test, labels=y_test, idx=0, num=10)
+
+x_Train = x_train.reshape(60000, 28 * 28).astype('float32')
+x_Test = x_test.reshape(10000, 28 * 28).astype('float32')
+
+print('x_train:', x_Train.shape)
+print('x_test:', x_Test.shape)
+
+print(x_train[0])
+
+# Scaling the data, which is also known as normalization
+x_Train_normalization = x_Train / 255
+x_Test_normalization = x_Test / 255
+
+# Checking the image after normalization
+print(x_Train_normalization[0])
+
+print(y_train[:5])
+
+y_Train_OneHot = np_utils.to_categorical(y=y_train)
+y_Test_OneHot = np_utils.to_categorical(y=y_test)
+
+print(y_Train_OneHot[:5])
+
+# Reference:
+# https://web.archive.org/web/20210508130921/https://waternotetw.blogspot.com/2018/03/keras-mnist.html
