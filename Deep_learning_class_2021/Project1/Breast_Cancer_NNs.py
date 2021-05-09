5f87044e9872d13283947699c6efaf8490aa01bd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-09 12:16:44 +0800

    Committed 2021/05/09

--- Deep_learning_class_2021/Project1/Breast_Cancer_NNs.py
+++ Deep_learning_class_2021/Project1/Breast_Cancer_NNs.py
@@ -23,3 +23,35 @@ n_negative = (y == 0).sum()
 print(f'{n_positive} positive samples and {n_negative} negative samples.')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.33)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+# Input's shape
+print(X_train.shape)
+
+model = keras.Sequential()
+
+model.add(keras.Input(shape=(30, )))
+model.add(keras.layers.Dense(units=128, activation='relu', name='layer1'))
+model.add(keras.layers.Dense(units=64, activation='relu', name='layer2'))
+model.add(keras.layers.Dense(units=3, activation='softmax', name='layer3'))
+
+print(model.summary())
+
+keras.utils.plot_model(model=model, show_shapes=True, show_layer_names=True)
+
+# compile
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy')
+
+# Train
+history = model.fit(x=X_train, y=y_train, epochs=200, batch_size=10, validation_split=0.33)
+
+y_prediction = model.predict(x=X_test)
+
+y_prediction_bool = np.argmax(y_prediction, axis=1)
+
+print(classification_report(y_true=y_test, y_pred=y_prediction_bool,
+                            target_names=cancer_data.target_names))
