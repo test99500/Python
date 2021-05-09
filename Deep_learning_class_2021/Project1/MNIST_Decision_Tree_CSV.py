45dc985c97a229b30c5a3411f2f37a0ea94cff5c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-09 20:44:01 +0800

    Committed 2021/05/09

--- Deep_learning_class_2021/Project1/MNIST_Decision_Tree_CSV.py
+++ Deep_learning_class_2021/Project1/MNIST_Decision_Tree_CSV.py
@@ -5,3 +5,11 @@ url = "https://media.githubusercontent.com/media/ResilientSpring/Python/master/D
 
 MNIST_data = pd.read_csv(filepath_or_buffer=url, header=None)
 print(MNIST_data.head(n=100))
+
+# Feature set
+features = MNIST_data.iloc[1:, 1:]
+print(features)
+
+# Label set
+labels = MNIST_data.iloc[1:, 0]
+print(labels)
