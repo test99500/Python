f2e5222e7c9ca75fc7d152aa8311bdbfcb2936db
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-09 20:31:58 +0800

    Committed 2021/05/09

---
+++ Deep_learning_class_2021/Project1/MNIST_Decision_Tree_CSV.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+url = "https://media.githubusercontent.com/media/ResilientSpring/Python/master/Deep_learning_class_2021/Project1/mnist_train.csv"
+
+MNIST_data = pd.read_csv(filepath_or_buffer=url, header=None)
+print(MNIST_data.head(n=100))
