a4d48e877888c628e595beedc5713cec21556a62
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-09 14:05:49 +0800

    Committed 2021/05/09

--- Deep_learning_class_2021/Project1/Waveform_SVM.py
+++ Deep_learning_class_2021/Project1/Waveform_SVM.py
@@ -28,3 +28,10 @@ support_vector_machine.fit(X=X_train, y=y_train)
 y_prediction = support_vector_machine.predict(X=X_test)
 
 print(classification_report(y_true=y_test, y_pred=y_prediction))
+
+scores = cross_val_score(estimator=support_vector_machine, X=X_train, y=y_train,
+                         scoring='accuracy', cv=10)
+
+y_prediction2 = support_vector_machine.predict(X=X_train)
+
+print(classification_report(y_true=y_train, y_pred=y_prediction2))
