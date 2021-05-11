1cfe24431b924af9e4642892c9d58bca05ff4bbe
Ceiling_roof <ted10014@hotmail.com>
2021-05-11 23:03:14 +0800

    Committed 2021/05/11

---
+++ Python for Excel/Chapter5 - Data Analysis with pandas/Selecting Data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+url = "https://github.com/fzumstein/python-for-excel/blob/1st-edition/xl/course_participants.xlsx?raw=true"
+
+df = pd.read_excel(url, sheet_name="Sheet1")
+
+print(df)
+
+print(df.loc[2, "country"])
+
