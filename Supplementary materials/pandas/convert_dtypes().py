1e8c1c627bec24de58add4a9f4959338e0ac9eba
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-14 13:19:32 +0800

    Committed 2021/05/14

---
+++ Supplementary materials/pandas/convert_dtypes().py
@@ -0,0 +1,4 @@
+# convert_dtypes(infer_objects=True, convert_string=True, convert_integer=True,
+#               convert_boolean=True, convert_floating=True)
+
+# Reference: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.convert_dtypes.html#pandas.DataFrame.convert_dtypes
