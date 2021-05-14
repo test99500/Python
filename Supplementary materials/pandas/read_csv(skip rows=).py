d5de16861c9cf267e633ffe2e451a891ca6c16cb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-14 13:29:56 +0800

    Committed 2021/05/14

---
+++ Supplementary materials/pandas/read_csv(skip rows=).py
@@ -0,0 +1,10 @@
+# skiprows : Line numbers to skip while reading csv.
+## If it’s an int then skip that lines from top
+## If it’s a list of int then skip lines at those index positions
+## If it’s a callable function then pass each index to this function to check if line to skipped or not.
+
+# Reference: https://web.archive.org/web/20201231022908/https://thispointer.com/pandas-skip-rows-while-reading-csv-file-to-a-dataframe-using-read_csv-in-python/
+
+
+import pandas as pd
+
