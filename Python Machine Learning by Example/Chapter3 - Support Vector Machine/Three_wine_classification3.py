eaf377773a3282a8b246a9ef0cad170e4ddedfa5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-15 17:49:31 +0800

    Committed 2021/05/15

--- Python Machine Learning by Example/Chapter3 - Support Vector Machine/Three_wine_classification3.py
+++ Python Machine Learning by Example/Chapter3 - Support Vector Machine/Three_wine_classification3.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
+import numpy as np
 
 dataset = load_wine(as_frame=True)
 
@@ -9,3 +10,4 @@ print(dataset)
 
 dataset2 = load_wine(return_X_y=True, as_frame=True)
 print(dataset2)
+
