a29c5bbb421b75cb524d45d30413fa6b790146ad
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-15 15:54:58 +0800

    Committed 2021/05/14

--- Python Machine Learning by Example/Chapter3 - Support Vector Machine/Face_image_classification.py
+++ Python Machine Learning by Example/Chapter3 - Support Vector Machine/Face_image_classification.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import fetch_lfw_people
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 face_data = fetch_lfw_people(min_faces_per_person=80)
 
@@ -12,3 +14,13 @@ print('Label names: ', face_data.target_names)
 for i in range(5):
     print(f'Class {i} has {(y == i).sum()} samples.')
 
+
+fig, ax = plt.subplots(3, 4)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(face_data.images[i], cmap='bone')
+    axi.set(xticks=[], yticks=[], xlabel=face_data.target_names[face_data.target[i]])
+
+
+plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
