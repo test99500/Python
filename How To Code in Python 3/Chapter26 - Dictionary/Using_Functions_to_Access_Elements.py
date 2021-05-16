749a9456eb7a965082de7f47caba3e70050d177b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-16 15:58:32 +0800

    Committed 2021/05/16

--- How To Code in Python 3/Chapter26 - Dictionary/Using_Functions_to_Access_Elements.py
+++ How To Code in Python 3/Chapter26 - Dictionary/Using_Functions_to_Access_Elements.py
@@ -4,6 +4,22 @@ sammy = {'username': 'sammy-shark',
 
 print(sammy)
 
-# dict.keys() isolates key
+# dict.keys() isolates keys
 keys = sammy.keys()
 print(keys)
+
+# dict.values() isolates values
+values = sammy.values()
+print(values)
+
+# dict.items() returns all of the items a dictionary, in a list format of (key, value) tuple pairs.
+items = sammy.items()
+print(items)
+
+# dict.keys() can be used to query across dictionaries.
+# For example, we can take a look at the common keys shared between two dictionary data structures.
+jesse = {'username': 'JOctopus', 'online': False, 'points': 723}
+print(jesse)
+
+for common_key in sammy.keys() & jesse.keys():
+    print(sammy[common_key], jesse[common_key])
