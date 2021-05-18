5783f44aa5f350ba04934e5440b9ee1b056d2df4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-18 18:57:18 +0800

    Committed 2021/05/18

---
+++ Python Machine Learning For Beginners/Chapter9/Convolution_operation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+input_image = np.array([
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+])
+
+feature_detector = np.array([
+    [0, 0, 1],
+    [1, 0, 0],
+    [0, 1, 1]
+])
