fa63979e2485c3f53c85afa357edc1d129c4be31
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-19 23:34:30 +0800

    Committed 2021/05/19

---
+++ Supplementary materials/Matplotlib/fig, ax = plt.subplot().py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+# plt.subplots() is a function that returns a tuple containing a figure and axes object(s).
+# Thus, when using fig, ax = plt.subplots(), you unpack this tuple into the variables fig and ax.[1]
+
+# An Axes object is generated implicitly with the plotted line chart (shown in red). [2]
+
+# References:
+# 1. https://stackoverflow.com/a/34162641/14900011
+# 2. https://megalodon.jp/2021-0520-0030-00/https://towardsdatascience.com:443/what-are-the-plt-and-ax-in-matplotlib-exactly-d2cf4bf164a9
