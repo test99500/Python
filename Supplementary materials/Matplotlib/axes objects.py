50ef8b4f5eee67e2f455c196b3e703638f322831
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-19 23:41:42 +0800

    Committed 2021/05/19

--- Supplementary materials/Matplotlib/axes objects.py
+++ Supplementary materials/Matplotlib/axes objects.py
@@ -8,3 +8,10 @@ plt.savefig('axes_objects.jpg')
 
 plt.show()
 
+# when we draw a graph using plt:
+# A Figure object is generated (shown in green)
+# An Axes object is generated implicitly with the plotted line chart (shown in red)
+# All the elements of the plot such as x and y-axis are rendered inside the Axes object (shown in blue)
+
+# References:
+# 1. https://archive.ph/mWja8
