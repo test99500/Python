9486540698f685bc84b1c4b022e75bb04053f20c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-19 13:29:08 +0800

    Committed 2021/05/19

---
+++ NumPy User Guide/Basic Operations/Matrix product3.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# The matmul function implements the semantics of the @ operator introduced in Python 3.5
+
+a = np.array([[1, 0],
+              [0, 1]])
+
+b = np.array([[4, 1],
+              [2, 2]])
+
+c = np.matmul(a, b)
+
+print(c)
+
+d = a @ b
+
+print(d)
+
+# Source: https://numpy.org/doc/stable/reference/generated/numpy.matmul.html
--- Python Machine Learning by Example/Chapter8 - Artificial Neural Networks/A shallow neural network.py
+++ Python Machine Learning by Example/Chapter8 - Artificial Neural Networks/A shallow neural network.py
@@ -12,3 +12,15 @@ def train(X, y, n_hidden, learning_rate, n_iter):
     m, n_input = X.shape
 
     W1 = np.random.randn(n_input, n_hidden)
+
+    b1 = np.zeros((1, n_hidden))
+
+    W2 = np.random.randn(n_hidden, 1)
+
+    b2 = np.zeros((1, 1))
+
+    for i in range(1, n_iter + 1):
+
+        Z2 = np.matmul(X, W1) + b1
+
+        A2 =
