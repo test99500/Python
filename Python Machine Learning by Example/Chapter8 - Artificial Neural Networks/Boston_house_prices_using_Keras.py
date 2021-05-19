7b4dbefff76be46491f47baf4fc4f5ed7bb5712d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-19 23:54:48 +0800

    Committed 2021/05/19

--- Python Machine Learning by Example/Chapter8 - Artificial Neural Networks/Boston_house_prices_using_Keras.py
+++ Python Machine Learning by Example/Chapter8 - Artificial Neural Networks/Boston_house_prices_using_Keras.py
@@ -1,2 +1,17 @@
+import numpy as np
+import tensorflow as tf
+import random as python_random
+
+np.random.seed(42)
+python_random.seed(42)
+tf.random.set_seed(42)
+
+'''adapted from Supplementary materials/Keras/Obtaining reproducible results.py'''
+
+# Rest of code follows ...
 import keras
+
+model = keras.Sequential()
+
+
 
