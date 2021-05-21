9c62f810fcb6270fef9e3b6d92412d5e9b0a2ef3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-21 16:21:23 +0800

    Committed 2021/05/21

--- Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
+++ Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
@@ -4,3 +4,25 @@ import pandas as pd
 # First, we implement feature generation by starting with a sub-function that directly creates
 # features from the original six features.
 
+def add_original_feature(df, df_new):
+    df_new['open'] = df['Open']
+    df_new['open_1'] = df['Open'].shift(1)
+    df_new['close_1'] = df['Close'].shift(1)
+    df_new['high_1'] = df['High'].shift(1)
+    df_new['low_1'] = df['Low'].shift(1)
+    df_new['volume_1'] = df['Volume'].shift(1)
+
+
+# Then, we develop a sub-function that generates six features related to average close prices.
+def add_avg_price(df, df_new):
+    df_new['avg_price_5'] = df['Close'].rolling(5).mean().shift(1)
+    df_new['avg_price_30'] = df['Close'].rolling(21).mean().shift(1)
+    df_new['avg_price_365'] = df['Close'].rolling(252).mean().shift(1)
+    df_new['ration_avg_price_5_30'] = df_new['avg_price_5'] / df_new['avg_price_30']
+    df_new['ratio_avg_price_5_365'] = df_new['avg_price_5'] / df_new['avg_price_365']
+    df_new['ratio_avg_price_30_365'] = df_new['avg_price_30'] / df_new['avg_price_365']
+
+
+# Similarly, a sub-function that generates six features related to average volumes is as follows.
+def add_avg_volume(df, df_new):
+    df_new
