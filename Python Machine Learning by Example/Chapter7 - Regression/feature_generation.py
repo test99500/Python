ada7464f715193aea184e746029932ebea5aea52
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-21 19:59:55 +0800

    Committed 2021/05/21

--- Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
+++ Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
@@ -25,4 +25,24 @@ def add_avg_price(df, df_new):
 
 # Similarly, a sub-function that generates six features related to average volumes is as follows.
 def add_avg_volume(df, df_new):
-    df_new
+    df_new['avg_volume_5'] = df['Volume'].rolling(5).mean().shift(1)
+    df_new['avg_volume_30'] = df['Volume'].rolling(21).mean().shift(1)
+    df_new['avg_volume_365'] = df['Volume'].rolling(252).mean().shift(1)
+    df_new['ratio_avg_volume_5_30'] = df_new['avg_volume_5'] / df_new['avg_volume_30']
+    df_new['ration_avg_volume_5_365'] = df_new['avg_volume_5'] / df_new['avg_volume_365']
+    df_new['ratio_avg_volume_30_365'] = df_new['avg_volume_30'] / df_new['avg_volume_365']
+
+
+# As for the standard deviation, we develop the following sub-function for the price-related
+# features:
+def add_std_price(df, df_new):
+    df_new['std_price_5'] = df['Close'].rolling(5).std().shift(1)
+    df_new['std_price_30'] = df['Close'].rolling(21).std().shift(1)
+    df_new['std_price_365'] = df['Close'].rolling(252).std().shift(1)
+    df_new['ratio_std_price_5_30'] = df_new['std_price_5'] / df_new['std_price_30']
+    df_new['ratio_std_price_5_365'] = df_new['std_price_5'] / df_new['std_price_365']
+    df_new['ratio_std_price_30_365'] = df_new['std_price_30'] / df_new['std_price_365']
+
+
+# A sub-function that generates six volume-based standard deviation features is as follows:
+def add_std_volume(df, df_new):
