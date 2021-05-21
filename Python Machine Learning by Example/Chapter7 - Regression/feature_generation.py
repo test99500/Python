7bad96b774afb336051c84917461d7062531d239
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-21 16:03:33 +0800

    Committed 2021/05/21

---
+++ Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
@@ -0,0 +1,6 @@
+import Stock_price as dataset
+import pandas as pd
+
+# First, we implement feature generation by starting with a sub-function that directly creates
+# features from the original six features.
+
