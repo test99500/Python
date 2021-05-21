90a1824cd1e148cc57f57cdcd0e5cf01d0495053
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-21 22:19:03 +0800

    Committed 2021/05/21

--- Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
+++ Python Machine Learning by Example/Chapter7 - Regression/feature_generation.py
@@ -55,4 +55,45 @@ def add_std_volume(df, df_new):
 
 # 7 return-based features are generated using the following sub-function.
 def add_return_feature(df, df_new):
-    df_new['return_1'] = ((df['Close'] - df['Close'].shift(1)))
+    df_new['return_1'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
+    df_new['return_5'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
+    df_new['return_30'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
+    df_new['return_365'] = ((df['Close'] - df['Close'].shift(252)) / df['Close'].shift(252)).shift(1)
+    df_new['moving_avg_5'] = df_new['return_1'].rolling(5).mean().shift(1)
+    df_new['moving_avg_30'] = df_new['return_1'].rolling(21).mean().shift(1)
+    df_new['moving_avg_365'] = df_new['return_1'].rolling(252).mean().shift(1)
+
+
+# Finally, we put together the main feature generation function that calls all the preceding
+# sub-functions:
+def generate_features(df):
+    """
+    Generate features for a stock/index based on historical price and performance
+
+    :parameter df:
+    dataframe with columns "Open", "Close", "High", "Low", "Volume", "Adjusted close"
+
+    :return dataframe, data set with new features.
+
+    """
+    df_new = pd.DataFrame()
+
+    # 6 original features
+    add_original_feature(df, df_new)
+
+    # 31 generated features
+    add_avg_price(df, df_new)
+
+    add_avg_volume(df, df_new)
+
+    add_std_price(df, df_new)
+
+    add_avg_volume(df, df_new)
+
+    add_return_feature(df, df_new)
+
+    # the target
+    df_new['close'] = df['Close']
+    df_new = df_new.dropna(axis=0)
+
+    return df_new
