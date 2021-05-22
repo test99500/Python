95815e60dcde0f53c0317d66e872c230c9708d0f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-22 12:23:18 +0800

    Committed 2021/05/22

--- Python Machine Learning by Example/Chapter8 - Neural Networks/Training_a_simple_neural_network3.py
+++ Python Machine Learning by Example/Chapter8 - Neural Networks/Training_a_simple_neural_network3.py
@@ -1,5 +1,25 @@
 import pandas as pd
+import feature_generation
+import numpy as np
 
-data_raw = pd.read_csv('19920103_20191231.csv', index_col='Date')
+data_raw = pd.read_csv('19920103_20191231.csv', index_col='Date', thousands=',') # [1]
 print(data_raw)
 print(data_raw.info())
+
+data = feature_generation.generate_features(df=data_raw)
+
+start_train = '3-Jan-92'
+end_train = '29-Dec-18'
+start_test = '1-Jan-1'
+end_test = '31-Dec-19'
+
+data_train = data.loc[start_train:end_train]
+print(data_train)
+
+X_train = data_train.drop('close', axis=1).to_numpy()
+
+y_train = data_train['close'].to_numpy()
+
+
+# References:
+# 1. https://stackoverflow.com/a/22137890/14900011
--- Python Machine Learning by Example/Chapter8 - Neural Networks/feature_generation.py
+++ Python Machine Learning by Example/Chapter8 - Neural Networks/feature_generation.py
@@ -1,4 +1,3 @@
-import Stock_price as dataset
 import pandas as pd
 
 # First, we implement feature generation by starting with a sub-function that directly creates
