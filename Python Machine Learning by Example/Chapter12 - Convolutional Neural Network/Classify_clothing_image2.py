4ba38a2b4a6a400a1016d4e035b8bef87002f96f
Ceiling_roof <ted10014@hotmail.com>
2021-05-23 01:46:00 +0800

    Committed 2021/05/23

---
+++ Python Machine Learning by Example/Chapter12 - Convolutional Neural Network/Classify_clothing_image2.py
@@ -0,0 +1,87 @@
+from keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import datasets, layers, models, losses
+
+fashion_mnist = fashion_mnist
+
+(X_train_full, y_train_full), (test_imanges, test_labels) = fashion_mnist.load_data()
+
+# Print a few samples from these four arrays, for example, the training labels as follows:
+print(y_train_full)
+
+# The label arrays do not include class names. Hence, we define them as follows and will
+# use them for plotting later on:
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt',
+               'Sneaker', 'Bag', 'Ankle boot']
+
+# Take a look at the format of the image
+print(X_train_full.shape)
+
+# Similarly for the 10000 testing samples, we check the format as follows:
+print(test_imanges.shape)
+
+# Let's now inspect a random training sample
+plt.figure()
+plt.imshow(X_train_full[42])
+plt.colorbar()
+plt.grid(False)
+plt.title(class_names[y_train_full[42]])
+
+plt.savefig('Fashion_MNIST.jpg')
+
+plt.show()
+
+# In the ankle boot sample, the pixel values are in the range of 0 to 255.
+# Hence, we need to rescale the data to a range of 0 to 1 before feeding it to the neural network.
+X_train_full = X_train_full / 255.0
+test_images = test_imanges / 255.0
+
+# Now we display the first 16  training samples after the pre-processing
+for i in range(16):
+    plt.subplot(4, 4, i + 1) # Do not mistake subplot as subplots [1]
+    plt.subplots_adjust(hspace=.3)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+
+    plt.imshow(X_train_full[i], cmap=plt.cm.binary)
+
+    plt.title(class_names[y_train_full[i]])
+
+
+plt.savefig('Fashion_MINIST_.jpg')
+plt.show()
+
+# As the convolutional layer in Keras only takes in individual samples in three dimensions,
+# we need to first reshape the data into four dimensions:
+X_train = X_train_full.reshape((X_train_full.shape[0], 28, 28, 1))
+
+X_test = test_images.reshape((test_imanges.shape[0], 28, 28, 1))
+
+print(X_train.shape)
+
+# Before we develop the CNN model, let's specify the random speed in TensorFlow for reproducibility
+tf.random.set_seed(42)
+
+model = models.Sequential()
+
+# For the convolutional extractor, we are going to use three convolutional layers.
+
+## We start with the first convolutional layer with 32 small-sized 3 * 3 filters.
+model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(units=64, activation='relu'))
+model.add(layers.Dense(units=10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+
+print(model.summary())
+
+# Fitting/training the CNN model we just built.
+model.fit(x=X_train, y=y_train_full, epochs=10, validation_data=(X_test, test_labels))
