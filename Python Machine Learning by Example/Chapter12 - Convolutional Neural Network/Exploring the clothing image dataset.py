9ef74fb46e7abcacf15de3fedb6587437e3fe55a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-22 23:14:19 +0800

    Committed 2021/05/22

--- Python Machine Learning by Example/Chapter12 - Convolutional Neural Network/Exploring the clothing image dataset.py
+++ Python Machine Learning by Example/Chapter12 - Convolutional Neural Network/Exploring the clothing image dataset.py
@@ -1,4 +1,5 @@
 from keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
 
 fashion_mnist = fashion_mnist
 
@@ -9,3 +10,22 @@ print(train_labels)
 
 # The label arrays do not include class names. Hence, we define them as follows and will
 # use them for plotting later on:
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt',
+               'Sneaker', 'Bag', 'Ankle boot']
+
+# Take a look at the format of the image
+print(train_images.shape)
+
+# Similarly for the 10000 testing samples, we check the format as follows:
+print(test_imanges.shape)
+
+# Let's now inspect a random training sample
+plt.figure()
+plt.imshow(train_images[42])
+plt.colorbar()
+plt.grid(False)
+plt.title(class_names[train_labels[42]])
+
+plt.savefig('Fashion_MNIST.jpg')
+
+plt.show()
