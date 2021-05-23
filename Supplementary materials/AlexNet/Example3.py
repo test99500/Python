205819ac85eeeabcd6478c8d796fea928b45b3b4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-23 18:38:37 +0800

    Committed 2021/05/23

---
+++ Supplementary materials/AlexNet/Example3.py
@@ -0,0 +1,4 @@
+# Sources:
+# 1. https://archive.ph/rgm5F
+# 2. https://megalodon.jp/2021-0523-1915-46/https://medium.com:443/analytics-vidhya/multi-class-image-classification-using-alexnet-deep-learning-network-implemented-in-keras-api-c9ae7bc4c05f
+# 3. https://github.com/keshav1245/Image-Classification-using-AlexNet-CNN-Model
