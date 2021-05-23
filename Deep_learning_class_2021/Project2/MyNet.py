1efb5d00b8dd84c25819a239fd0a13d5bd9a8dc7
Ceiling_roof <ted10014@hotmail.com>
2021-05-24 05:49:01 +0800

    Committed 2021/05/23

--- Deep_learning_class_2021/Project2/MyNet.py
+++ Deep_learning_class_2021/Project2/MyNet.py
@@ -2,13 +2,22 @@ from keras.datasets import cifar10
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import cv2
+import keras
+from sklearn.metrics import classification_report
 
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
 CLASS_NAMES = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-train_pipeline = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-test_pipeline = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+labels_ = [
+    1 if 'airplane' in CLASS_NAMES else 2 if 'automobile' else 3 if 'bird' else 4 if 'cat' else 5 if 'deer' else 6 if 'dog' else 7 if 'frog' else 8 if 'horse' else 9 if 'ship' else 10]
+
+y_train_labels = labels_
+y_test_labels = labels_
+
+train_pipeline = tf.data.Dataset.from_tensor_slices((X_train, y_train_labels))
+test_pipeline = tf.data.Dataset.from_tensor_slices((X_test, y_test_labels))
 
 # Check if the data is colorful or grayscale, i.e., the number of channels.
 plt.figure(figsize=(20, 20))
@@ -37,10 +46,46 @@ test_pipeline_size = tf.data.experimental.cardinality(test_pipeline).numpy()
 
 # It's a good idea to shuffle your training data if you are training by batches,
 # especially if the batch size is small. [1]
-train_pipeline = (train_pipeline.map(process_images).batch(batch_size=32, drop_remainder=True).
+train_pipeline = (train_pipeline.map(process_images).batch(batch_size=8, drop_remainder=True).
                   shuffle(buffer_size=train_pipeline_size))
 
-test_pipeline = (test_pipeline_size)
+test_pipeline = (test_pipeline.map(process_images).batch(batch_size=8, drop_remainder=True).
+                 shuffle(buffer_size=test_pipeline_size))
+
+# The code snippet represents the Keras implementation of the AlexNet CNN architecture.
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation='relu', input_shape=(227, 227, 3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same"),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model.
+model.compile(loss='sparse_categorical_crossentropy', optimizer=tf.optimizers.SGD(lr=0.001),
+              metrics=['accuracy'])
+
+model.fit(train_pipeline, epochs=4, validation_split=0.3)
+
+y_prediction = model.predict(x=X_test)
+
+test_label = np.argmax(y_test, axis=1)
+
+print(classification_report(y_true=test_label, y_pred=np.argmax(y_prediction, axis=1)))
 
 # References:
 # 1. https://stackoverflow.com/a/41176694/14900011
