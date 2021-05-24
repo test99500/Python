dc9554b9c9860ba55d2ab3fed1318e148733bfc5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-24 09:48:39 +0800

    Committed 2021/05/24

--- Python Machine Learning For Beginners/Chapter9/Implementing_CNN_with_Keras.py
+++ Python Machine Learning For Beginners/Chapter9/Implementing_CNN_with_Keras.py
@@ -25,3 +25,24 @@ plt.grid(False)
 plt.savefig('Fashion_MNIST.jpg')
 
 plt.show()
+
+# Change the dimensions of our input images.
+# CNN in Keras expects data to be in the format Width-Height-Channel.
+# Our images contain width and height but no channels.
+# Since the images are greyscale, we set the image channel to 1.
+training_images = np.expand_dims(training_images, -1)
+test_images = np.expand_dims(test_images, -1)
+
+print(training_images.shape)
+
+# The next step is to find the number of output classes.
+# This number will be used to define the number of neurons in the output layer.
+output_classes = len(set(training_labels))
+
+print('Number of output classes is: ', output_classes)
+
+# Let's print the shape of a single image in the training set
+print(training_images[0].shape) # This shape will be used to train our convolutional neural network
+
+input_layer = Input(shape=training_images[0].shape)
+
