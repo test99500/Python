2e9511a52337bed2bc9c47ad7ef896b58dc604de
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-27 18:06:14 +0800

    Committed 2021/05/27

--- Hands on Machine Learning2/Chapter13 - TensorFlow/Chaning Transformations.py
+++ Hands on Machine Learning2/Chapter13 - TensorFlow/Chaning Transformations.py
@@ -14,3 +14,33 @@ dataset_pipeline = dataset.repeat(3).batch(batch_size=7)
 
 for item in dataset_pipeline:
     print(item)
+
+
+print(dataset_pipeline)
+
+# You can also transform the items by calling the map() method.
+# For example, this creates a new dataset with all items doubled:
+dataset3 = dataset.map(lambda x: X * 2, num_parallel_calls=2)
+
+print(dataset3)
+
+for item in dataset3:
+    print(item)
+
+
+# While map() applies a transformation to each item,
+# the apply() method applies a transformation to the dataset as a whole.
+dataset4 = dataset_pipeline.unbatch() # undo batch()
+
+print(dataset4)
+
+for item in dataset4:
+    print(item)
+
+
+# Simply filter the dataset using filter()
+dataset5 = dataset4.filter(lambda x: X < 10)  # keep only items < 10
+
+# To lok at at just a few items from a dataset, use take()
+for item in dataset5.take(3):
+    print(item)
