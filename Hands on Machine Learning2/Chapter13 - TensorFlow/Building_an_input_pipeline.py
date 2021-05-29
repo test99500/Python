6f6389e01f273d4c09dca47ca4250563ef1d3560
Ceiling_roof <ted10014@hotmail.com>
2021-05-29 20:26:01 +0800

    Committed 2021/05/29

--- Hands on Machine Learning2/Chapter13 - TensorFlow/Building_an_input_pipeline.py
+++ Hands on Machine Learning2/Chapter13 - TensorFlow/Building_an_input_pipeline.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import Split_the_California_dataset_to_multiple_CSV_files as spliter
 import tensorflow as tf
 
@@ -16,3 +18,8 @@ for line in dataset.take(5):
     print(line.numpy())
 
 
+# Notice that the field 4 of the output of parsed_field is interpreted as a string
+record_defaults = [0, np.nan, tf.constant(np.nan, dtype=tf.float64), "Hello", tf.constant([])]
+parsed_fields = tf.io.decode_csv(records='1, 2, 3, 4, 5', record_defaults=record_defaults)
+
+print(parsed_fields)
