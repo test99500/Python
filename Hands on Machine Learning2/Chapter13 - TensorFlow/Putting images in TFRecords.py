a0f17ed943e7b18b425bf03061883599014d4447
Ceiling_roof <ted10014@hotmail.com>
2021-05-30 15:28:58 +0800

    Committed 2021/05/30

--- Hands on Machine Learning2/Chapter13 - TensorFlow/Putting images in TFRecords.py
+++ Hands on Machine Learning2/Chapter13 - TensorFlow/Putting images in TFRecords.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_sample_images
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 img = load_sample_images()['images'][0]
 plt.imshow(img)
@@ -9,3 +10,5 @@ plt.title('Original Image')
 plt.savefig('TFRecords.jpg')
 
 plt.show()
+
+data = tf.io.encode_jpeg(img)
