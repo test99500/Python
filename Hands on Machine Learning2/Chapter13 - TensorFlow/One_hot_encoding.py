dc650d6150b95ee1cf3fbedc779ce9dc04c81f58
Ceiling_roof <ted10014@hotmail.com>
2021-05-30 17:10:10 +0800

    Committed 2021/05/30

---
+++ Hands on Machine Learning2/Chapter13 - TensorFlow/One_hot_encoding.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
+
+dataset = pd.read_csv(filepath_or_buffer=url, header=0)
+
+print(dataset.head())
+
+print(dataset["ocean_proximity"].unique())
