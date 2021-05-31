77a9226504fa43f69177600acb3fd4b468fa7c4d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-05-31 12:03:02 +0800

    Committed 2021/05/31

--- Supplementary materials/TensorFlow/Load_images.py
+++ Supplementary materials/TensorFlow/Load_images.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 import PIL
 import PIL.Image
 import tensorflow as tf
@@ -13,3 +14,51 @@ data_dir = tf.keras.utils.get_file(origin=dataset_url,
                                    untar=True)
 
 data_dir = pathlib.Path(data_dir)
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+
+print(image_count)
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[1]))
+
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=32)
+
+print(train_ds)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=32)
+
+print(val_ds)
+
+class_names = train_ds.class_names
+print(class_names)
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+
+
+plt.savefig('Loaded_images.jpg')
+plt.show()
