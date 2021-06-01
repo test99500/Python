99db3f8bf337663918ff75252a67778ff468a662
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-02 02:18:37 +0800

    Committed 2021/06/01

--- Python Machine Learning/Chapter13/Creating a dataset from files on my local storage disk.py
+++ Python Machine Learning/Chapter13/Creating a dataset from files on my local storage disk.py
@@ -3,9 +3,16 @@ import pathlib
 # Use the pathlib library to generate a list of image files:
 image_directory_path = pathlib.Path('cat_dog_images')
 
+for path in image_directory_path.glob('*.jpg'):
+    print(path)
 
+list = [str(path) for path in image_directory_path.glob('*.jpg')]
+print(list)
 
-# sorted(iterable sequence to sort, key=key, reverse=reverse)
-file_list = sorted([str(path) for path in image_directory_path.glob('*.jpg')])
+# sorted(iterable sequence to sort, key=key, reverse=reverse) [1]
+file_list = sorted(list)
 
 print(file_list)
+
+# References:
+# 1. https://www.w3schools.com/python/ref_func_sorted.asp
