b6e0c525c6dbe599302c2a0937119d478f8b7890
Ceiling_roof <ted10014@hotmail.com>
2021-06-01 17:05:25 +0800

    Committed 2021/06/01

--- Deep_learning_class_2021/Project3/AlexNet3.py
+++ Deep_learning_class_2021/Project3/AlexNet3.py
@@ -27,17 +27,17 @@ model = Sequential([
     Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation=tf.nn.relu,
            data_format='channels_last', input_shape=(227, 227, 3)),
     BatchNormalization(),
-    MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'),
+    MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'), # Prevent Negative dimension size caused by subtracting 3 from 2 for '{{node max_pooling2d_10/MaxPool}} [1]
     Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), activation=tf.nn.relu, padding="same"),
     BatchNormalization(),
-    MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'),
+    MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'), # [1]
     Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), activation=tf.nn.relu, padding="same"),
     BatchNormalization(),
     Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), activation=tf.nn.relu, padding="same"),
     BatchNormalization(),
     Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), activation=tf.nn.relu, padding="same"),
     BatchNormalization(),
-    MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'),
+    MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'), # [1]
     Flatten(),
     Dense(4096, activation=tf.nn.relu),
     Dropout(0.5),
@@ -51,3 +51,6 @@ model.compile(optimizer=Adam(), loss=sparse_categorical_crossentropy, metrics=['
 history = model.fit(x=training_images, y=training_labels, epochs=20, batch_size=32)
 
 model.evaluate(x=test_images, y=test_labels)
+
+# References:
+# 1. https://stackoverflow.com/questions/49079115/valueerror-negative-dimension-size-caused-by-subtracting-2-from-1-for-max-pool
