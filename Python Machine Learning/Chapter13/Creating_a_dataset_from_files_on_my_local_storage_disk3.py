73e111b91e49cd37c984d5c9df6e7ba7abccf191
Ceiling_roof <ted10014@hotmail.com>
2021-06-03 02:30:35 +0800

    Committed 2021/06/02

--- Python Machine Learning/Chapter13/Creating_a_dataset_from_files_on_my_local_storage_disk3.py
+++ Python Machine Learning/Chapter13/Creating_a_dataset_from_files_on_my_local_storage_disk3.py
@@ -23,3 +23,32 @@ dataset_of_image_and_label = tf.data.Dataset.from_tensor_slices((list_of_image_p
 for item in dataset_of_image_and_label:
     print('X:', item[0].numpy(), 'y:', item[1].numpy())
 
+def preprocessing(image, label):
+
+    loaded_image = tf.io.read_file(image)
+
+    decoded_image = tf.image.decode_image(contents=loaded_image, channels=3)
+
+    resized_image = tf.image.resize(images=decoded_image, size=[image_height, image_width])
+
+    postprocessed_image = resized_image / 255.0
+
+    return postprocessed_image, label
+
+
+image_height, image_width = 80, 120
+
+dataset_of_processed_image_and_label = dataset_of_image_and_label.map(preprocessing)
+
+figure = plt.figure(figsize=(10, 5))
+
+for i, example in enumerate(dataset_of_processed_image_and_label):
+    print(example[0].shape, example[1].numpy())
+    ax = figure.add_subplot(2, 3, i + 1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(example[0])
+    ax.set_title('{}'.format(example[1].numpy()), size=15)
+
+plt.tight_layout()
+plt.show()
