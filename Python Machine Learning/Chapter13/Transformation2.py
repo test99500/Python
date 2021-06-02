8861ea0ca5c25c4701d166fefd1d66e45d9e3210
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-02 18:44:32 +0800

    Committed 2021/06/02

--- Python Machine Learning/Chapter13/Transformation2.py
+++ Python Machine Learning/Chapter13/Transformation2.py
@@ -6,11 +6,11 @@ X = tf.random.uniform(shape=[4, 3], dtype=tf.float32)
 
 y = tf.range(4)
 
-joint_dataset = tf.data.Dataset.from_tensor_slices((X, y))
+joint_dataset = tf.data.Dataset.from_tensor_slices((y, X))
 
 print(joint_dataset)
 
 for individual_dataset in joint_dataset:
-    print('X:', individual_dataset[0].numpy(), 'y:', individual_dataset[1].numpy())
+    print('y:', individual_dataset[0].numpy(), 'X:', individual_dataset[1].numpy())
 
 
