d1142bf8ba0875b17e85a5d59b56b08523b56271
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-03 01:19:10 +0800

    Committed 2021/06/02

--- Python Machine Learning/Chapter13/Creating_a_dataset_from_files_on_my_local_storage_disk3.py
+++ Python Machine Learning/Chapter13/Creating_a_dataset_from_files_on_my_local_storage_disk3.py
@@ -13,3 +13,13 @@ for path in image_directory_path.glob('*.jpg'):
 list_of_image_paths = [str(path) for path in image_directory_path.glob('*.jpg')]
 print(list_of_image_paths)
 
+labels = [1 if 'dog' in os.path.basename(image) else 0
+          for image in list_of_image_paths]
+
+print(labels)
+
+dataset_of_image_and_label = tf.data.Dataset.from_tensor_slices((list_of_image_paths, labels))
+
+for item in dataset_of_image_and_label:
+    print('X:', item[0].numpy(), 'y:', item[1].numpy())
+
