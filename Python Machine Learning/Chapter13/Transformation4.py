04a65b79e033a549011d5ec051466f103e04d523
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-02 19:01:25 +0800

    Committed 2021/06/02

--- Python Machine Learning/Chapter13/Transformation4.py
+++ Python Machine Learning/Chapter13/Transformation4.py
@@ -18,7 +18,8 @@ for individual_dataset in joint_dataset:
 # For this, we will use the previous joint_dataset and apply feature-scaling to scale the values
 # to the range [-1, 1), as currently the values of X are in the range [0, 1) based on
 # a random uniform distribution.
-transformed_joint_dataset = joint_dataset.map(lambda (X, y): (X * 2 - 1.0, y))
+transformed_joint_dataset = joint_dataset.map(lambda X, y:  (X * 2 - 1.0, y))
+#                                           (lambda (X, y): (X * 2 - 1.0, y))
 
 for individual_dataset in transformed_joint_dataset:
     print('X:', individual_dataset[0].numpy(), 'y:', individual_dataset[1].numpy())
