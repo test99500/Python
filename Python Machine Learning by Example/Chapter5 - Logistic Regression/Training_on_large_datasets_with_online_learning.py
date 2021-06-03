27f1749cbde1b978b81bdbedba7c8170e7463a97
Ceiling_roof <ted10014@hotmail.com>
2021-06-03 17:19:37 +0800

    Committed 2021/06/03

--- Python Machine Learning by Example/Chapter5 - Logistic Regression/Training_on_large_datasets_with_online_learning.py
+++ Python Machine Learning by Example/Chapter5 - Logistic Regression/Training_on_large_datasets_with_online_learning.py
@@ -5,3 +5,19 @@ n_rows = 100000 * 11
 df = pd.read_csv(filepath_or_buffer='train.csv', nrows=n_rows)
 
 print(df)
+
+X = df.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1)
+
+print(X)
+
+X_train = X.to_numpy()
+
+print(X_train)
+
+y = df['click']
+
+print(y)
+
+y_train = df['click'].to_numpy()
+
+print(y_train)
