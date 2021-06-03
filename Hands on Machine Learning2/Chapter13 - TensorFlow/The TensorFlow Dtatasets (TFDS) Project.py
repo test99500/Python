64a773c8a30a78773d55dcdf97568c92fbc5f206
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-03 16:17:18 +0800

    Committed 2021/06/03

--- Hands on Machine Learning2/Chapter13 - TensorFlow/The TensorFlow Dtatasets (TFDS) Project.py
+++ Hands on Machine Learning2/Chapter13 - TensorFlow/The TensorFlow Dtatasets (TFDS) Project.py
@@ -4,7 +4,15 @@ dataset = tfds.load(name='cifar10')
 
 print(dataset)
 
-# Image, label = dataset['image'], dataset['label']
+cifar10_train, cifar10_test = dataset['train'], dataset['test']
 
 # You can then apply any transformation you want (typically shuffling, batching, and prefetching),
 # and you're ready to train your model.
+cifar10_train = cifar10_train.batch(128)
+cifar10_train = cifar10_train.map(lambda items : (items['image'], items['label']))
+cifar10_train = cifar10_train.prefetch(1)
+
+for images, labels in cifar10_train.take(1):
+    print(images.shape)
+    print(labels.numpy())
+
