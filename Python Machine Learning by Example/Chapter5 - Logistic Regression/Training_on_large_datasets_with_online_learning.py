8b78766a78af862d848f63f79f8df0fafe984d08
Ceiling_roof <ted10014@hotmail.com>
2021-06-03 17:34:59 +0800

    Committed 2021/06/03

--- Python Machine Learning by Example/Chapter5 - Logistic Regression/Training_on_large_datasets_with_online_learning.py
+++ Python Machine Learning by Example/Chapter5 - Logistic Regression/Training_on_large_datasets_with_online_learning.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from tensorflow.keras.optimizers import SGD
+from sklearn.linear_model import SGDClassifier
+import timeit
 
 n_rows = 100000 * 11
 
@@ -21,3 +25,16 @@ print(y)
 y_train = df['click'].to_numpy()
 
 print(y_train)
+
+enc = OneHotEncoder(handle_unknown='ignore')
+
+enc.fit(X=X_train)
+
+Stochastic_Gradient_Descent_online_learning = SGDClassifier(loss='log', penalty=None,
+                                                            fit_intercept=True, max_iter=1,
+                                                            learning_rate='constant', eta0=0.01)
+
+X_train_enc = enc.transform(X=X_train)
+
+Stochastic_Gradient_Descent_online_learning.partial_fit(X=X_train_enc.toarray(), y=y_train,
+                                                        classes=[0, 1])
