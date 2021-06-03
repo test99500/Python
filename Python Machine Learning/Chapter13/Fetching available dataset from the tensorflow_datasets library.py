31aced0420146f26057593df4d2006615e4e7071
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-03 12:37:22 +0800

    Committed 2021/06/03

--- Python Machine Learning/Chapter13/Fetching available dataset from the tensorflow_datasets library.py
+++ Python Machine Learning/Chapter13/Fetching available dataset from the tensorflow_datasets library.py
@@ -31,3 +31,10 @@ print('\n', 30*"=", '\n')
 
 print(celeba_bldr.info.citation)
 
+# Download the dataset and store it in a designated folder for all TensorFlow dataset on local disk.
+celeba_bldr.download_and_prepare()
+
+# Next, we will instantiate the dataset as follows:
+datasets = celeba_bldr.as_dataset(shuffle_files=False)
+
+print(datasets.keys())
