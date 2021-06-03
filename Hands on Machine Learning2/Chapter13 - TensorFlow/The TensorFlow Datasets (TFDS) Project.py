3279ab04537d4053587493b70df652bd9b695e4d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-03 16:18:13 +0800

    Committed 2021/06/03

--- Hands on Machine Learning2/Chapter13 - TensorFlow/The TensorFlow Datasets (TFDS) Project.py
+++ Hands on Machine Learning2/Chapter13 - TensorFlow/The TensorFlow Datasets (TFDS) Project.py
@@ -1,19 +1,18 @@
 import tensorflow_datasets as tfds
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Lambda
-import tensorflow as tf
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.losses import sparse_categorical_crossentropy
 
-dataset = tfds.load(name='cifar10', batch_size=128, as_supervised=True)
+dataset = tfds.load(name='cifar10')
 
-cifar10_train = dataset['train'].prefetch(1)
+print(dataset)
 
-model = Sequential([Flatten(input_shape=(32, 32, 3)),
-                    Lambda(lambda images: tf.cast(images, tf.float32)),
-                    Dense(units=10, activation='softmax')])
+cifar10_train, cifar10_test = dataset['train'], dataset['test']
 
-model.compile(loss=sparse_categorical_crossentropy, optimizer=SGD(learning_rate=1e-3),
-              metrics=['accuracy'])
+# You can then apply any transformation you want (typically shuffling, batching, and prefetching),
+# and you're ready to train your model.
+cifar10_train = cifar10_train.batch(128)
+cifar10_train = cifar10_train.map(lambda items : (items['image'], items['label']))
+cifar10_train = cifar10_train.prefetch(1)
 
-model.fit(x=cifar10_train, steps_per_epoch=60000 // 32, epochs=5)
+for images, labels in cifar10_train.take(1):
+    print(images.shape)
+    print(labels.numpy())
+
