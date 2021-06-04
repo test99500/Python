85420cf93a91d329c4b8279ee0ab10a2981062bc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-04 16:17:59 +0800

    Committed 2021/06/04

--- Python Machine Learning by Example/Chapter5 - Logistic Regression/Implementing logistic regression using TensorFlow.py
+++ Python Machine Learning by Example/Chapter5 - Logistic Regression/Implementing logistic regression using TensorFlow.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
 # Read in the first 6000 rows.
 df = pd.read_csv('train.csv', nrows=6000)
@@ -11,8 +13,32 @@ print(X)
 y = df['click']
 print(y)
 
-X_train = X[: 6000 * 0.9] # Use 90% of the read-in rows for training.
-y_train = y[: 6000 * 0.9]
+# X_train = X[: 6000 * 0.9] # Use 90% of the read-in rows for training.
+# y_train = y[: 6000 * 0.9]
 
-X_test = X[6000 * 0.9 :]
-y_test = y[6000 * 0.9 :]
+# X_test = X[6000 * 0.9 :]
+# y_test = y[6000 * 0.9 :]
+
+# Because Pandas Dataframe can't recognize 6000 * 0.9 in [:], we need to break down the steps.
+number_of_rows = 6000 * 0.9
+
+X_train = X[:number_of_rows]
+y_train = y[:number_of_rows]
+
+X_test = X[number_of_rows:]
+y_test = y[number_of_rows:]
+
+# Convert Pandas.Dataframe to NumPy array because Tensor is only compatible with NumPy array.
+X_train = X_train.to_numpy()
+y_train = y_train.to_numpy().astype('float32')
+
+X_test = X_test.to_numpy()
+y_test = y_test.to_numpy().astype('float32')
+
+enc = OneHotEncoder(handle_unknown='ignore')
+
+X_train_enc = enc.fit_transform(X=X_train)
+X_test_enc = enc.transform(X=X_test)
+
+# train_data = tf.data.Dataset.from_tensor_slices((X_train_enc, y_train))
+# train_data = train_data.repeat().shuffle(len(X_train_enc))
