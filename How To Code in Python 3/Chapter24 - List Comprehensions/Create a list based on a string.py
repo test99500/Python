7ca8333d1212ac89f7ea8634134d4b3ee002af56
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-04 22:05:16 +0800

    Committed 2021/06/04

---
+++ How To Code in Python 3/Chapter24 - List Comprehensions/Create a list based on a string.py
@@ -0,0 +1,13 @@
+# [letter, for letter in 'shark']
+shark_letters = [letter for letter in 'shark']
+
+print(shark_letters)
+
+# shark_letters2 = [letter for item in 'shark']
+
+shark_letters = []
+
+for letter in 'shark':
+    shark_letters.append(letter)
+
+print(shark_letters)
