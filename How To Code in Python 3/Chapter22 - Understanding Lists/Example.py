fd7d284444a253b86b6d08b169dfdf44253c601f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-04 21:30:36 +0800

    Committed 2021/06/04

---
+++ How To Code in Python 3/Chapter22 - Understanding Lists/Example.py
@@ -0,0 +1,9 @@
+sea_creatures = ['shark', 'cuttlefish', 'squid', 'mantis shrimp', 'anemone']
+print(sea_creatures)
+
+print('Sammy is a ' + sea_creatures[0])
+
+# Stride
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+print(numbers[1:11:2])
