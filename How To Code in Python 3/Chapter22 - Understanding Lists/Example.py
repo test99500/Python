164c888002cbe384a06612c9873d7446a7e309d7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-04 21:39:51 +0800

    Committed 2021/06/04

--- How To Code in Python 3/Chapter22 - Understanding Lists/Example.py
+++ How To Code in Python 3/Chapter22 - Understanding Lists/Example.py
@@ -7,3 +7,17 @@ print('Sammy is a ' + sea_creatures[0])
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 print(numbers[1:11:2])
+
+print(numbers[::3])
+
+# Concatenate two or more lists together:
+oceans = ['Pacific', 'Atlantic', 'Indian', 'Southern', 'Arctic']
+
+print(sea_creatures + oceans)
+
+sea_creatures = sea_creatures + ['yeti crab']
+print(sea_creatures)
+
+# The * operator can be used to multiply lists.
+print(sea_creatures * 2)
+print(oceans * 3)
