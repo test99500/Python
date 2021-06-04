b643f63118a03bf33591de15576ecff861a46b29
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-04 23:22:09 +0800

    Committed 2021/06/04

--- How To Code in Python 3/Chapter24 - List Comprehensions/Create a list based on a string.py
+++ How To Code in Python 3/Chapter24 - List Comprehensions/Create a list based on a string.py
@@ -5,6 +5,8 @@ print(shark_letters)
 
 # shark_letters2 = [letter for item in 'shark']
 
+# When creating a list with a for loop, the variable assigned to the list must be initialized
+# with an empty list.
 shark_letters = []
 
 for letter in 'shark':
