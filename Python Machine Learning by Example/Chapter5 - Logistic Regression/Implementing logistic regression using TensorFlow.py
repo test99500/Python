902e5a9ebc03dce94cac2dd1added5d8eedb67ad
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-04 16:56:54 +0800

    Committed 2021/06/04

--- Python Machine Learning by Example/Chapter5 - Logistic Regression/Implementing logistic regression using TensorFlow.py
+++ Python Machine Learning by Example/Chapter5 - Logistic Regression/Implementing logistic regression using TensorFlow.py
@@ -25,14 +25,21 @@ y_train = y_train.to_numpy().astype('float32')
 
 print(X_train)
 
-X_test = X_test.to_numpy()
+X_test = X_test.to_numpy()  # We cannot convert the array containing words to float32.
 y_test = y_test.to_numpy().astype('float32')
 
 enc = OneHotEncoder(handle_unknown='ignore')
 
 X_train_enc = enc.fit_transform(X=X_train)
+print(X_train_enc)  # It hasn't been enclosed in an array.
+
+X_train_enc = enc.fit_transform(X=X_train).toarray().astype('float32') # Enclosing it within an array and convert the encoded words to float32.
+print(X_train_enc)
+
 X_test_enc = enc.transform(X=X_test)
 
-# train_data = tf.data.Dataset.from_tensor_slices((X_train_enc, y_train))
-# train_data = train_data.repeat().shuffle(len(X_train_enc))
+print(len(X_train_enc))
+
+train_data = tf.data.Dataset.from_tensor_slices((X_train_enc, y_train))
+train_data = train_data.repeat().shuffle(len(X_train_enc)).batch(batch_size=1000).prefetch(buffer_size=1)
 
