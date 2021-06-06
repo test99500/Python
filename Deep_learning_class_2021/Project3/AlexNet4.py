052892e507b4709b0672f217cd6ec7affaf49259
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-06 14:47:02 +0800

    Committed 2021/06/06

---
+++ Deep_learning_class_2021/Project3/AlexNet4.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import categorical_crossentropy, sparse_categorical_crossentropy
+from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPool2D, Flatten, Dense, Dropout
+from sklearn.metrics import classification_report
+import numpy as np
+
+(ds_train, ds_test), ds_info = tfds.load(
+    name='cifar10',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
