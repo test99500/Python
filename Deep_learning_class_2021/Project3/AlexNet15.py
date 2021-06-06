f335627b03b4c0472d906b35c5f4f68d1a3fa7d9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-06 21:19:03 +0800

    Committed 2021/06/06

--- Deep_learning_class_2021/Project3/AlexNet15.py
+++ Deep_learning_class_2021/Project3/AlexNet15.py
@@ -55,7 +55,7 @@ print(ds_test)
 # Plug the input pipeline into Keras.
 model = Sequential([
     Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation=tf.nn.relu,
-           data_format='channels_last', input_shape=(227, 227, 3)),
+           data_format='channels_last', input_shape=(227, 227, 1)),
     BatchNormalization(),
     MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'),
     Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), activation=tf.nn.relu, padding="same"),
