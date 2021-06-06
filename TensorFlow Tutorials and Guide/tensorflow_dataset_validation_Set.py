d5d31e1e685f09b41f030e48dad379b7c34711bd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-06 23:48:58 +0800

    Committed 2021/06/06

--- TensorFlow Tutorials and Guide/tensorflow_dataset_validation_Set.py
+++ TensorFlow Tutorials and Guide/tensorflow_dataset_validation_Set.py
@@ -6,5 +6,22 @@ test_set = tfds.load("cifar10", split=test_split, as_supervised=True)
 valid_set = tfds.load("cifar10", split=valid_split, as_supervised=True)
 train_set = tfds.load("cifar10", split=train_split, as_supervised=True)
 
+label_train = []  # [1]
+for image, label in tfds.as_numpy(train_set):
+    label_train.append(label)
+
+label_test = []
+for image, label in tfds.as_numpy(test_set):
+    label_test.append(label)
+
+label_validation = []
+for image, label in tfds.as_numpy(valid_set):
+    label_validation.append(label)
+
+
+print(label_train)
+print(label_test)
+print(label_validation)
+
 # Reference:
 # 1. https://github.com/tensorflow/datasets/issues/291#issuecomment-474668134
