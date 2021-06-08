320b1351db016f455ab028005a3d2147a1294f26
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-08 18:32:03 +0800

    Committed 2021/06/08

--- Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting ad click-through with a decision tree.py
+++ Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting ad click-through with a decision tree.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 the_number_of_rows_to_read = 300000
 dataset = pd.read_csv(filepath_or_buffer='train.csv', nrows=the_number_of_rows_to_read)
@@ -7,4 +8,20 @@ print(dataset.info())
 print(dataset.head(50))
 print(dataset.tail(50))
 print(dataset.describe())
+
+y = dataset.filter(['click'])
+
+print(y)
+
+X = dataset.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1)
+
+print(X)
+print(X.shape)
+
+the_number_of_training_samples = int(the_number_of_rows_to_read * 0.9)
+
+X_train = X[:the_number_of_training_samples]
+y_train = y[:the_number_of_training_samples]
+X_test = X[the_number_of_training_samples:]
+y_test = y[the_number_of_training_samples:]
 
