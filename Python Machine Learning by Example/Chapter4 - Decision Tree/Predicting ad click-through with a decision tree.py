d3ba6d125a3708b0bf8c3373a5d7754b6b22bbdb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-08 18:58:32 +0800

    Committed 2021/06/08

--- Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting ad click-through with a decision tree.py
+++ Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting ad click-through with a decision tree.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
 
 the_number_of_rows_to_read = 300000
 dataset = pd.read_csv(filepath_or_buffer='train.csv', nrows=the_number_of_rows_to_read)
