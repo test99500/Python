0d67769b9d5a0903a19a06ed70a66a55c61f4469
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-08 17:41:44 +0800

    Committed 2021/06/08

--- Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting_ad_click_through_with_a_decision_tree.py
+++ Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting_ad_click_through_with_a_decision_tree.py
@@ -4,3 +4,12 @@ the_number_of_rows_to_read = 30000
 dataset = pd.read_csv(filepath_or_buffer='train.csv', nrows=the_number_of_rows_to_read)
 
 print(dataset.head(5))
+
+y = dataset.filter(['click'])
+
+print(y)
+
+X = dataset.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1)
+
+print(X)
+print(X.shape)
