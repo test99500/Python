e9e29e26997159e89c9d3e7fe425de2e8961a768
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-09 00:16:38 +0800

    Committed 2021/06/08

---
+++ Python Machine Learning by Example/Chapter4 - Decision Tree/Predicting ad click-through with a decision tree 1_.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+the_number_of_rows_to_read = 300000
+dataset = pd.read_csv(filepath_or_buffer='train.csv', nrows=the_number_of_rows_to_read)
+
+print(dataset.info())
+print(dataset.head(50))
+print(dataset.tail(50))
+print(dataset.describe())
+
+y = dataset['click']
+
+print(y)
+print((y == 1).sum())
+
+X = dataset.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1)
+
+print(X)
+print(X.shape)
+
+the_number_of_training_samples = int(the_number_of_rows_to_read * 0.9)
+
+X_train = X[:the_number_of_training_samples]
+y_train = y[:the_number_of_training_samples]
+X_test = X[the_number_of_training_samples:]
+y_test = y[the_number_of_training_samples:]
+
+enc = OneHotEncoder(handle_unknown='ignore')
+
+X_train_enc = enc.fit_transform(X=X_train)
+
+print(type(X_train_enc))
+
+print(X_train_enc[0])
+
+X_test = enc.transform(X=X_test)
+
+# Options for the maximal depth:
+parameters = {'max_depth': [3, 10, None]}
+
+decision_tree = DecisionTreeClassifier(criterion='gini', min_samples_split=30)
+
+grid_search = GridSearchCV(estimator=decision_tree, param_grid=parameters, cv=3,
+                           scoring='roc_auc', n_jobs=-1)
+
+grid_search.fit(X=X_train, y=y_train)
+
+print(grid_search.best_params_)
