e6c68d520a6bbfd48cef3588457555ed48562ff2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-09 20:08:40 +0800

    Committed 2021/06/09

--- Python Machine Learning/Chapter14/Solving an XOR classification problem.py
+++ Python Machine Learning/Chapter14/Solving an XOR classification problem.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 tf.random.set_seed(1)
-np.random.set_state(1)
+np.random.seed(1)
 
 X = np.random.uniform(low=-1, high=1, size=(200, 2))
 print(X)
