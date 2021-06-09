3f3fbf50a394a54f7bc45c7c4db5f737fff9c69d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-09 19:57:52 +0800

    Committed 2021/06/09

---
+++ Python Machine Learning/Chapter14/Solving an XOR classification problem.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+tf.random.set_seed(1)
+np.random.set_state(1)
+
+X = np.random.uniform(low=-1, high=1, size=(200, 2))
+print(X)
+
+y = np.ones(len(X))
+print(y)
