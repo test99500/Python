10a3b0fcdb9f8f01e8c32f5ae90f219178b23ff3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-10 15:46:14 +0800

    Committed 2021/06/10

--- Machine Learning for Finance/Chapter4 - Time Series/LSTM.py
+++ Machine Learning for Finance/Chapter4 - Time Series/LSTM.py
@@ -1,7 +1,9 @@
-from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import LSTM, SimpleRNN, Dense
 from tensorflow.keras.models import Sequential
 
 number_of_features = 29
 max_length = 100
 
-model = Sequential([LSTM(units=16, input_shape=)])
+model = Sequential([LSTM(units=16, input_shape=(max_length, number_of_features)),
+                    Dense(units=1)])
+
