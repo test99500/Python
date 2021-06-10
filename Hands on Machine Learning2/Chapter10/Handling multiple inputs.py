47358b7ee3a6b98b5bf30246f807568e315d5d29
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-10 11:09:58 +0800

    Committed 2021/06/10

--- Hands on Machine Learning2/Chapter10/Handling multiple inputs.py
+++ Hands on Machine Learning2/Chapter10/Handling multiple inputs.py
@@ -7,7 +7,7 @@ input_B = tf.keras.layers.Input(shape=[6], name='deep_input')
 
 hidden1 = tf.keras.layers.Dense(units=30, activation='relu')(input_B)
 hidden2 = tf.keras.layers.Dense(units=30, activation='relu')(hidden1)
-concatenation = tf.keras.layers.Concatenate([input_A, input_B])
+concatenation = tf.keras.layers.Concatenate([input_A, hidden2])
 output = tf.keras.layers.Dense(units=1, name='output')(concatenation)
 
 model = tf.keras.Model(inputs=[input_A, input_B], outputs=[output])
