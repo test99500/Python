3057ae8b834908a93c9c3010c036f69232190e65
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-10 11:41:16 +0800

    Committed 2021/06/10

--- Python Machine Learning/Chapter16 - Recurrent Neural Network/Hidden-recurrence versus output-recurrence.py
+++ Python Machine Learning/Chapter16 - Recurrent Neural Network/Hidden-recurrence versus output-recurrence.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.layers import SimpleRNN
+import numpy as np
 
 tf.random.set_seed(1)
 
@@ -7,8 +8,37 @@ rnn_layer = SimpleRNN(units=2, use_bias=True, return_sequences=True)
 
 rnn_layer.build(input_shape=(None, None, 5))
 
-w_xh, w_oo, b_h = rnn_layer.weights
+weight_xhidden, w_oo, bias_hidden = rnn_layer.weights
 
-print('W_xh shape:', w_xh.shape)
+print('W_xh shape:', weight_xhidden.shape)
 print('W_oo shape:', w_oo.shape)
-print('b_h shape:', b_h.shape)
+print('b_h shape:', bias_hidden.shape)
+print()
+
+X_sequence = tf.convert_to_tensor([[1.0] * 5, [2.0] * 5, [3.0] * 5], dtype=tf.float32)
+
+# Output of simple RNN
+output = rnn_layer(tf.reshape(X_sequence, shape=(1, 3, 5)))
+
+# Manually computing the output
+out_manually = []
+for t in range(len(X_sequence)):
+    xt = tf.reshape(X_sequence[t], (1, 5))
+
+    print('Time step{}=>'.format(t))
+    print('Input :', xt.numpy())
+    ht = tf.matmul(xt, weight_xhidden) + bias_hidden
+    print('Hidden:', ht.numpy())
+
+    if t > 0:
+        prev_o = out_manually[t-1]
+    else:
+        prev_o = tf.zeros(shape=(ht.shape))
+
+    ot = ht + tf.matmul(prev_o, w_oo)
+    ot = tf.math.tanh(x=ot)
+    out_manually.append(ot)
+
+    print('Output (manual):', ot.numpy())
+    print('SimpleRNN output:'.format(t), output[0][t].numpy())
+    print()
