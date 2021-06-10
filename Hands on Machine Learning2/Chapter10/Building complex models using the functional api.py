25db3dae75f322f225b6bc6c41826c851ad2a871
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-10 10:45:02 +0800

    Committed 2021/06/10

--- Hands on Machine Learning2/Chapter10/Building complex models using the functional api.py
+++ Hands on Machine Learning2/Chapter10/Building complex models using the functional api.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 input = tf.keras.layers.Input(shape=X_train.shape[1:])
 hidden1 = tf.keras.layers.Dense(units=30, activation='relu')(input)
 hidden2 = tf.keras.layers.Dense(units=30, activation='relu')(hidden1)
-concatenation = tf.keras.layers.Concatenate()([input, hidden2])
+concatenation = tf.keras.layers.Concatenate([input, hidden2])
 output = tf.keras.layers.Dense(units=1)
 
 model = tf.keras.Model(inputs=[input], outputs=[output])
--- Hands on Machine Learning2/Chapter10/Handling multiple inputs.py
+++ Hands on Machine Learning2/Chapter10/Handling multiple inputs.py
@@ -1,2 +1,13 @@
 import tensorflow as tf
 
+# You should name at least the most important layers, especially when the model gets
+# a bit complex like this.
+input_A = tf.keras.layers.Input(shape=[5], name='wide_input')
+input_B = tf.keras.layers.Input(shape=[6], name='deep_input')
+
+hidden1 = tf.keras.layers.Dense(units=30, activation='relu')(input_B)
+hidden2 = tf.keras.layers.Dense(units=30, activation='relu')(hidden1)
+concatenation = tf.keras.layers.Concatenate([input_A, input_B])
+output = tf.keras.layers.Dense(units=1, name='output')(concatenation)
+
+model = tf.keras.Model(inputs=[input_A, input_B], outputs=[output])
