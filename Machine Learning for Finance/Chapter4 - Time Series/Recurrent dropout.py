1c31527eb80e84ced05853b21dd9c22c65a42ed0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-10 16:13:02 +0800

    Committed 2021/06/10

--- Machine Learning for Finance/Chapter4 - Time Series/Recurrent dropout.py
+++ Machine Learning for Finance/Chapter4 - Time Series/Recurrent dropout.py
@@ -6,9 +6,10 @@ import Conv1D_2 as eyes_meet
 number_of_features = 29
 max_length = 100
 
-model = Sequential([LSTM(units=32, input_shape=(max_length, number_of_features),
-                         return_sequences=True),
-                    SimpleRNN(units=16, return_sequences=True)])
+model = Sequential([LSTM(units=16, recurrent_dropout=0.1, return_sequences=True,
+                         input_shape=(max_length, number_of_features)),
+                    LSTM(units=16, recurrent_droput=0.1),
+                    Dense(units=1)])
 
 model.compile(optimizer='adam', loss=mean_absolute_percentage_error)
 
@@ -16,4 +17,3 @@ model.fit_generator(eyes_meet.train_gen, epochs=1,
                     steps_per_epoch=eyes_meet.n_train_samples // eyes_meet.batch_size,
                     validation_data=eyes_meet.val_gen,
                     validation_steps=eyes_meet.n_train_samples // eyes_meet.batch_size)
-
