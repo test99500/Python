40e15c342e022b580bd50f4d0c647453b8da9ec7
Ceiling_roof <ted10014@hotmail.com>
2021-06-12 13:47:36 +0800

    Committed 2021/06/12

--- Python Machine Learning/Chapter16 - Recurrent Neural Network/Predicting the sentiment of IMDB movie reviews.py
+++ Python Machine Learning/Chapter16 - Recurrent Neural Network/Predicting the sentiment of IMDB movie reviews.py
@@ -9,8 +9,16 @@ print(dataframe)
 target = dataframe.pop('sentiment')
 print(target)
 
+# DataFrame.pop(item)
+#   Return item and drop from frame. [1]
+
+print(dataframe)  # Column ['sentiment'] is no longer present.
+
 target2 = dataframe.filter(['sentiment'], axis=1)
 print(target2)
 
 target3 = dataframe['sentiment']
 print(target3)
+
+# Reference:
+# 1. https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.pop.html
