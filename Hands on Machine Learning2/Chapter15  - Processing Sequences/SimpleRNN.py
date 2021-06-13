c1d14920588f2daa344ed356df9fcfe1ab978114
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-14 01:14:24 +0800

    Committed 2021/06/13

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/SimpleRNN.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/SimpleRNN.py
@@ -2,6 +2,7 @@ import Time_Series_Generator as time
 
 number_of_steps = 50
 series = time.generate_time_series(batch_size=10000, number_of_steps=number_of_steps + 1)
-X_train, y_train = series[:7000, :number_of_steps], series[:7000, -1]
 
-X_valid, y_valid = series[7000:9000, :number_of_steps]
+X_train, y_train = series[:7000, :number_of_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :number_of_steps], series[7000:9000, -1]
+X_test, y_test   = series[9000:, :number_of_steps], series[9000:, -1]
