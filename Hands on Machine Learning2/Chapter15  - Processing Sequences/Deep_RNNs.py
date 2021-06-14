62bf782b0ecd06a4ecc7bc99f797aabecada00be
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-14 14:19:17 +0800

    Committed 2021/06/14

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/Deep_RNNs.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/Deep_RNNs.py
@@ -1,3 +1,4 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import SimpleRNN
 
+model = Sequential([SimpleRNN(units=20, return_sequences=True, input_shape=[None, 1])])
