ba50efa9575a2c24015c30c1022587aa68a9cef0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-15 19:30:49 +0800

    Committed 2021/06/15

--- Soft Computing 2021/Particle Swarm Optimization with NN.py
+++ Soft Computing 2021/Particle Swarm Optimization with NN.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-model = Sequential([Dense(units=3, activation='sigmoid', use_bias=True)])
+model = Sequential([Dense(units=2, activation='sigmoid', use_bias=True),
+                    Dense(units=1, use_bias=True, bias_initializer=1.0)])
