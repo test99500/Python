1864e299864d6526bb08d9ad855f411399988b5e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-17 23:59:23 +0800

    Committed 2021/06/17

--- Python Machine Learning/Chapter12/Multilayer_Perceptron.py
+++ Python Machine Learning/Chapter12/Multilayer_Perceptron.py
@@ -5,7 +5,34 @@ class NeuralNetMLP(object):
     """Feedforward neural network / Multi-layer perceptron classifier.
 
     :parameter
-    number_of_hidden : int(default:30)
 
+    number_of_hidden_units : int (default:30)
+        Number of hidden units.
+
+    l2 : float (default: 0.)
+        Lambda value for L2-regularization.
+        No regularization if l2 = 0 (default)
+
+    epochs : int (default: 100)
+        Number of passes over the training set.
+
+    eta : float (default: 0.001)
+        Learning rate.
+
+    shuffle : bool (default: True)
+        Shuffles training data every epoch if true to prevent circles.
+
+    minibatch_size : int (default: 1)
+        Number of training examples per minibatch.
+
+    seed : int (default: None)
+        Random seed for initializing weights and shuffling.
+
+    Attributes
+    --------------------
+    eval_ : dict
+        Dictionary collecting the cost, training accuracy,
+        and validation accuracy for each epoch during training.
 
     """
+
