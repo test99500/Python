5ad29b0f056da18ba3b7e24ffe5a50df69382c85
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-17 23:30:32 +0800

    Committed 2021/06/17

---
+++ Python Machine Learning/Chapter12/Multilayer_Perceptron.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sys
+
+class NeuralNetMLP(object):
+    """Feedforward neural network / Multi-layer perceptron classifier.
+
+    :parameter
+    number_of_hidden : int(default:30)
+
+
+    """
