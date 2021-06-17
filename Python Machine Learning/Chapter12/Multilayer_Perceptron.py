c3e895567bac91d94ed86c22d02893242c935b3e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-18 01:01:50 +0800

    Committed 2021/06/17

--- Python Machine Learning/Chapter12/Multilayer_Perceptron.py
+++ Python Machine Learning/Chapter12/Multilayer_Perceptron.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 
+
 class NeuralNetMLP(object):
     """Feedforward neural network / Multi-layer perceptron classifier.
 
@@ -36,3 +37,61 @@ class NeuralNetMLP(object):
 
     """
 
+    def __init__(self, number_of_hidden_units=30, l2=0., epochs=100, eta=0.001,
+                 shuffle=True, minibatch_size=1, seed=None):
+        self.random = np.random.RandomState(seed)
+        self.n_hidden = number_of_hidden_units
+        self.l2 = l2
+        self.epochs = epochs
+        self.eta = eta
+        self.shuffle = shuffle
+        self.minibatch_size = minibatch_size
+
+    def _sigmoid(self, z):
+        """Compute logistic function (sigmoid)"""
+
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def _forward(self, X):
+        """Compute forward propagation step"""
+
+        # step 1: net input of hidden layer
+        # [n_examples, n_features] dot [n_features, n_hidden]
+        # -> [n_examples, n_hidden]
+        z_h = np.dot(X, self.w_h) + self.b_h
+
+        # step 2: activation of hidden layer
+        a_h = self._sigmoid(z_h)
+
+        # step 3: net input of output layer
+        # [n_examples, n_hidden] dot [n_hidden, n_classlabels]
+        # -> [n_examples, n_classlabels]
+
+        z_out = np.dot(a_h, self.w_out) + self.b_out
+
+        # step 4: activation output layer
+        a_out = self._sigmoid(z_out)
+
+        return z_h, a_h, z_out, a_out
+
+
+    def _compute_cost(self, y_enc, output):
+        """Compute cost function.
+        Parameters
+        ----------
+        y_enc : array, shape = (n_examples, n_labels)
+            one-hot encoded class labels.
+        output : array, shape = [n_examples, n_output_units]
+            Activation of the output layer (forward propagation)
+        Returns
+        ---------
+        cost : float
+            Regularized cost
+        """
+        L2_term = (self.l2 *
+                   (np.sum(self.w_h ** 2.) +
+                    np.sum(self.w_out ** 2.)))
+
+        term1 = -y_enc * (np.log(output))
+        term2 = (1. - y_enc) * np.log(1. - output)
+        cost = np.sum(term1 - term2) + L2_term
