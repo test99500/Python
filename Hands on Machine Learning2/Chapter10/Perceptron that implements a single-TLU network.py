7e4c65fcd9bbd8af4ee700da0b25449179ca80d3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-18 14:59:56 +0800

    Committed 2021/06/18

--- Hands on Machine Learning2/Chapter10/Perceptron that implements a single-TLU network.py
+++ Hands on Machine Learning2/Chapter10/Perceptron that implements a single-TLU network.py
@@ -4,3 +4,13 @@ from sklearn.linear_model import Perceptron
 
 iris = load_iris()
 print(iris)
+
+print('\n', 30*"=", '\n')
+
+X = iris['data']
+print(X)
+
+print('\n', 30*"=", '\n')
+
+X1 = iris.data[:, (2, 3)]
+print(X1)
