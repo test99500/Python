91ff65238e0467352025a6849217e8932b926f21
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-18 15:28:15 +0800

    Committed 2021/06/18

--- Hands on Machine Learning2/Chapter10/Perceptron that implements a single-TLU network.py
+++ Hands on Machine Learning2/Chapter10/Perceptron that implements a single-TLU network.py
@@ -14,3 +14,17 @@ print('\n', 30*"=", '\n')
 
 X1 = iris.data[:, (2, 3)]
 print(X1)
+
+print('\n', 30*'=', '\n')
+
+y = iris.target
+print(y)
+
+print('\n', 30*'=', '\n')
+
+per_clf = Perceptron()
+
+per_clf.fit(X=X1, y=y)
+
+y_prediction = per_clf.predict(X=[[2, 0.5]])
+print(y_prediction)
