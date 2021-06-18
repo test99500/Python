e190dae74435e161c75a5b904fcd5f157be9a152
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-18 20:37:55 +0800

    Committed 2021/06/18

--- Soft Computing 2021/ANN.py
+++ Soft Computing 2021/ANN.py
@@ -20,3 +20,13 @@ def fit(X, y, number_of_units_in_the_hidden_layer, iteration, velocity):
     weight_a02_a13 = np.random.rand()
     weight_a12_a13 = np.random.rand()
     weight_a22_a13 = np.random.rand()
+
+    for i in range(1, iteration + 1):
+        input_a12  = (X1 * weight_X1_a12) + (X2 * weight_X2_a12) + (X0 * weight_X0_a12)
+        output_a12 = sigmoid(input_a12)
+
+        input_a22  = (X1 * weight_X1_a22) + (X2 * weight_X2_a22) + (X0 * weight_X0_a22)
+        output_a22 = sigmoid(input_a22)
+
+        input_a13  = (a02 * weight_a02_a13) + (output_a12 * weight_a12_a13) + (output_a22 * weight_a22_a13)
+        output_a13 = input_a13
