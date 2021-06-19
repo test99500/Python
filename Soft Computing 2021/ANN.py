de02d7e353de31c687423ffcf3a81881df90f1b4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-19 14:06:34 +0800

    Committed 2021/06/19

--- Soft Computing 2021/ANN.py
+++ Soft Computing 2021/ANN.py
@@ -1,32 +1,46 @@
 import numpy as np
 
-def sigmoid(z):
-    return 1.0 / (1 + np.exp(-z))
+
+class Particle_Swarm_Optimization(object):
+
+    def __init__(self, number_of_units_in_the_hidden_layer=2, l2=0., iterations=100, velocity=0.001,
+                 shuffle=True, minibatch_size=1, seed=None):
+        self.random = np.random.RandomState(seed)
+        self.number_of_hidden_units = number_of_units_in_the_hidden_layer
+        self.l2 = l2
+        self.iterations = iterations
+        self.velocity = velocity
+        self.shuffle = shuffle
+        self.minibatch_size = minibatch_size
+
+    def sigmoid(self, z):
+        return 1.0 / (1 + np.exp(-z))
 
 
-def fit(X, y, number_of_units_in_the_hidden_layer, iteration, velocity):
-    X0 = 1
-    X1 = X[0]
-    X2 = X[1]
+    def forward(self, X):
+        X0 = 1
+        X1 = X[0]
+        X2 = X[1]
 
-    weight_X0_a12 = np.random.rand()
-    weight_X0_a22 = np.random.rand()
-    weight_X1_a12 = np.random.rand()
-    weight_X1_a22 = np.random.rand()
-    weight_X2_a12 = np.random.rand()
-    weight_X2_a22 = np.random.rand()
+        weight_X0_a12 = np.random.rand()
+        weight_X0_a22 = np.random.rand()
+        weight_X1_a12 = np.random.rand()
+        weight_X1_a22 = np.random.rand()
+        weight_X2_a12 = np.random.rand()
+        weight_X2_a22 = np.random.rand()
 
-    a02 = 1
-    weight_a02_a13 = np.random.rand()
-    weight_a12_a13 = np.random.rand()
-    weight_a22_a13 = np.random.rand()
+        a02 = 1
+        weight_a02_a13 = np.random.rand()
+        weight_a12_a13 = np.random.rand()
+        weight_a22_a13 = np.random.rand()
 
-    for i in range(1, iteration + 1):
-        input_a12  = (X1 * weight_X1_a12) + (X2 * weight_X2_a12) + (X0 * weight_X0_a12)
-        output_a12 = sigmoid(input_a12)
+        for i in range(1, self.iterations + 1):
+            input_a12 = (X1 * weight_X1_a12) + (X2 * weight_X2_a12) + (X0 * weight_X0_a12)
+            output_a12 = self.sigmoid(input_a12)
 
-        input_a22  = (X1 * weight_X1_a22) + (X2 * weight_X2_a22) + (X0 * weight_X0_a22)
-        output_a22 = sigmoid(input_a22)
+            input_a22 = (X1 * weight_X1_a22) + (X2 * weight_X2_a22) + (X0 * weight_X0_a22)
+            output_a22 = self.sigmoid(input_a22)
 
-        input_a13  = (a02 * weight_a02_a13) + (output_a12 * weight_a12_a13) + (output_a22 * weight_a22_a13)
-        output_a13 = sigmoid(input_a13)
+            input_a13 = (a02 * weight_a02_a13) + (output_a12 * weight_a12_a13) + (output_a22 * weight_a22_a13)
+            output_a13 = self.sigmoid(input_a13)
+
