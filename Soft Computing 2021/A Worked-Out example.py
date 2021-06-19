669653ab4135a910131731ccf9ca0a5125606e21
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-19 19:24:43 +0800

    Committed 2021/06/19

--- Soft Computing 2021/A Worked-Out example.py
+++ Soft Computing 2021/A Worked-Out example.py
@@ -3,26 +3,50 @@ import numpy as np
 np.random.seed(1)
 
 # A swarm with two particles
-swarm = np.random.uniform(low=-5, high=5, size=[2, 9])
+swarm = np.random.uniform(low=-5, high=5, size=[5, 2])
 
 print(swarm)
 print(swarm.shape)
 
+# Time step
+t = 0
+
+c1 = c2 = 2
+
 # Position matrix X
-X = swarm.copy()
-print(X[0])
+position = swarm.copy()
+print(position[0])
+
+pbest = position
 
 np.random.seed(12)
 
 # Velocity corresponding to a particle
-Velocity = np.random.uniform(low=-5, high=5, size=[2, 9])
-print(Velocity)
+v = np.random.uniform(low=-5, high=5, size=[5, 2])
+print(v)
 
-def sigmoid(z):
-    return 1.0 / (1 + np.exp(-z))
+def objective_function(x):
+
+    fitness = []
+    gbest = [1, 2]
+
+    for i in range(0, len(x), 1):
+        y = (x[i][0] ** 2) + (x[i][1] ** 2)
+        fitness.append(y)
+
+    for j in range(0, len(fitness), 1):
+        if min(fitness) == fitness[j]:
+            gbest = [x[j, 0], x[j, 1]]
+
+    return fitness, gbest
 
 
-fitness = []
+fitness, gbest = objective_function(x=position)
 
+# Velocity update
+v[t+1] = v[t] + c1 * np.random.uniform() * (pbest[t] - position[t]) + c2 * np.random.uniform() * \
+         (gbest[t] - position[t])
 
+# Position update
+position[t+1] = position[t] + v[t]
 
