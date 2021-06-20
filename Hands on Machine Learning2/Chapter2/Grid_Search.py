1b37aeee99f7b3bcbda2c06830c144f44c87f0f3
Ceiling_roof <ted10014@hotmail.com>
2021-06-20 13:36:26 +0800

    Committed 2021/06/20

--- Hands on Machine Learning2/Chapter2/Grid_Search.py
+++ Hands on Machine Learning2/Chapter2/Grid_Search.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedShuffleSplit
 
 url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
 
@@ -15,3 +16,31 @@ print(housing.describe())
 housing.hist(bins=50, figsize=(20, 15))
 plt.savefig('histogram for numeric attribute.jpg')
 plt.show()
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+print(test_set.head())
+
+housing["median_income"].hist()
+plt.savefig('histogram for a column.jpg')
+plt.show()
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+print(housing["income_cat"].value_counts())
+
+housing["income_cat"].hist()
+plt.savefig('histogram for column income_cat.jpg')
+plt.show()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+    print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+    print(housing["income_cat"].value_counts() / len(housing))
+
+
+
