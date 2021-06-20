01b9a75087b875feb6281ceca6783e9686ba7273
Ceiling_roof <ted10014@hotmail.com>
2021-06-20 12:26:19 +0800

    Committed 2021/06/20

--- Hands on Machine Learning2/Chapter2/Grid_Search.py
+++ Hands on Machine Learning2/Chapter2/Grid_Search.py
@@ -2,17 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
 
 housing = pd.read_csv(filepath_or_buffer=url, header=0)
 
-X_train, X_test, y_train, y_test = train_test_split(housing, test_size=0.2, random_state=42)
+print(housing)
+print(housing.info())
+print(housing.describe())
 
-print(X_train)
-
-print(X_test)
-
-print(y_train)
-
-print(y_test)
+housing.hist(bins=50, figsize=(20, 15))
+plt.savefig('histogram for numeric attribute.jpg')
+plt.show()
