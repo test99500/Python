b3b57629cf666b6cd34f86f7175e32f85ff85015
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 01:14:47 +0800

    Committed 2021/06/22

--- Genetic Algorithms with Python/guessPassword.py
+++ Genetic Algorithms with Python/guessPassword.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 geneSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.'
 
@@ -15,3 +16,14 @@ def generate_parent(length):
 
 def get_fitness(guess):
     return sum(1 for expected, actual in zip(target, guess) if expected == actual)
+
+
+def mutate(parent):
+    index = random.randrange(start=0, stop=len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet, 2)
+    childGenes[index] = alternate if newGene == childGenes[index] else newGene
+    return ''.join(childGenes)
+
+
+
