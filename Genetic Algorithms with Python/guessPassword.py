d8bac07af291aac460911bbae34c2a9b649ba9b8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 00:35:12 +0800

    Committed 2021/06/22

--- Genetic Algorithms with Python/guessPassword.py
+++ Genetic Algorithms with Python/guessPassword.py
@@ -1,1 +1,13 @@
+import random
+
 geneSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.'
+
+target = 'Hello World!'
+
+def generate_parent(length):
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(geneSet))
+
+        genes.extend(random.sample(geneSet, sampleSize))
+        return ''.join(genes)
