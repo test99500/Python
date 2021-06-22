1e97462a54380d35dd84cfb6a8f5bc1f3cc57d0b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 19:32:39 +0800

    Committed 2021/06/22

---
+++ Supplementary materials/Python/min().py
@@ -0,0 +1,8 @@
+# Return the item in a tuple with the lowest value:
+a = (1, 5, 3, 9)
+x = min(a)
+print(x)
+
+# Return the name with the lowest value, ordered alphabetically:
+y = min("Mike", "John", "Vicky")
+print(y)
