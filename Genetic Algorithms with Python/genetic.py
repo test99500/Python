9d43431c234de04dc65560302c20a5ce990462fa
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 23:17:21 +0800

    Committed 2021/06/22

--- Genetic Algorithms with Python/genetic.py
+++ Genetic Algorithms with Python/genetic.py
@@ -1,5 +1,7 @@
 import random
 import datetime
+import statistics
+import sys
 import time
 
 geneSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.'
@@ -59,4 +61,26 @@ def get_best(get_fitness, targetLen, optimalFitness, geneSet, display):
         bestParent = child
 
 
+class Chromosome:
+    def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
 
+
+class Benchmark:
+    @staticmethod
+    def run(function):
+        timings = []
+        stdout = sys.stdout
+        for i in range(100):
+            sys.stdout = None
+            startTime = time.time()
+            function()
+            seconds = time.time() - startTime
+            sys.stdout = stdout
+            timings.append(seconds)
+            mean = statistics.mean(timings)
+            if i < 10 or i % 10 == 9:
+                print("{} {:3.2f} {:3.2f}".format(
+                    1 + i, mean,
+                    statistics.stdev(timings, mean) if i > 1 else 0))
--- Genetic Algorithms with Python/guessPassword4.py
+++ Genetic Algorithms with Python/guessPassword4.py
@@ -1,18 +1,51 @@
 import datetime
+import random
+import unittest
+
 import genetic
 
-def test_Hello_World():
-    target = 'Hello World!'
-    guess_password(target)
+def get_fitness(guess, target):
+    return sum(1 for expected, actual in zip(target, guess)
+               if expected == actual)
 
 
-def guess_password(target):
-    geneset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
-    startTime = datetime.datetime.now()
+def display(candidate, startTime):
+    timeDiff = datetime.datetime.now() - startTime
+    print("{}\t{}\t{}".format(
+        candidate.Genes, candidate.Fitness, timeDiff))
 
 
-def fnGetFitness(genes):
-    return get_fitness(genes, target)
+class GuessPasswordTests(unittest.TestCase):
+    geneset = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,"
 
+    def test_Hello_World(self):
+        target = "Hello World!"
+        self.guess_password(target)
 
-def fnDisplay(genes):
+    def test_For_I_am_fearfully_and_wonderfully_made(self):
+        target = "For I am fearfully and wonderfully made."
+        self.guess_password(target)
+
+    def guess_password(self, target):
+        startTime = datetime.datetime.now()
+
+        def fnGetFitness(genes):
+            return get_fitness(genes, target)
+
+        def fnDisplay(candidate):
+            display(candidate, startTime)
+
+        optimalFitness = len(target)
+        best = genetic.get_best(fnGetFitness, len(target), optimalFitness,
+                                self.geneset, fnDisplay)
+        self.assertEqual(best.Genes, target)
+
+    def test_Random(self):
+        length = 150
+        target = ''.join(random.choice(self.geneset)
+                         for _ in range(length))
+
+        self.guess_password(target)
+
+    def test_benchmark(self):
+        genetic.Benchmark.run(self.test_Random)
