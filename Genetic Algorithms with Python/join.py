9e0f907a7e8e675ec6e074a0d3952602fb3ece5d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 15:41:35 +0800

    Committed 2021/06/22

--- Genetic Algorithms with Python/join.py
+++ Genetic Algorithms with Python/join.py
@@ -12,3 +12,13 @@ print(news)
 cram = ''.join(array)
 
 print(cram)
+
+stutter = 'abcdefg'
+
+nationalistic = list(stutter)
+
+print(nationalistic)
+
+secret_sauce = str(nationalistic)
+
+print(secret_sauce)
