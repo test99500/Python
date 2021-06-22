8da8d7293b9a4e6745a4baaf5821540be86f8975
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 17:44:25 +0800

    Committed 2021/06/22

---
+++ How To Code in Python 3/Chapter25 - Tuples/Multiplying Tuples.py
@@ -0,0 +1,8 @@
+coral = ('blue coral', 'staghorn coral', 'pillar coral', 'elkhorn coral')
+kelp = ('wakame', 'alaria', 'deep-sea tangle', 'macrocystis')
+
+multiplied_coral = coral * 2
+multiplied_kelp = kelp * 3
+
+print(multiplied_coral)
+print(multiplied_kelp)
