8254990ef17a7f18d39fb024c6c618c3797c08b9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-22 13:28:02 +0800

    Committed 2021/06/22

--- Genetic Algorithms with Python/random_sampling.py
+++ Genetic Algorithms with Python/random_sampling.py
@@ -4,3 +4,10 @@ import time
 
 geneSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.'
 
+newGene, alternate = random.sample(geneSet, 2)
+
+print(newGene, alternate)
+
+genome = random.sample(geneSet, 4)
+
+print(genome)
