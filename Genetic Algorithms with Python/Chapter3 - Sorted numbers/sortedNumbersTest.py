b314bba4fab05bc588dc37e3a46fbc59a6b32cd9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-23 13:35:29 +0800

    Committed 2021/06/23

--- Genetic Algorithms with Python/Chapter3 - Sorted numbers/sortedNumbersTest.py
+++ Genetic Algorithms with Python/Chapter3 - Sorted numbers/sortedNumbersTest.py
@@ -1,6 +1,6 @@
 import unittest
 import datetime
-
+import genetic
 
 def display(candidate, startTime):
     timeDiff = datetime.datetime.now() - startTime
@@ -10,6 +10,15 @@ def display(candidate, startTime):
         timeDiff))
 
 
+def get_fitness(genes):
+    fitness = 1
+
+    for i in range(1, len(genes)):
+        if genes[i] > genes[i - 1]:
+            fitness += 1
+            return fitness
+
+
 class SortedNumbersTest(unittest.TestCase):
 
     def test_sort_10_numbers(self):
@@ -17,6 +26,21 @@ class SortedNumbersTest(unittest.TestCase):
 
     def sort_numbers(self, totalNumbers):
         geneset = [i for i in range(100)]
+        startTime = datetime.datetime.now()
+
+        def fnDisplay(candidate):
+            display(candidate=candidate, startTime=startTime)
+
+        def fnGetFitness(genes):
+            return get_fitness(genes)
+
+
+        optimalFitness = totalNumbers
+        best = genetic.get_best(get_fitness=fnGetFitness, targetLen=totalNumbers,
+                                optimalFitness=optimalFitness, geneSet=geneset, display=fnDisplay)
+
+        self.assertTrue(not optimalFitness > best.Fitness)
+
 
     def get_fitness(self, genes):
         fitness = 1
@@ -25,4 +49,3 @@ class SortedNumbersTest(unittest.TestCase):
                 fitness += 1
 
         return fitness
-
