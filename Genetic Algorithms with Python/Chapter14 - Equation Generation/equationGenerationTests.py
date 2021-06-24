052a983d59a8b0c139caf76acb2fbc2ab8958c2e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-24 20:48:12 +0800

    Committed 2021/06/24

--- Genetic Algorithms with Python/Chapter14 - Equation Generation/equationGenerationTests.py
+++ Genetic Algorithms with Python/Chapter14 - Equation Generation/equationGenerationTests.py
@@ -1,2 +1,20 @@
 import unittest
 import datetime
+import genetic
+import random
+
+
+def create(numbers, operations, minNumbers, maxNumbers):
+    genes = [random.choice(numbers)]
+    count = random.randint(a=minNumbers, b=1 + maxNumbers)
+    while count > 1:
+        count -= 1
+        genes.append(random.choice(operations))
+        genes.append(random.choice(numbers))
+
+    return genes
+
+class EquationGenerationTests(unittest.TestCase):
+    def test(self):
+        numbers = [1, 2, 3, 4, 5, 6, 7]
+        operations = ['+', '-']
