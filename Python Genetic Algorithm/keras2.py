d9000bb566d9de2ec78606088b0e4d84e4955584
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-25 23:08:01 +0800

    Committed 2021/06/25

--- Python Genetic Algorithm/keras2.py
+++ Python Genetic Algorithm/keras2.py
@@ -2,34 +2,19 @@ import tensorflow.keras
 import pygad.kerasga
 import numpy
 import pygad
+from pygad.kerasga import predict
+from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy, MeanAbsoluteError
 
-
-def fitness_func(solution, sol_idx):
-    global data_inputs, data_outputs, keras_ga, model
-
-    predictions = pygad.kerasga.predict(model=model,
-                                        solution=solution,
-                                        data=data_inputs)
-
-    bce = tensorflow.keras.losses.BinaryCrossentropy()
-    solution_fitness = 1.0 / (bce(data_outputs, predictions).numpy() + 0.00000001)
-
-    return solution_fitness
-
-def callback_generation(ga_instance):
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
 
 # Build the keras model using the functional API.
 input_layer  = tensorflow.keras.layers.Input(2)
-dense_layer = tensorflow.keras.layers.Dense(4, activation="relu")(input_layer)
-output_layer = tensorflow.keras.layers.Dense(2, activation="softmax")(dense_layer)
+dense_layer = tensorflow.keras.layers.Dense(2, activation="sigmoid", use_bias=True)(input_layer)
+output_layer = tensorflow.keras.layers.Dense(1, activation="sigmoid", use_bias=True)(dense_layer)
 
 model = tensorflow.keras.Model(inputs=input_layer, outputs=output_layer)
 
 # Create an instance of the pygad.kerasga.KerasGA class to build the initial population.
-keras_ga = pygad.kerasga.KerasGA(model=model,
-                                 num_solutions=10)
+keras_ga = pygad.kerasga.KerasGA(model=model, num_solutions=9)
 
 # XOR problem inputs
 data_inputs = numpy.array([[0, 0],
@@ -38,20 +23,33 @@ data_inputs = numpy.array([[0, 0],
                            [1, 1]])
 
 # XOR problem outputs
-data_outputs = numpy.array([[1, 0],
-                            [0, 1],
-                            [0, 1],
-                            [1, 0]])
+data_outputs = numpy.array([[0],
+                            [1],
+                            [1],
+                            [0]])
 
-# Prepare the PyGAD parameters. Check the documentation for more information: https://pygad.readthedocs.io/en/latest/README_pygad_ReadTheDocs.html#pygad-ga-class
-num_generations = 250 # Number of generations.
-num_parents_mating = 5 # Number of solutions to be selected as parents in the mating pool.
-initial_population = keras_ga.population_weights # Initial population of network weights.
+# Build the fitness function
+def fitness_func(solution, sol_idx):
+    global data_inputs, data_outputs, kerasGA, model
+
+    predictions = predict(model=model, solution=solution, data=data_inputs)
+
+    mae = MeanAbsoluteError()
+    abs_error = mae(y_true=data_outputs, y_pred=predictions).numpy() + 0.00000001
+    solution_fitness = 1.0 / abs_error
+
+    return solution_fitness
+
+# Create a callback
+def callback_generation(ga_instance):
+    print('Generation = {generation}'.format(generation=ga_instance.generations_completed))
+    print('Fitness = {fitness}'.format(fitness=ga_instance.best_solution()[1]))
+
 
 # Create an instance of the pygad.GA class
-ga_instance = pygad.GA(num_generations=num_generations,
-                       num_parents_mating=num_parents_mating,
-                       initial_population=initial_population,
+ga_instance = pygad.GA(num_generations=250,# Number of generations.
+                       num_parents_mating=5,# Number of solutions to be selected as parents in the mating pool.
+                       initial_population=keras_ga.population_weights,# Initial population of network weights.
                        fitness_func=fitness_func,
                        on_generation=callback_generation)
 
