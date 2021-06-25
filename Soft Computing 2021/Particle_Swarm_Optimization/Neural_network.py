fee45c4af14728936bd22084169eddeef7fd6d23
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-26 05:12:29 +0800

    Committed 2021/06/25

--- Soft Computing 2021/Particle_Swarm_Optimization/Neural_network.py
+++ Soft Computing 2021/Particle_Swarm_Optimization/Neural_network.py
@@ -1,4 +1,24 @@
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, InputLayer, Input
+import tensorflow.keras
+import pygad.kerasga
+from pygad.kerasga import KerasGA
+import numpy
+import pygad
+from tensorflow.keras.initializers import Ones, ones
+from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy, MeanAbsoluteError
+from tensorflow.keras import Model
+from tensorflow.keras.utils import plot_model, pack_x_y_sample_weight
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.activations import sigmoid
+from pygad.kerasga import predict
+
+
+# Create a Keras model
+model = Sequential([InputLayer(input_shape=(2, )),
+                    Dense(units=2, use_bias=True, bias_initializer=Ones(), activation=sigmoid),
+                    Dense(units=1, use_bias=True, bias_initializer=Ones(), activation=sigmoid)])
 
 
 def error(current_pos):
