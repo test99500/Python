b3585e75366e76aeb7524492110f2c38e9edfb9c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-26 05:35:40 +0800

    Committed 2021/06/25

---
+++ Hands on Machine Learning2/Chapter12 -  Basic TensorFlow/Customization/Custom_Loss_Function.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+def huber_fn(y_true, y_pred):
+    error = y_true - y_pred
+    is_small_error = tf.abs(error) < 1
+    squared_loss = tf.square(error) / 2
+    linear_loss = tf.abs(error) - 0.5
+
+    return tf.where(is_small_error, squared_loss, linear_loss)
+
