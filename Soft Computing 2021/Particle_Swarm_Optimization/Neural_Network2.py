afb7fd6c540d9492ebeceb2c45f93cb27d052b93
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-26 08:41:28 +0800

    Committed 2021/06/26

---
+++ Soft Computing 2021/Particle_Swarm_Optimization/Neural_Network2.py
@@ -0,0 +1,125 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, InputLayer, Input
+import tensorflow.keras
+import pygad.kerasga
+from pygad.kerasga import KerasGA
+import numpy
+import pygad
+from tensorflow.keras.initializers import Ones, ones
+from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy, MeanAbsoluteError
+from tensorflow.keras import Model
+from tensorflow.keras.utils import plot_model, pack_x_y_sample_weight
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.activations import sigmoid
+from pygad.kerasga import predict
+
+
+def smooth_L1_loss(y_true, y_pred):
+    error = y_true - y_pred
+    is_small_error = tf.abs(error) < 1
+    squared_loss = tf.square(error) / 2
+    linear_loss = tf.abs(error) - 0.5
+
+    return tf.where(is_small_error, squared_loss, linear_loss)
+
+
+def error(current_position):
+    a12 = current_position[0]
+    a22 = current_position[1]
+    a13 = current_position[2]
+
+    return 1.0 / (1.0 + np.exp(-current_position))
+
+
+# Create a Keras model
+model = Sequential([InputLayer(input_shape=(2, )),
+                    Dense(units=2, use_bias=True, bias_initializer=Ones(), activation=sigmoid),
+                    Dense(units=1, use_bias=True, bias_initializer=Ones(), activation=sigmoid)])
+
+model.compile(loss=smooth_L1_loss)
+
+
+def error(current_pos):
+    x = current_pos[0]
+    y = current_pos[1]
+
+    return (x ** 2 - 10 * np.cos(2 * np.pi * x)) + (y ** 2 - 10 * np.cos(2 * np.pi * y)) + 20
+
+
+def grad_error(current_pos):
+    x = current_pos[0]
+    y = current_pos[1]
+
+    return np.array(
+        [2 * x + 10 * 2 * np.pi * x * np.sin(2 * np.pi * x),
+         2 * y + 10 * 2 * np.pi * y * np.sin(2 * np.pi * y)])
+
+
+class Particle:
+
+    def __init__(self, dim, minx, maxx):
+        self.position = np.random.uniform(low=minx, high=maxx, size=dim)
+        self.velocity = np.random.uniform(low=minx, high=maxx, size=dim)
+        self.best_part_pos = self.position.copy()
+
+        self.error = error(self.position)
+        self.best_part_err = self.error.copy()
+
+    def setPos(self, pos):
+        self.position = pos
+        self.error = error(pos)
+        if self.error < self.best_part_err:
+            self.best_part_err = self.error
+            self.best_part_pos = pos
+
+class PSO:
+
+    w = 0.729
+    c1 = 1.49445
+    c2 = 1.49445
+    lr = 0.01
+
+    def __init__(self, dims, numOfBoids, numOfEpochs):
+        self.swarm_list = [Particle(dims, -500, 500) for i in range(numOfBoids)]
+        self.numOfEpochs = numOfEpochs
+
+        self.best_swarm_position = np.random.uniform(low=-500, high=500, size=dims)
+        self.best_swarm_error = 1e80  # Set high value to best swarm error
+
+
+    def optimize(self):
+        for i in range(self.numOfEpochs):
+
+            for j in range(len(self.swarm_list)):
+
+                current_particle = self.swarm_list[j]  # get current particle
+
+                Vcurr = grad_error(current_particle.position)  # calculate current velocity of the particle
+
+                deltaV = self.w * Vcurr \
+                         + self.c1 * (current_particle.best_part_pos - current_particle.position) \
+                         + self.c2 * (self.best_swarm_position - current_particle.position)  # calculate delta V
+
+                new_position = self.swarm_list[j].position - self.lr * deltaV  # calculate the new position
+
+                self.swarm_list[j].setPos(new_position)  # update the position of particle
+
+                if error(new_position) < self.best_swarm_error:  # check the position if it's best for swarm
+                    self.best_swarm_position = new_position
+                    self.best_swarm_error = error(new_position)
+
+            print('Epoch: {0} | Best position: [{1},{2}] | Best known error: {3}'.format(i,
+                                                                                         self.best_swarm_position[0],
+                                                                                         self.best_swarm_position[1],
+                                                                                         self.best_swarm_error))
+
+
+pso = PSO(dims=2, numOfBoids=30, numOfEpochs=500)
+pso.optimize()
+
+# References:
+# 1. https://stackoverflow.com/a/56255595/14900011
+# 2. https://zhuanlan.zhihu.com/p/48426076
+# 3. https://archive.ph/MbzAm
+# 4. https://cloudxlab.com/blog/writing-custom-optimizer-in-tensorflow-and-keras/
