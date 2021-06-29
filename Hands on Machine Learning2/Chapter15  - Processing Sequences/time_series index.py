4d0f5c876215fa5ee64443197120d1796744b858
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-29 13:48:53 +0800

    Committed 2021/06/29

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/time_series index.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/time_series index.py
@@ -6,3 +6,7 @@ series = Time_Series_Generator.generate_time_series(batch_size=10000,
                                                     number_of_steps=number_of_steps + 10)
 
 X_train, y_train = series[:7000, :number_of_steps], series[:7000, -10, 0]
+
+print(X_train)
+print('='*30)
+print(y_train)
