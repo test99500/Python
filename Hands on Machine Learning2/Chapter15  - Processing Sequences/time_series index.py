159cf43b3c07d2beaba92f0b9f61c54888051253
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-06-29 13:53:38 +0800

    Committed 2021/06/29

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/time_series index.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/time_series index.py
@@ -8,5 +8,14 @@ series = Time_Series_Generator.generate_time_series(batch_size=10000,
 X_train, y_train = series[:7000, :number_of_steps], series[:7000, -10, 0]
 
 print(X_train)
+print(X_train.shape)
 print('='*30)
 print(y_train)
+print(y_train.shape)
+print('='*30)
+
+print(series[:5, -10])
+print('='*30)
+print(series[:5, 0])
+print('='*30)
+print(series[:5, -10, 0])
