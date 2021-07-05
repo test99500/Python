28a70b64db601672c73f4e061418eabc1950f19e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-05 13:08:34 +0800

    Committed 2021/07/05

--- Python for Data Analysis/Chapter11 - Time Series/Converting between string and datetime.py
+++ Python for Data Analysis/Chapter11 - Time Series/Converting between string and datetime.py
@@ -6,3 +6,19 @@ print(30*'=')
 print(str(stamp))
 print(30*'=')
 print(type(stamp))
+print(30*'=')
+#print(stamp.strftime(fmt='%Y-%m-%d'))
+print(stamp.strftime('%Y-%m-%d'))
+
+print(30*'=')
+
+# Convert strings to dates
+value = '2011-01-03'
+# value2 = datetime.strptime(date_string=value, format='%Y-%m-%d')
+# print(value2)
+value3 = datetime.strptime(value, '%Y-%m-%d')
+print(value3)
+
+datestrs = ['7/6/2011', '8/6/2011']
+surely = [datetime.strptime(x, '%m/%d/%Y') for x in datestrs]
+print(surely)
