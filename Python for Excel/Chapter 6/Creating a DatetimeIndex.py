903a18771d8204f2712ee7cd953bb8208d5abf62
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-06 01:57:28 +0800

    Committed 2021/07/05

--- Python for Excel/Chapter 6/Creating a DatetimeIndex.py
+++ Python for Excel/Chapter 6/Creating a DatetimeIndex.py
@@ -1,3 +1,18 @@
 import pandas as pd
 import numpy as np
 
+pd.options.plotting.backend = 'plotly'
+
+daily_index = pd.date_range(start="2020-02-28", periods=4, freq='D')
+print(daily_index)
+
+flashback = pd.date_range(start='2021-07-05', freq='D', periods=10)
+print(flashback)
+
+# This creates a DatetimeIndex based on start/end timestamp.
+# The frequency is set to 'weekly on Sundays'.
+weekly_index = pd.date_range(start='2020-01-01', end='2020-01-31', freq='W-SUN')
+print(weekly_index)
+
+weekly_index2 = pd.date_range(start='2021-07-06', end='2021-08-31', freq='W-SUN')
+print(weekly_index2)
