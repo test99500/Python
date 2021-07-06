7146084bd8cc422791a87ad1a203c753d6761bad
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-06 14:25:56 +0800

    Committed 2021/07/06

--- Python for Excel/Chapter 6/Filtering a DatatimeIndex2.py
+++ Python for Excel/Chapter 6/Filtering a DatatimeIndex2.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 url = 'https://raw.githubusercontent.com/fzumstein/python-for-excel/1st-edition/csv/MSFT.csv'
-microsoft = pd.read_csv(url, parse_dates=['Date'])
+microsoft = pd.read_csv(url, parse_dates=['Date'], index_col=['Date'])
 print(microsoft)
 print(microsoft.info())
 print(microsoft.describe())
 
-microsoft = microsoft.sort_values(by=['Date'])
+microsoft = microsoft.sort_index()
 print(microsoft)
 
-rows_from2019 = microsoft[:, microsoft['Date'].isin('2019')]
+rows_from2019 = microsoft['2019', 'Adj Close']
 print(rows_from2019)
