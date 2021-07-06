cf13a32707d02cd335a4fabc235563194677373c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-06 16:07:40 +0800

    Committed 2021/07/06

--- Supplementary materials/pandas/find().py
+++ Supplementary materials/pandas/find().py
@@ -6,7 +6,14 @@ data = pd.read_csv("https://media.geeksforgeeks.org/wp-content/uploads/nba.csv")
 print(data)
 
 # dropping null value columns to avoid errors
-data.dropna(inplace = True)
+data.dropna(inplace=True)
 
 # substring to be searched
-sub ='a'
+sub = 'a'
+
+# creating and passing series to new column
+data["Indexes"] = data["Name"].str.find(sub)
+
+print(data)
+
+# Reference: https://www.geeksforgeeks.org/python-pandas-series-str-find/
