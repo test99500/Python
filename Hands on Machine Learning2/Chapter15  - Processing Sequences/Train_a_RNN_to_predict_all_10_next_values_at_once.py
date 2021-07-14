8faafeb7ac3cc87b9389c9177fe0713e9de541f2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-14 19:50:55 +0800

    Committed 2021/07/14

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/Train_a_RNN_to_predict_all_10_next_values_at_once.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/Train_a_RNN_to_predict_all_10_next_values_at_once.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import Time_Series_Generator
+import tensorflow as tf
 
 number_of_steps = 50
 
@@ -13,8 +14,20 @@ X_train, y_train = series[:7000, :number_of_steps], series[:7000, -10, 0]
 
 print(X_train.shape)
 
+X_valid, Y_valid = series[7000:9000, :number_of_steps], series[7000:9000, -10:, 0]
+X_test, Y_test = series[9000:, :number_of_steps], series[9000:, -10:, 0]
+
 # X_train_df = pd.DataFrame(data=X_train)
 # print(X_train_df)
 
 # series_pd = pd.Series(series)
 # print(series_pd)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    tf.keras.layers.SimpleRNN(20),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, Y_valid))
