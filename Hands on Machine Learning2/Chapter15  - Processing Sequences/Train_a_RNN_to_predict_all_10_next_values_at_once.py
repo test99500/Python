2a7cbdf8ff0274519ee529126dc1565db02d4778
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-14 12:51:40 +0800

    Committed 2021/07/14

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/Train_a_RNN_to_predict_all_10_next_values_at_once.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/Train_a_RNN_to_predict_all_10_next_values_at_once.py
@@ -6,6 +6,15 @@ number_of_steps = 50
 series = Time_Series_Generator.generate_time_series(batch_size=10000,
                                                     number_of_steps=number_of_steps + 10)
 
+print(series)
+print(series.shape)
+
 X_train, y_train = series[:7000, :number_of_steps], series[:7000, -10, 0]
 
+print(X_train.shape)
 
+# X_train_df = pd.DataFrame(data=X_train)
+# print(X_train_df)
+
+# series_pd = pd.Series(series)
+# print(series_pd)
