b1dcaadcc13c8ab06961e30114b35e445e0703f6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-21 13:44:51 +0800

    Committed 2021/07/20

--- Supplementary materials/pandas/Reset_index.py
+++ Supplementary materials/pandas/Reset_index.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
 
+dataset = np.array([['day_time', 'data'],
+                    ['2014-02-02', 0.45],
+                    ['2014-02-02', 0.41]])
 
+df = pd.DataFrame(data=dataset)
 
+print(df)
+
+df = df.set_index('day_time')
+print(df)
+
+settings = {'day_time': ['2014-02-02', '2014-02-02'], 'data': [0.45, 0.41]}
+print(settings)
