ac65ac8e5fb8eeaa7ae963ee1466b72bea15aa0e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-25 23:29:49 +0800

    Committed 2021/07/25

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/WaveNet.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/WaveNet.py
@@ -17,4 +17,11 @@ X_test, y_test = series[9000:, :number_of_steps], series[9000:, -1]
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = Sequential([InputLayer(input_shape=[None, 1])])
+model = Sequential([InputLayer(input_shape=[None, 1]),
+                    Conv1D(filters=20, kernel_size=2, padding='causal', activation='relu',
+                           dilation_rate=[rate for rate in ((1, 2, 4, 8) * 2)]),
+                    Conv1D(filters=10, kernel_size=1)])
+
+model.compile(optimizer='adam', loss='mse')
+
+history = model.fit(x=X_train, y=y_train, epochs=20, validation_data=(X_valid, y_valid))
