2546e1ae809faa0458f4c09218540855036fb917
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-26 17:30:24 +0800

    Committed 2021/07/26

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/time_series_excel.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/time_series_excel.py
@@ -1,4 +1,6 @@
 import Time_Series_Generator as t
+import pandas as pd
 
-series = t.generate_time_series(batch_size=1, number_of_steps=2)
-print(series)
+series = t.generate_time_series(batch_size=100, number_of_steps=51)
+
+print(series.shape)
