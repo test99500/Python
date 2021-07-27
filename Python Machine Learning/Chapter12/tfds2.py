9facd15c058f7137b1d8e1fd19a692f1c04c79ce
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-27 14:27:43 +0800

    Committed 2021/07/27

--- Python Machine Learning/Chapter12/tfds2.py
+++ Python Machine Learning/Chapter12/tfds2.py
@@ -2,3 +2,8 @@ import tensorflow_datasets as tfds
 
 celebrity = tfds.load('celeb_a', with_info=True, shuffle_files=False)
 print(celebrity)
+
+print('='*30)
+
+celebrity2 = tfds.load(name='celeb_a')
+print(celebrity2)
