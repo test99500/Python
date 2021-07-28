86c230aa1804130fd56762eadb366eae70fb493b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-28 13:41:26 +0800

    Committed 2021/07/28

--- Python Machine Learning/Chapter12/tfds_celebrity.py
+++ Python Machine Learning/Chapter12/tfds_celebrity.py
@@ -2,3 +2,8 @@ import tensorflow_datasets as tfds
 
 celebrity, celebrity_info = tfds.load(name='celeb_a', with_info=True, )
 print(celebrity_info)
+print(celebrity)
+print(celebrity.keys())
+
+ds_train = celebrity['train']
+print(ds_train.keys())
