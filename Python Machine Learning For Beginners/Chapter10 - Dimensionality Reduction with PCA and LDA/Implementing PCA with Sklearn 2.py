c97f8ce0e4e06195c1c4b51abcfa8c91d032f017
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-04 13:55:34 +0800

    Committed 2021/08/04

---
+++ Python Machine Learning For Beginners/Chapter10 - Dimensionality Reduction with PCA and LDA/Implementing PCA with Sklearn 2.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+# importing the dataset
+iris_df = sns.load_dataset("iris");
+
+print(iris_df.head())
+
+# Creating feature set
+X = iris_df.drop(["species"], axis=1)
+
+# Creating label set
+y = iris_df["species"]
+
+# Converting labels to numbers
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# Dividing data into 80-20% training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# applying scaling on training and test data
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Creating object of PCA class
+pca = PCA()
+
+# Training PCA model on training data
+X_train_PCAed = pca.fit_transform(X_train)
+
+# Making predictions on test data
+X_test_PCAed = pca.transform(X_test)
+
+print(X_train_PCAed)
+print('=' * 30)
+print(X_test_PCAed)
+print('=' * 30)
+
+# How much variance does the first, second, third, and fourth principal component cause?
+variance_ratio = pca.explained_variance_ratio_
+print(variance_ratio)
+
+# According to the variance_ratio, select the two principal components that caused a
+# collective variance of 96.19% (72.22% + 23.97% = 96.19%)
+pca2 = PCA(n_components=2)
+
+X_train = pca2.fit_transform(X_train)
+X_test = pca2.transform(X_test)
+
+logistic_regression = LogisticRegression()
+logistic_regression.fit(X=X_train, y=y_train)
+
+y_prediction = logistic_regression.predict(X=X_test)
+
+print(accuracy_score(y_true=y_test, y_pred=y_prediction))
+
+# With two features, you can easily visualize the dataset.
+## Print the actual data points.
+plt.scatter(x=X_test[:, 0], y=X_test[:, 1], c=y_test, cmap='rainbow')
+plt.title("Print actual data points")
+plt.show()
+
+print(X_test[:, 0])
+print('=' * 30)
+print(X_test[:, 1])
