7919836e795dff6114ec84c073d1cb42e91481ae
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-04 14:20:17 +0800

    Committed 2021/08/04

--- EPUB_Check/Validate_a_single_EPUB_file.py
+++ EPUB_Check/Validate_a_single_EPUB_file.py
@@ -1,3 +1,11 @@
 from epubcheck import EpubCheck
 
+result = EpubCheck('')
 
+print(result.valid)
+print(result.messages)
+
+# References:
+# 1. https://www.w3.org/publishing/epubcheck/about/
+# 2. https://w3c.github.io/epubcheck/docs/apps-and-tools/
+# 3. https://pypi.org/project/epubcheck/
