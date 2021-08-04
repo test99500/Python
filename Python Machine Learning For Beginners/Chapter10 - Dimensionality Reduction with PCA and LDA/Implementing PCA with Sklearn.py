0e7fd860ae0bf8aca90b44821eea4eccb0dba9be
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-04 12:23:54 +0800

    Committed 2021/08/04

--- Python Machine Learning For Beginners/Chapter10 - Dimensionality Reduction with PCA and LDA/Implementing PCA with Sklearn.py
+++ Python Machine Learning For Beginners/Chapter10 - Dimensionality Reduction with PCA and LDA/Implementing PCA with Sklearn.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 
 # importing the dataset
 iris_df = sns.load_dataset("iris");
 
 print(iris_df.head())
+
+# Creating feature set
+X = iris_df.drop(["species"], axis=1)
+
+# Creating label set
+y = iris_df["species"]
+
+# Converting labels to numbers
