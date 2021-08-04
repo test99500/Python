8dc0ce56eec75e3ba5d27665f348629e6b20d120
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-04 13:52:45 +0800

    Committed 2021/08/04

--- Python Machine Learning For Beginners/Chapter10 - Dimensionality Reduction with PCA and LDA/Accuracy without dimensionality reduction.py
+++ Python Machine Learning For Beginners/Chapter10 - Dimensionality Reduction with PCA and LDA/Accuracy without dimensionality reduction.py
@@ -31,3 +31,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+logistic_regression = LogisticRegression()
+logistic_regression.fit(X=X_train, y=y_train)
+
+y_prediction = logistic_regression.predict(X=X_test)
+
+print(accuracy_score(y_true=y_test, y_pred=y_prediction))
+
+# With four features, visualize the dataset.
+## Print the actual data points.
+plt.scatter(x=X_test[:, 0], y=X_test[:, 1], c=y_test, cmap='rainbow')
+plt.title("Print actual data points")
+plt.savefig('Without_dimensionality_reduction.jpg')
+plt.show()
