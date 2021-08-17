6500e2c4a93753c23ca8c2d5a273aeaff9ef3a1c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-17 12:25:36 +0800

    Committed 2021/08/17

---
+++ Python Machine Learning by Example/Chapter13 - Recurrent Neural Network/Analyzing movie review sentiment with RNNs2.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras import layers, models, losses, optimizers
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+
+vocabulary_size = 5000
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocabulary_size)
+
+# Check whether the training set is balanced, that is,
+# having the same number of positive and negative samples.
+print('Number of training samples: ', len(y_train))
+print('Number of positive samples: ', sum(y_train))
+print('Number of test samples: ', len(y_test))
+
+# Print a training sample
+print(X_train[0])
+
+word_index = imdb.get_word_index()
+index_word = {index: word for word, index in word_index.items()}
+print([index_word.get(i, ' ') for i in X_train[0]])
+
+review_length = [len(x) for x in X_train]
+
+plt.hist(x=review_length, bins=5)
+plt.show()
