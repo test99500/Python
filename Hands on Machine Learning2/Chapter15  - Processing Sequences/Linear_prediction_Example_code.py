885d4d376191d1ea305eac65ceac3677e7d689fc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-21 18:58:28 +0800

    Committed 2021/08/21

---
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/Linear_prediction_Example_code.py
@@ -0,0 +1,72 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def generate_time_series(batch_size, n_steps):
+    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
+    time = np.linspace(0, 1, n_steps)
+    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
+    return series[..., np.newaxis].astype(np.float32)
+
+
+def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
+    plt.plot(series, ".-")
+    if y is not None:
+        plt.plot(n_steps, y, "bx", markersize=10)
+    if y_pred is not None:
+        plt.plot(n_steps, y_pred, "ro")
+    plt.grid(True)
+    if x_label:
+        plt.xlabel(x_label, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label, fontsize=16, rotation=0)
+    plt.hlines(0, 0, 100, linewidth=1)
+    plt.axis([0, n_steps + 1, -1, 1])
+
+
+def plot_learning_curves(loss, val_loss):
+    plt.plot(np.arange(len(loss)) + 0.5, loss, "b.-", label="Training loss")
+    plt.plot(np.arange(len(val_loss)) + 1, val_loss, "r.-", label="Validation loss")
+    plt.gca().xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
+    plt.axis([1, 20, 0, 0.05])
+    plt.legend(fontsize=14)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.grid(True)
+
+
+np.random.seed(42)
+
+n_steps = 50
+series = generate_time_series(10000, n_steps + 1)
+X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
+X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=[50, 1]),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+
+model.evaluate(X_valid, y_valid)
+
+
+plot_learning_curves(history.history["loss"], history.history["val_loss"])
+
+plt.savefig('Linear prediction')
+plt.show()
+
+y_pred = model.predict(X_valid)
+plot_series(X_valid[0, :, 0], y_valid[0, 0], y_pred[0, 0])
+
+plt.savefig('Linear prediction series')
+plt.show()
