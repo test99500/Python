c5910c8a023771533beec6d7b83511757d8c7c0a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-21 21:25:49 +0800

    Committed 2021/08/21

--- Hands on Machine Learning2/Chapter15  - Processing Sequences/WaveNet4.py
+++ Hands on Machine Learning2/Chapter15  - Processing Sequences/WaveNet4.py
@@ -38,12 +38,14 @@ Y_test = Y[9000:]
 np.random.seed(42)
 tf.random.set_seed(42)
 
-model = Sequential()
-model.add(InputLayer(input_shape=[None, 1]))
-for rate in (1, 2, 4, 8) * 2:
-    model.add(Conv1D(filters=20, kernel_size=2, padding="causal",
-                     activation="relu", dilation_rate=rate))
-model.add(Conv1D(filters=10, kernel_size=1))
+model = Sequential([Conv1D(filters=20, kernel_size=2, padding='causal', activation='relu',
+                           dilation_rate=1, input_shape=[None, 1]),
+                    Conv1D(filters=20, kernel_size=2, padding='causal', activation='relu',
+                           dilation_rate=2),
+                    Conv1D(filters=20, kernel_size=2, padding='causal', activation='relu',
+                           dilation_rate=4),
+                    Conv1D(filters=20, kernel_size=2, padding='causal', activation='relu',
+                           dilation_rate=8)])
 
 model.compile(loss="mse", optimizer="adam", metrics=[last_time_step_mse])
 history = model.fit(X_train, Y_train, epochs=20, validation_data=(X_valid, Y_valid))
