3cd8350af3bf98177170482adb524fdf6ef1aa08
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-22 17:28:41 +0800

    Committed 2021/08/22

--- TensorFlow Tutorials and Guide/Basic text classification.py
+++ TensorFlow Tutorials and Guide/Basic text classification.py
@@ -1,12 +1,11 @@
-import matplotlib.pyplot as plt
-import os
-import re
-import shutil
-import string
-import tensorflow as tf
-
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow.keras import preprocessing
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras import layers, models, losses, optimizers
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+
 
