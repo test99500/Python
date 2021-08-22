7900005857ce67a2bd43b9eb0d399949cb7523c7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-22 17:01:09 +0800

    Committed 2021/08/22

---
+++ TensorFlow Tutorials and Guide/Basic text classification.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import preprocessing
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
