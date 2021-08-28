af4916f5e4aca6da175cf8de05f0ef26294ff48b
Ceiling_roof <ted10014@hotmail.com>
2021-08-29 02:37:09 +0800

    Committed 2021/08/29

---
+++ Machine Learning for Finance/Chapter4 - Time Series/Visualization and preparation pandas3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+train = pd.read_csv('train_1.csv').fillna(0)
+print(train.head())
+
+def parse_page(page):
+    x = page.split('_') # split the string by underscore.
+    return ''.join(x[:-3]), x[-3], x[-2], x[-1]
+
+
+print(parse_page(train.Page[0]))
