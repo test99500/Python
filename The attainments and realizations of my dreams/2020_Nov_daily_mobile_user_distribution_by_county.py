075420e055b0c10049ac6ad24e296b8f15ce8bd4
Ceiling_roof <ted10014@hotmail.com>
2021-09-01 12:30:41 +0800

    Committed 2021/09/01

--- The attainments and realizations of my dreams/2020_Nov_daily_mobile_user_distribution_by_county.py
+++ The attainments and realizations of my dreams/2020_Nov_daily_mobile_user_distribution_by_county.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+# Skip one or more rows by giving the row indices (the first row that's not skipped is the header):[1]
 df = pd.read_csv("2020_November_daily_mobile_user_distribution_by_county.csv"
-                 , header=0, skiprows=1)
+                 , header=0, skiprows=[1], encoding="big5")
 
 print(df)
+
+# References:
+# 1. https://stackoverflow.com/a/27325729/14900011
