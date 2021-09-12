50c718011eb813a7ca53ed35233b52de1d2defa9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-12 19:19:52 +0800

    Committed 2021/09/12

--- The attainments and realizations of my dreams/2020_translated.py
+++ The attainments and realizations of my dreams/2020_translated.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-distribution = pd.read_csv('2020_translated_Nov_daily_mobile_user_distribution_by_county_.csv')
+distribution = pd.read_csv('2020_translated_Nov_daily_mobile_user_distribution_by_county_.csv',
+                           index_col=0)
 
-print(distribution)
+print(distribution.head())
