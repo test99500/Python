56ba06720f49c152c82265f16fa21e0d4a31ca6d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-13 15:49:27 +0800

    Committed 2021/09/13

--- The attainments and realizations of my dreams/2020_translated.py
+++ The attainments and realizations of my dreams/2020_translated.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 distribution = pd.read_csv('2020_translated_Nov_daily_mobile_user_distribution_by_county_.csv',
                            index_col=0)
@@ -10,3 +11,6 @@ print(30*'=')
 print(distribution["INFO_TIME"].value_counts())
 print(30*'=')
 print(distribution.describe())
+print(30*'=')
+print(distribution.hist(bins=50, figsize=(20, 15)))
+plt.show()
