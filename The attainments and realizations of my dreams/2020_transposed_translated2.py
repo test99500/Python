c785488f418976a99a7f0f14aaa398972b8e0b51
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-13 17:40:40 +0800

    Committed 2021/09/13

--- The attainments and realizations of my dreams/2020_transposed_translated2.py
+++ The attainments and realizations of my dreams/2020_transposed_translated2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 distribution = pd.read_csv('2020_transposed_translated_mobile_user_distribution_by_county.csv',
-                            index_col=0)
+                           index_col=0)
 print(distribution)
 
 print(distribution.info())
