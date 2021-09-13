a9e69b5180e95ededdd904e177442b271d8fcd49
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-13 17:57:08 +0800

    Committed 2021/09/13

--- The attainments and realizations of my dreams/2020_transposed_translated3.py
+++ The attainments and realizations of my dreams/2020_transposed_translated3.py
@@ -2,7 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 distribution = pd.read_csv('2020_transposed_translated_mobile_user_distribution_by_county.csv',
-                           index_col=0, header=0)
+                           index_col=0, header=0, skiprows=0)
 print(distribution)
 
-print(distribution.info())
+distribution = pd.read_csv('2020_transposed_translated_mobile_user_distribution_by_county.csv',
+                           index_col=0, skiprows=0, header=0)
+print(distribution)
+
+distribution = pd.read_csv('2020_transposed_translated_mobile_user_distribution_by_county.csv',
+                           index_col=0, skiprows=[0], header=0)
+print(distribution)
+
+distribution = pd.read_csv('2020_transposed_translated_mobile_user_distribution_by_county.csv',
+                           index_col=0, header=0, skiprows=[0])
+print(distribution)
+
+distribution = pd.read_csv('2020_transposed_translated_mobile_user_distribution_by_county.csv',
+                           index_col=0, header=0, skiprows=1)
+print(distribution)
