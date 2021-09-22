63a013b13f2f8e41c19269b3513b0d72e1e2cf8e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-22 13:40:02 +0800

    Committed 2021/09/22

--- The attainments and realizations of my dreams/Annual_household_income.py
+++ The attainments and realizations of my dreams/Annual_household_income.py
@@ -8,11 +8,16 @@ income_Taoyuan = [1307158, 1317790, 1337361, 1378732, 1392199, 1424027]
 income_Taipei = [1581899, 1568945, 1648122, 1649348, 1723021, 1716591]
 income_New_Taipei = [1171978, 1223867, 1265798, 1292753, 1319841, 1352548]
 
-plt.plot(year, income_Taipei)
-plt.plot(year, income_New_Taipei)
-plt.plot(year, income_Taoyuan)
-plt.plot(year, income_Taichung)
-plt.plot(year, income_Tainan)
-plt.plot(year, income_Kaohsiung)
+plt.plot(year, income_Taipei, label = "Taipei")
+plt.plot(year, income_New_Taipei, label="New Taipei")
+plt.plot(year, income_Taoyuan, label="Taoyuan")
+plt.plot(year, income_Taichung, label="Taichung")
+plt.plot(year, income_Tainan, label="Tainan")
+plt.plot(year, income_Kaohsiung, label="Kaohsiung")
+
+plt.title("Annual household income by region in Taiwan")
+plt.legend()
+plt.grid(linewidth=0.7)
+plt.ylabel("Income (1 million New Taiwan Dollar)")
 
 plt.show()
