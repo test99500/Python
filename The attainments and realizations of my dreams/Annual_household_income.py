b3075e85f18a4ac2566ab7289899850308c98cd5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-22 14:04:09 +0800

    Committed 2021/09/22

--- The attainments and realizations of my dreams/Annual_household_income.py
+++ The attainments and realizations of my dreams/Annual_household_income.py
@@ -7,13 +7,17 @@ income_Taichung = [1169183, 1140325, 1245350, 1279865, 1298497, 1289700]
 income_Taoyuan = [1307158, 1317790, 1337361, 1378732, 1392199, 1424027]
 income_Taipei = [1581899, 1568945, 1648122, 1649348, 1723021, 1716591]
 income_New_Taipei = [1171978, 1223867, 1265798, 1292753, 1319841, 1352548]
+income_Hsinchu_county = [1283995, 1365150, 1616327, 1519478, 1539555, 1619782]
+income_Hsinchu_city = [1427572, 1537317, 1572296, 1426379, 1602826, 1618903]
 
-plt.plot(year, income_Taipei, label = "Taipei")
+plt.plot(year, income_Taipei, label="Taipei")
 plt.plot(year, income_New_Taipei, label="New Taipei")
 plt.plot(year, income_Taoyuan, label="Taoyuan")
 plt.plot(year, income_Taichung, label="Taichung")
 plt.plot(year, income_Tainan, label="Tainan")
 plt.plot(year, income_Kaohsiung, label="Kaohsiung")
+plt.plot(year, income_Hsinchu_county, label="Hsinchu county")
+plt.plot(year, income_Hsinchu_city, label="Hsinchu city")
 
 plt.title("Annual household income by region in Taiwan")
 plt.legend()
