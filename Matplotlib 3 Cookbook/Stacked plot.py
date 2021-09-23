dfae7485f2c371dd59f4c8cd0e548de25fdbe10c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-23 09:42:58 +0800

    Committed 2021/09/23

---
+++ Matplotlib 3 Cookbook/Stacked plot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([1, 2, 3, 4, 5, 6], dtype=np.int32)
+April = [5, 6, 7, 8, 9]
+May = [0, 4, 3, 7, 8, 9]
+June = [6, 7, 4, 5, 6, 8]
+
+labels = ["April ", "May", "June"]
+
+fig, ax = plt.subplots()
+
+ax.stackplot(x, April, May, June, labels=labels)
+ax.legend(loc=2)
+plt.xlabel('defect reason code')
+plt.ylabel('number of defects')
+plt.title('Product Defects - Q1 FY2019')
+
+plt.show()
