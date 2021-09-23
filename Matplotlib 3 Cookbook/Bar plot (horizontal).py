0264a56aa5c2d2b1ff80a74c9a11107474f86c15
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-23 11:12:13 +0800

    Committed 2021/09/23

--- Matplotlib 3 Cookbook/Bar plot (horizontal).py
+++ Matplotlib 3 Cookbook/Bar plot (horizontal).py
@@ -1,4 +1,18 @@
 import matplotlib.pyplot as plt
+import calendar
 
+# matplotlib accepts only floating point data types as its arguments for data.
+# So months have to be represented in numerical format.
+month_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+units_sold = [500, 600, 750, 900, 1100, 1050, 1000, 950, 800, 700, 550, 450]
 
+fig, ax = plt.subplots()
 
+# change the month number to month name on y axis
+plt.yticks(month_num, calendar.month_name[1:13], rotation=20)
+
+# plot horizontal bar graph
+plot = plt.barh(month_num, units_sold)
+
+# display the graph on the screen
+plt.show()
