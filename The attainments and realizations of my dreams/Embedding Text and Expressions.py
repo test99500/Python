9367436f6bc67671f378d63686fa3d1461078090
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-23 18:47:42 +0800

    Committed 2021/09/23

--- The attainments and realizations of my dreams/Embedding Text and Expressions.py
+++ The attainments and realizations of my dreams/Embedding Text and Expressions.py
@@ -1,3 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+font = {'family': 'DejaVu Sans', 'name': 'Times New Roman', 'style': 'italic', 'color': 'orange',
+        'weight': 'bold', 'size': 16}
