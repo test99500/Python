4c5340af3bbc031e3dc1cfc38c191a3604823759
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-23 16:07:53 +0800

    Committed 2021/09/23

---
+++ Matplotlib for Python Developers/Customizing data point markers.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+custom_markers =
--- Matplotlib for Python Developers/Data point markers.py
+++ Matplotlib for Python Developers/Data point markers.py
@@ -3,5 +3,6 @@ from matplotlib.lines import Line2D
 
 for i, marker in enumerate(Line2D.markers):
     plt.scatter(x=i%10, y=i, marker=marker, s=100)  # plot each of the markers in size of 100.
-    plt.show()
+
+plt.show()
 
