370df0196e4722cf51bed7b31336ac9d2c652627
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-23 09:32:50 +0800

    Committed 2021/09/23

--- Matplotlib 3 Cookbook/Pie plot2.py
+++ Matplotlib 3 Cookbook/Pie plot2.py
@@ -6,7 +6,7 @@ sizes = [5, 15, 10, 20, 40, 10]  # Add upto 100%
 # Show one slice slightly outside the circle.
 explode = (0, 0, 0, 0, 0.1, 0)  # only "explode" the 5th slice (i.e. 'Action')
 
-plt.pie(x=sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.pie(x=sizes, explode=explode, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
 
 # The equal aspect ratio ensures that pie is drawn as a circle. The default is an ellipse.
 plt.axis('equal')
