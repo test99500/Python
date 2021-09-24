e7435cb4538e489fd0824ef7e1ab26ae4a412bbc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-24 16:59:17 +0800

    Committed 2021/09/24

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots6.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots6.py
@@ -2,13 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 figure = plt.figure()
-
 axes1 = figure.add_subplot(2, 2, 1)
 axes2 = figure.add_subplot(2, 2, 2)
 axes3 = figure.add_subplot(2, 2, 3)
 
-axes1.hist(np.random.randn(100), bins=20, color='k', alpha=0.3)
+axes3.plot(np.random.randn(50).cumsum(), linestyle='--', color='k')
 axes2.scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
-axes3.plot([1.5, 3.5, -2, 1.6])
+axes1.hist(np.random.randn(100), bins=20, color='k', alpha=0.3)
 
 plt.show()
