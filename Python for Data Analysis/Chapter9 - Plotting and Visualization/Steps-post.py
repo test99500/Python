0273411005f4aba6f0a40cbe7955b75c2c75abcd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-24 13:36:37 +0800

    Committed 2021/09/24

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Steps-post.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Steps-post.py
@@ -2,5 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import randn
 
+data = np.random.randn(30).cumsum()
 
+plt.plot(data, color='k', linestyle='--', label='default')
 
+plt.plot(data, color='k', linestyle='--', drawstyle='steps-post', label='steps-post')
+
+plt.legend(loc='best')
+
+plt.show()
