fee1fd4998f3113d199395f476acf441b3dfe993
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-24 16:38:49 +0800

    Committed 2021/09/24

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots4.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots4.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 # When you directly issue a plotting command like the following:
 plt.plot([1.5, 3.5, -2, 1.6])
 # matplotlib draws on the last figure and subplot used (automatically creating one if necessary),
-# thus hiding the figure and subplot creation by you.
+# thus skipping the need for you to create the figure and subplot.
 
 plt.show()
