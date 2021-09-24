55a9beba52c66165896d3fe3d3a3f8e6583747eb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 00:36:06 +0800

    Committed 2021/09/24

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label.py
@@ -3,6 +3,6 @@ import numpy as np
 
 figure, axes = plt.subplots(nrows=1, ncols=1, figsize=(8, 6))
 
-axes[1, 1].plot(np.random.randn(1000).cumsum())
+axes[0, 0].plot(np.random.randn(1000).cumsum())
 
 plt.show()
