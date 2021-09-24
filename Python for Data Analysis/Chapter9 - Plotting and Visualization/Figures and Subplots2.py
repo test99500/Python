70e8a70bc3eb2b8200a09720af4ac2a017701f91
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-24 15:59:58 +0800

    Committed 2021/09/24

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots2.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots2.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+figure = plt.figure()
 
+axes1 = figure.add_subplot(2, 2, 1)
+axes2 = figure.add_subplot(2, 2, 2)
+axes3 = figure.add_subplot(2, 2, 3)
+
+plt.show()
