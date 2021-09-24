ca98bf4da0586b3f86cd5c6bd2901df75e2d3b94
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-24 17:46:00 +0800

    Committed 2021/09/24

---
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Figures and Subplots11.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+figure, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 6), sharex=True, sharey=True)
+
+axes[0, 0].hist(np.random.randn(100), bins=20, color='k', alpha=0.3)
+axes[0, 1].scatter(np.arange(30), np.arange(30) + 3 * np.random.randn(30))
+axes[0, 2].plot(np.random.randn(50).cumsum(), linestyle='--', color='k')
+axes[1, 0].plot([1.5, 3.5, -2, 1.6])
+
+plt.show()
