6671b36244a381a93fc3b0e46e47df9118885b3e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 12:10:32 +0800

    Committed 2021/09/25

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label_3.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label_3.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Call the figure() method via the plt module, which draws an empty figure.
 figure = plt.figure()
 
+# Next, call the axes() method, which returns an axes object.
+axes = plt.axes()
 
+# You can then call the plot() method from the axes object to create a plot.
+axes.plot(np.random.randn(1000).cumsum())
+
+plt.show()
