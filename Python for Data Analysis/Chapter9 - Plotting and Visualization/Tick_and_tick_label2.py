5776d43c1462015997f681107fd55c07038c4cfe
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 11:54:57 +0800

    Committed 2021/09/25

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label2.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label2.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# There is no need to follow figure, axes = plt.subplots() if
+# There is no need to follow figure, axes = plt.subplots()
+# if you are not planning to plot multiple subplots in the same figure.
 
 plt.rcParams["figure.figsize"] = [8, 6]
 
