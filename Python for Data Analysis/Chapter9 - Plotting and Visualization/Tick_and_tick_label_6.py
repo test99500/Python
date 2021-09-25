5340732c62e2880124f84781a72471974eb8145e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 12:56:52 +0800

    Committed 2021/09/25

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label_6.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label_6.py
@@ -4,3 +4,9 @@ import numpy as np
 figure = plt.figure(num=1, figsize=(8, 6),)
 axes = plt.axes()
 axes.plot(np.random.randn(1000).cumsum())
+
+axes.set_xticks([0, 250, 500, 750, 1000])
+axes.set_xticklabels(['one', 'two', 'three', 'four', 'five'], rotation=30, fontsize='small')
+
+
+plt.show()
