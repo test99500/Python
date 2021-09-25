93e27b49fddbc15e7e96fbbfb0a971e4e0d52891
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 11:46:32 +0800

    Committed 2021/09/25

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label2.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label2.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# There is no need to follow figure, axes = plt.subplots() if
 
+plt.rcParams["figure.figsize"] = [8, 6]
+
+plt.plot(np.random.randn(1000).cumsum())
+
+plt.show()
