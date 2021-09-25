5cc0b770a2cd9d6a209dbb38ddb9f367e476c962
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 13:00:34 +0800

    Committed 2021/09/25

--- Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label_6.py
+++ Python for Data Analysis/Chapter9 - Plotting and Visualization/Tick_and_tick_label_6.py
@@ -8,5 +8,7 @@ axes.plot(np.random.randn(1000).cumsum())
 axes.set_xticks([0, 250, 500, 750, 1000])
 axes.set_xticklabels(['one', 'two', 'three', 'four', 'five'], rotation=30, fontsize='small')
 
+axes.set_yticks([0, 10, 20, 30, 40, 50, 60])
+axes.set_yticklabels(['0.0', '10.0', '20.0', '30.0', '40.0', '50.0', '60.0'])
 
 plt.show()
