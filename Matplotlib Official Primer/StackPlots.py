b0bf493a1a6c610893a5ce1f8aed5421559e60a9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-25 15:01:48 +0800

    Committed 2021/09/25

---
+++ Matplotlib Official Primer/StackPlots.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# data from United Nations World Population Prospects (Revision 2019)
+# https://population.un.org/wpp/, license: CC BY 3.0 IGO
+year = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2018]
+population_by_continent = {
+    'africa': [228, 284, 365, 477, 631, 814, 1044, 1275],
+    'americas': [340, 425, 519, 619, 727, 840, 943, 1006],
+    'asia': [1394, 1686, 2120, 2625, 3202, 3714, 4169, 4560],
+    'europe': [220, 253, 276, 295, 310, 303, 294, 293],
+    'oceania': [12, 15, 19, 22, 26, 31, 36, 39],
+}
+
+fig, ax = plt.subplots()
+ax.stackplot(year, population_by_continent.values(),
+             labels=population_by_continent.keys())
+ax.legend(loc='upper left')
+ax.set_title('World population')
+ax.set_xlabel('Year')
+ax.set_ylabel('Number of people (millions)')
+
+plt.show()
+
+# Source: https://matplotlib.org/stable/gallery/lines_bars_and_markers/stackplot_demo.html#stackplots
