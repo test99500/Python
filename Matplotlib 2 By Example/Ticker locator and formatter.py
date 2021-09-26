0e0a0e4151929f3f07bf84597d1087d5aef04273
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-26 13:25:24 +0800

    Committed 2021/09/26

---
+++ Matplotlib 2 By Example/Ticker locator and formatter.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plt
+
+x = range(2011, 2018)
+y = [26.48, 27.56, 29.41, 33.27, 36.32, 37.55, 40.28, 44.35, 48.36, 50.05, 53.06, 57.39, ]
