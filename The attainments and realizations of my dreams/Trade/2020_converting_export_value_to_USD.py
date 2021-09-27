33f5dd13a4e0c7cd0eb9a2bc4c7b5588eb442b3a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 18:27:49 +0800

    Committed 2021/09/27

--- The attainments and realizations of my dreams/Trade/2020_converting_export_value_to_USD.py
+++ The attainments and realizations of my dreams/Trade/2020_converting_export_value_to_USD.py
@@ -4,5 +4,19 @@ import matplotlib.pyplot as plt
 exportation = pd.read_csv("2020_export_value_from_each_customs.csv", header=0, index_col=0)
 
 print(exportation)
+print(exportation.info())
+
+def TW_to_USD(x):
+    """
+    TWD	Taiwan New Dollar	28.0881028819	0.0356022621
+
+    https://web.archive.org/web/20210927101148/https://www.xe.com/currencytables/?from=USD&date=2020-12-31
+    """
+    return x / 28.0881028819
+
+
+exportation.applymap(TW_to_USD)
+
+print(exportation)
 
 
