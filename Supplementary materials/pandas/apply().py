7f766a4c3c9e6ee8aaa3d3a5eb2239215a493348
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 18:32:51 +0800

    Committed 2021/09/27

--- Supplementary materials/pandas/apply().py
+++ Supplementary materials/pandas/apply().py
@@ -1,4 +1,18 @@
 import pandas as pd
+import numpy as np
+
+frame = pd.DataFrame(np.random.randn(4, 3),
+                     columns=list('bde'), index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+
+print(frame)
+
+def f(x):
+    return x.max() - x.min()
 
 
+frame.apply(f)
 
+print(frame)
+
+# Reference:
+# 1. https://stackoverflow.com/a/19798528/14900011
