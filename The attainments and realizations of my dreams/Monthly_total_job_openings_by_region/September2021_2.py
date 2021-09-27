e6c245595b733f22d2f926bfdf3fa4f4ceb80799
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 19:30:11 +0800

    Committed 2021/09/27

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/September2021_2.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/September2021_2.py
@@ -5,8 +5,8 @@ position_vacancies = [85928, 50512, 37109, 29894, 48837, 22775, 27978]
 
 label = ["Taipei", "New Taipei", "Taoyuan", "Hsinchu", "Taichung", "Tainan", "Kaohsiung"]
 
-fig, ax = plt.subplots(figsize=(10, 9))  # [1]
-plt.xticks(region_num, labels=label, rotation=47)
+fig, ax = plt.subplots(figsize=(8, 7))
+plt.xticks(region_num, labels=label, rotation=7)
 # plt.yticks([0, 250, 300, 350, 400, 450, 500, 550])
 
 plot = ax.bar(region_num, position_vacancies)
@@ -21,6 +21,6 @@ plt.title("2021/09 the number of job openings in Taiwan by region")
 # plt.ylabel("")
 plt.xlabel("Reference: https://web.archive.org/web/20210927094609/https://www.104.com.tw/jb/category/?cat=2")
 
-# plt.ylim(280, 560)
+plt.ylim(10000, 90000)
 
 plt.show()
