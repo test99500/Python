f4963193928a5477d5976da758c201fdfb8a49c2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 17:31:56 +0800

    Committed 2021/09/27

--- Python for Excel/Chapter5 - Data Analysis with pandas/Applying a function.py
+++ Python for Excel/Chapter5 - Data Analysis with pandas/Applying a function.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
 rainfall = pd.DataFrame(data=[[300.1, 400.1, 1000.5],
-                              [100.2, 300.4, 1100.6]])
+                              [100.2, 300.4, 1100.6]],
+                        columns=["City 1", "City 2", "City 3"])
+
+print(rainfall)
+
+def format_string(x):
+    return f"{x:, .2f}"
+
+
+# Note that we pass in the function without calling it.
+rainfall.applymap(format_string)
+
+print(rainfall)
