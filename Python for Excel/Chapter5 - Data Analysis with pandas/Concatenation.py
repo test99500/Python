73773dfd907f3d5fd7bb86e01e79c3740d25fbdf
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 12:59:15 +0800

    Committed 2021/09/27

--- Python for Excel/Chapter5 - Data Analysis with pandas/Concatenation.py
+++ Python for Excel/Chapter5 - Data Analysis with pandas/Concatenation.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
+more_users = pd.DataFrame(data=[[15, "France", 4.1, "Becky"],
+                                [44, "Canada", 6.1, "Leanne"]],
+                          columns=['age', 'country', 'score', 'name'],
+                          index=[1000, 1011])
 
+print(more_users)
