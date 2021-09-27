bd93ffc6f0424a285e8c2ed9671030163c5c0713
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 19:20:44 +0800

    Committed 2021/09/27

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/September2021_2.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/September2021_2.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 
-region_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-position_vacancies = [85928, 50512, 37109, 29894, 5718, 48837, 9500,
-                      3477, 4956, 22775, 27978, 3462]
+region_num = [1, 2, 3, 4, 5, 6, 7]
+position_vacancies = [85928, 50512, 37109, 29894, 48837, 22775, 27978]
 
-label = ["Taipei", "New Taipei", "Taoyuan", "Hsinchu", "Miaoli", "Taichung", "Changhua",
-         "Yunlin", "Chiayi", "Tainan", "Kaohsiung", "Pingtung"]
+label = ["Taipei", "New Taipei", "Taoyuan", "Hsinchu", "Taichung", "Tainan", "Kaohsiung"]
 
 fig, ax = plt.subplots(figsize=(10, 9))  # [1]
 plt.xticks(region_num, labels=label, rotation=47)
