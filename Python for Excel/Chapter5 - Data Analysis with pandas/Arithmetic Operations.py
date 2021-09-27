bc82fc330bcfd0aee94880ae9decc48631006dbc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-27 12:26:17 +0800

    Committed 2021/09/27

--- Python for Excel/Chapter5 - Data Analysis with pandas/Arithmetic Operations.py
+++ Python for Excel/Chapter5 - Data Analysis with pandas/Arithmetic Operations.py
@@ -1,7 +1,20 @@
 import pandas as pd
 
-rainfall = pd.DataFrame(data=[[200.1, 400.3, 1000.5],
+rainfall = pd.DataFrame(data=[[300.1, 400.3, 1000.5],
                               [100.2, 300.4, 1100.6]],
                         columns=["City1", "City2", "City3"])
 
 print(rainfall)
+
+rainfall_plus = rainfall + 100
+
+print(rainfall_plus)
+
+more_rainfall = pd.DataFrame(data=[[100, 200],
+                                   [300, 400]], index=[1, 2], columns=["City 1", "City 4"])
+
+print(more_rainfall)
+
+more_rainfall_plus = rainfall + more_rainfall
+
+print(more_rainfall_plus)
