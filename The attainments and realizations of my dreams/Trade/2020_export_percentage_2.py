7aa9d0587b9471bd84f964175170da0c36793840
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-28 10:50:11 +0800

    Committed 2021/09/28

--- The attainments and realizations of my dreams/Trade/2020_export_percentage_2.py
+++ The attainments and realizations of my dreams/Trade/2020_export_percentage_2.py
@@ -16,7 +16,7 @@ print(percentile)
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'Keelung customs\n(Container ports in N.TW & E.TW)', 'Taipei customs\n(Airports in N.TW,\n ' \
                                                               'including Miaoli county )', \
-         'Taichung customs\n(Airport & container\n ports in C.TW,\n including Yunlin county)', \
+         'Taichung customs\n(Airport & container\n ports in C.TW,\n incl. Yunlin county)', \
          'Kaohsiung customs\n(Airport & container ports\n in S.TW)'
 sizes = [11.18, 48.36, 18.48, 21.96]
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
