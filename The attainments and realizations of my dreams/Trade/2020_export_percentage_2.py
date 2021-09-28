965272c9fd8e3eb366a043fa4b6ac732de79c1ab
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-28 10:43:12 +0800

    Committed 2021/09/28

--- The attainments and realizations of my dreams/Trade/2020_export_percentage_2.py
+++ The attainments and realizations of my dreams/Trade/2020_export_percentage_2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 exportation = pd.read_csv("2020_export_value_from_each_customs.csv", header=0, index_col=0)
 
@@ -11,3 +12,20 @@ percentile = [(1544101314 / 13800075242),
               100]
 
 print(percentile)
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'Keelung customs\n(Container ports in N.TW & E.TW)', 'Taipei customs\n(Airports in N.TW,\n ' \
+                                                              'including Miaoli county )', \
+         'Taichung customs\n(Airport & container\n ports in C.TW,\n including Yunlin county)', \
+         'Kaohsiung customs\n(Airport & container ports\n in S.TW)'
+sizes = [11.18, 48.36, 18.48, 21.96]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
+
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.title("The contribution of each region to total export in Taiwan 2020")
+
+plt.show()
