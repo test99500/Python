94daaa3ffe0bbbc65ae722676546bec8599a29ce
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-28 09:47:41 +0800

    Committed 2021/09/28

---
+++ The attainments and realizations of my dreams/Trade/2020_export_percentile.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+exportation = pd.read_csv("2020_export_value_from_each_customs.csv", header=0, index_col=0)
+
+print(exportation)
+
+percentile = pd.DataFrame(data=["Percentile", 1544101314 / 13800075242, 6674773304 / 13800075242,
+                                2550413664 / 13800075242, 3030786960 / 13800075242, 100],
+                          )
