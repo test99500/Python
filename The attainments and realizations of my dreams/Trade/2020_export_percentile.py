c3fa423225eb6808bd6ecebf62822a928a2e4451
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-28 10:08:25 +0800

    Committed 2021/09/28

--- The attainments and realizations of my dreams/Trade/2020_export_percentile.py
+++ The attainments and realizations of my dreams/Trade/2020_export_percentile.py
@@ -4,6 +4,8 @@ exportation = pd.read_csv("2020_export_value_from_each_customs.csv", header=0, i
 
 print(exportation)
 
-percentile = pd.DataFrame(data=["Percentile", 1544101314 / 13800075242, 6674773304 / 13800075242,
+percentile = pd.DataFrame(data=[1544101314 / 13800075242, 6674773304 / 13800075242,
                                 2550413664 / 13800075242, 3030786960 / 13800075242, 100],
-                          )
+                          columns=["基隆關", "臺北關", "臺中關", "高雄關", "合計"])
+
+print(percentile)
