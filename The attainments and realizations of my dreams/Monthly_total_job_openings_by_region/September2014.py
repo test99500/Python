93e64175a44d26c200f4d7bafcf3917c63faac56
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-28 09:02:53 +0800

    Committed 2021/09/28

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/September2014.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/September2014.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 region_num = [1, 2, 3, 4, 5, 6, 7]
-position_vacancies = [85928, 50512, 37109, 29894, 48837, 22775, 27978]
+position_vacancies = [66549, 34819, 21948, 17828, 27710, 12300, 16584]
 
 label = ["Taipei", "New Taipei", "Taoyuan", "Hsinchu", "Taichung", "Tainan", "Kaohsiung"]
 
@@ -16,11 +16,11 @@ for rect in plot:
     ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
             '%d' % int(height), ha='center', va='bottom')
 
-plt.title("2021/09 the number of job openings in Taiwan by region")
+plt.title("2014/09 the number of job openings in Taiwan by region")
 
 # plt.ylabel("")
-plt.xlabel("Reference: https://web.archive.org/web/20210927094609/https://www.104.com.tw/jb/category/?cat=2")
+plt.xlabel("Reference: https://web.archive.org/web/20140903150508/https://www.104.com.tw/jb/category/?cat=2")
 
-plt.ylim(10000, 90000)
+plt.ylim(10000, 70000)
 
 plt.show()
