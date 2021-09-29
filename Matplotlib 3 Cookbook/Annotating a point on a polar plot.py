18c85bcea0e2e05a4d9f4d391c08338aa4c14623
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-29 12:25:41 +0800

    Committed 2021/09/29

--- Matplotlib 3 Cookbook/Annotating a point on a polar plot.py
+++ Matplotlib 3 Cookbook/Annotating a point on a polar plot.py
@@ -2,10 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
+
+# Define the axes and declares it as a polar plot. (You can also use polar=True)
 ax = fig.add_subplot(111, projection='polar')
+
+# r and theta set the data for radius, and the angle for the plot of the polar coordinates.
 r = np.arange(0, 1, 0.001)
 theta = 2 * 2*np.pi * r
 
+# Plot the polar chart with theta and r.
 ax.plot(theta, r, color=[0.9, 0.4, 0.7], lw=3)
 
 # Plot a point with diamond marker at the 600 point on the polar curve.
