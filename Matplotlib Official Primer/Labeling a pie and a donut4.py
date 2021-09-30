43983a903eaf613f7812d4a45fcf224230a4d849
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-30 12:51:51 +0800

    Committed 2021/09/30

---
+++ Matplotlib Official Primer/Labeling a pie and a donut4.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+recipe = ["375 g flour",
+          "75 g sugar",
+          "250 g butter",
+          "300 g berries"]
+
+data = [float(x.split()[0]) for x in recipe]
+ingredients = [x.split()[-1] for x in recipe]
+
+
+def func(pct, allvals):
+    absolute = int(round(pct/100.*np.sum(allvals)))
+    return "{:.1f}%\n({:d} g)".format(pct, absolute)
+
+
+wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
+                                  textprops=dict(color="w"))
+
+#  We can use the legend's bbox_to_anchor argument
+#  to position the legend outside of the pie.
+#  Here we use the axes coordinates (1, 0, 0.5, 1) together with the location "center left";
+#  i.e. the left central point of the legend will be at the left central point of the bounding box,
+#  spanning from (1, 0) to (1.5, 1) in axes coordinates.
+ax.legend(wedges, ingredients,
+          title="Ingredients",
+          loc="center left",
+          bbox_to_anchor=(1, 0, 0.5, 1))
+
+plt.setp(autotexts, size=8, weight="bold")
+
+ax.set_title("Matplotlib bakery: A pie")
+
+plt.show()
+
+# Source:
+# https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html#sphx-glr-gallery-pie-and-polar-charts-pie-and-donut-labels-py
