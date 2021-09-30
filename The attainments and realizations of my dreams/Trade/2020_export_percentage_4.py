4383c1c2ad843fe06e318b5ebe6420dd963cc35f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-30 13:18:01 +0800

    Committed 2021/09/30

--- The attainments and realizations of my dreams/Trade/2020_export_percentage_4.py
+++ The attainments and realizations of my dreams/Trade/2020_export_percentage_4.py
@@ -23,7 +23,8 @@ sizes = [11.18, 48.36, 18.48, 21.96]
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90,
+        textprops=dict(color="w"))
 
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
