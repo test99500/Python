0b96e10e49850ec0c41da952626dff714e702072
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-02 16:28:41 +0800

    Committed 2021/10/02

--- Matplotlib Official Primer/bar_label()/Bar label label_type.py
+++ Matplotlib Official Primer/bar_label()/Bar label label_type.py
@@ -12,8 +12,7 @@ width = 0.35       # the width of the bars: can also be len(x) sequence
 fig, ax = plt.subplots()
 
 p1 = ax.bar(ind, menMeans, width, yerr=menStd, label='Men')
-p2 = ax.bar(ind, womenMeans, width,
-            bottom=menMeans, yerr=womenStd, label='Women')
+p2 = ax.bar(ind, womenMeans, width, bottom=menMeans, yerr=womenStd, label='Women')
 
 ax.axhline(0, color='grey', linewidth=0.8)
 ax.set_ylabel('Scores')
@@ -23,9 +22,11 @@ ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
 ax.legend()
 
 # Label with label_type 'center' instead of the default 'edge'
-ax.bar_label(p1, label_type='center')
-ax.bar_label(p2, label_type='center')
+ax.bar_label(p1, label_type='edge')
+ax.bar_label(p2, label_type='edge')
 ax.bar_label(p2)
+
+plt.savefig('bar label type of edge.jpg')
 
 plt.show()
 
