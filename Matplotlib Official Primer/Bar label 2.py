0e132a5bb54f99858186fb932c28682e9d7f85c5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-02 16:36:46 +0800

    Committed 2021/10/02

--- Matplotlib Official Primer/Bar label 2.py
+++ Matplotlib Official Primer/Bar label 2.py
@@ -12,8 +12,7 @@ width = 0.35       # the width of the bars: can also be len(x) sequence
 fig, ax = plt.subplots()
 
 p1 = ax.bar(ind, menMeans, width, yerr=menStd, label='Men')
-p2 = ax.bar(ind, womenMeans, width,
-            bottom=menMeans, yerr=womenStd, label='Women')
+p2 = ax.bar(ind, womenMeans, width, bottom=menMeans, yerr=womenStd, label='Women')
 
 ax.axhline(0, color='grey', linewidth=0.8)
 ax.set_ylabel('Scores')
