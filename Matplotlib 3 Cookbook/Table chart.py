14e0a0d0020c2d95fa813f27d41b9dcaa65aabe3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-02 16:16:32 +0800

    Committed 2021/10/02

--- Matplotlib 3 Cookbook/Table chart.py
+++ Matplotlib 3 Cookbook/Table chart.py
@@ -1,6 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# sold in each year
 rows = ['2011', '2012', '2013', '2014', '2015']
 
+# The types of batteries
 columns = ('7Ah', '35Ah', '40Ah', '135Ah', '150Ah', '150Ah')
+
+data = [[75, 144, 114, 102, 108],
+        [90, 126, 102, 84, 126],
+        [96, 114, 75, 105, 135],
+        [105, 90, 150, 90, 75],
+        [90, 75, 135, 75, 90]]
+
+# Define the range and scale for the y axis.
+values = np.arange(0, 600, 100)
+
+# Specify the color spectrum to be used.  Each year will be represented in a different color:
+colors = plt.cm.OrRd(np.linspace(0, 0.5, len(rows)))
+
