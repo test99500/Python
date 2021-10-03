2751811bc6bab4146843fbb299b22727b832f583
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-03 13:03:45 +0800

    Committed 2021/10/03

---
+++ Matplotlib 3 Cookbook/Table.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# sold in each year
+rows = ['2011', '2012', '2013', '2014', '2015']
+
+# The types of batteries
+columns = ('7Ah', '35Ah', '40Ah', '135Ah', '150Ah', '150Ah')
+
+data = [[75, 144, 114, 102, 108],
+        [90, 126, 102, 84, 126],
+        [96, 114, 75, 105, 135],
+        [105, 90, 150, 90, 75],
+        [90, 75, 135, 75, 90]]
+
+plt.table(cellText=data, rowLabels=rows, colLabels=columns, loc='bottom')
+plt.show()
