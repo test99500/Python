f03a9caaaaa4c938035abe13372f6d756c2988ca
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-09 16:03:28 +0800

    Committed 2021/10/09

---
+++ The attainments and realizations of my dreams/Road safety/Month_over_month_2021/July2021.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+administrative_duty = ["Taipei", "New Taipei", "Taoyuan", "Taichung", "Tainan", "Kaohsiung"]
+
+month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
