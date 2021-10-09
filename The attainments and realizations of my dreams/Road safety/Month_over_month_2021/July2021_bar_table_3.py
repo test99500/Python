4a4c60e5b41b37854bec791654926ee91b4734c1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-09 17:34:42 +0800

    Committed 2021/10/09

--- The attainments and realizations of my dreams/Road safety/Month_over_month_2021/July2021_bar_table_3.py
+++ The attainments and realizations of my dreams/Road safety/Month_over_month_2021/July2021_bar_table_3.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import matplotlib.cm as cm
+import matplotlib.patheffects as path_effects
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 administrative_duty = ["Taipei", "New Taipei", "Taoyuan", "Taichung", "Tainan", "Kaohsiung"]
 
@@ -9,13 +10,13 @@ month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "
 
 month_as_of_now = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"]
 
-deaths = [[14, 30, 18, 28, 31, 31], # Jan
-          [23, 55, 38, 46, 62, 70], # Feb
-          [36, 70, 56, 82, 83, 98], # Mar
-          [47, 85, 77, 106, 106, 123], # Apr
-          [57, 106, 99, 128, 147, 153], # May
-          [66, 123, 123, 151, 172, 177], # Jun
-          [73, 146, 145, 178, 190, 193], # Jul
+deaths = [[14, 30, 18, 28, 31, 31],  # Jan
+          [23, 55, 38, 46, 62, 70],  # Feb
+          [36, 70, 56, 82, 83, 98],  # Mar
+          [47, 85, 77, 106, 106, 123],  # Apr
+          [57, 106, 99, 128, 147, 153],  # May
+          [66, 123, 123, 151, 172, 177],  # Jun
+          [73, 146, 145, 178, 190, 193],  # Jul
           ]
 
 df = pd.DataFrame(data=deaths,
@@ -24,7 +25,7 @@ df = pd.DataFrame(data=deaths,
 print(df)
 
 # Get some pastel shades for the colors
-cmap = cm.get_cmap("Spectral") # [3]
+cmap = cm.get_cmap("Spectral")  # [3]
 colors = cmap(np.linspace(0, 0.5, len(month_as_of_now)))
 n_rows = len(deaths)
 
@@ -40,7 +41,6 @@ for row in range(n_rows):
     plt.bar(index, deaths[row], bar_width, bottom=y_offset, color=colors[row])
     y_offset = y_offset + deaths[row]
     cell_text.append([x for x in y_offset])
-
 
 # Reverse colors and text labels to display the last value at the top.
 colors = colors[::-1]
@@ -60,9 +60,19 @@ plt.ylabel("The number of deaths")
 plt.xticks([])
 plt.title("The Cumulative Number of Deaths in Road Accident\nin Taiwan by Region (2021/01-07)")
 
-plt.grid(True) # pyplot.grid [1][2]
+plt.grid(True)  # pyplot.grid [1][2]
 
 plt.tight_layout()
+
+# matplotlib text [1][2]
+text = plt.text(0.5, 0.04,
+                'Reference:https://roadsafety.tw/Dashboard/Custom?type=30日死亡人數',
+                horizontalalignment='center',
+                verticalalignment='center',
+                size=13,
+                fontproperties='MS Gothic')
+
+text.set_path_effects([path_effects.Normal()])
 
 plt.show()
 
