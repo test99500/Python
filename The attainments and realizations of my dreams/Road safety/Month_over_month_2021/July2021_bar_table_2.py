3817e2d6620c90f1a92d635aaf5eef5c7d5e18bb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-09 17:21:27 +0800

    Committed 2021/10/09

--- The attainments and realizations of my dreams/Road safety/Month_over_month_2021/July2021_bar_table_2.py
+++ The attainments and realizations of my dreams/Road safety/Month_over_month_2021/July2021_bar_table_2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib.cm as cm
 
 administrative_duty = ["Taipei", "New Taipei", "Taoyuan", "Taichung", "Tainan", "Kaohsiung"]
 
