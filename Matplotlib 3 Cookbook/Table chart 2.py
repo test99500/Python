f9a369ca407bdc4e634dea5303310ed40bfd0d51
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-10 15:52:58 +0800

    Committed 2021/10/10

--- Matplotlib 3 Cookbook/Table chart 2.py
+++ Matplotlib 3 Cookbook/Table chart 2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.cm as cm
 
 # table's row
 rows = ['2011', '2012', '2013', '2014', '2015']
@@ -7,6 +8,7 @@ rows = ['2011', '2012', '2013', '2014', '2015']
 # table's column
 columns = ('7Ah', '35Ah', '40Ah', '135Ah', '150Ah', '150Ah')
 
+# table's data
 data = [[75, 144, 114, 102, 108],
         [90, 126, 102, 84, 126],
         [96, 114, 75, 105, 135],
@@ -16,8 +18,9 @@ data = [[75, 144, 114, 102, 108],
 # Define the range and scale for the y axis.
 values = np.arange(0, 600, 100)
 
+cmap = cm.get_cmap("Spectral")
 # Specify the color spectrum to be used.  Each year will be represented in a different color:
-colors = plt.cm.OrRd(np.linspace(0, 0.5, len(rows)))
+colors = cmap(np.linspace(0, 0.5, len(rows)))
 
 # Define x axis ticks where the bars are to be plotted.
 index = np.arange(len(columns)) + 0.3
@@ -29,32 +32,36 @@ y_offset = np.zeros(len(columns))
 figure, axes = plt.subplots()
 
 # Plot bars and create text labels for the table.
-# Initialize the list in which data for the table is save.
+
+# Initialize the list where data for the table is save.
 cell_text = []
 
-# Each iteration of the for loop plots one year of data for all battery ratings in one color.
 n_rows = len(data)
 
+# Each iteration of the for loop plots one year of data (for all battery ratings in one color).
 for row in range(n_rows):
-    plot = plt.bar(index, data[row], 0.5, bottom=y_offset, color=colors[row])
+    plot = axes.bar(index, data[row], 0.5, bottom=y_offset, color=colors[row])
     y_offset = y_offset + data[row]
     cell_text.append(['%1.1f' % (x) for x in y_offset])
     i = 0
 
-    # Each iteration of this for loop, label each bar with corresponding value for the given year
+    # Each iteration of this for loop labels each bar with corresponding value for the given year
     for rectangle in plot:
         height = rectangle.get_height()
         axes.text(rectangle.get_x() + rectangle.get_width() / 2, y_offset[i], '%d' % int(y_offset[i]),
                   horizontalalignment='center', verticalalignment='bottom')
 
-        i = i + 1
+    i = i + 1
 
 # Add a table to the bottom of the axes
-table = plt.table(cellText=cell_text, rowLabels=rows, rowColours=colors, colLabels=columns,
-                  loc='bottom')
+table = axes.table(cellText=cell_text,
+                   rowLabels=rows,
+                   rowColours=colors,
+                   colLabels=columns,
+                   loc='bottom')
 
-plt.ylabel("Units Sold")
-plt.xticks([])
-plt.title("The Number Of Batteries Sold Per Year")
+axes.set_ylabel("Units Sold")
+axes.set_xticks([])
+axes.set_title("The Number Of Batteries Sold Per Year")
 
 plt.show()
