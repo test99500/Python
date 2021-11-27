86c69f54e76ac80d7332ad124126563cabb5249f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-27 15:16:16 +0800

    Committed 2021/11/27

--- 40 Algorithms Every Programmer Should Know/Graph Algorithms/Representations of graphs.py
+++ 40 Algorithms Every Programmer Should Know/Graph Algorithms/Representations of graphs.py
@@ -5,3 +5,20 @@ graph = nx.Graph()
 # Add a single vertex
 graph.add_node("Mike")
 
+# Add a bunch of vertices using a list
+graph.add_nodes_from(["Amine", "Wassim", "Nick"])
+
+# Add an edge between the existing vertices.
+graph.add_edge("Mike", "Amine")
+
+# Print the edges and vertices.
+print(graph.edges)
+print(graph.nodes)
+
+# If we are adding an edge, this also leads to adding the associated vertices,
+# if they do not already exist
+graph.add_edge("Amine", "Imran")
+
+print(graph.nodes)
+print(graph.edges)
+
