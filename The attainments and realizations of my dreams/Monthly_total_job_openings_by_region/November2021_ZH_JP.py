1cb6abf91ff92774fa75d5fb450ea24bae425df2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-29 22:02:43 +0800

    Committed 2021/11/29

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/November2021_ZH_JP.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/November2021_ZH_JP.py
@@ -24,7 +24,7 @@ for rect in plot:
 plt.title("2021/11 台灣各地區職缺數統計\nthe number of job openings in Taiwan by region", fontproperties="MS Gothic")
 
 # plt.ylabel("")
-plt.xlabel("Reference: https://web.archive.org/web/20211128163303/https://www.104.com.tw/jb/category/?cat=2")
+plt.xlabel("参照&出典: https://web.archive.org/web/20211128163303/https://www.104.com.tw/jb/category/?cat=2") # [2][3]
 
 plt.ylim(20000, 90000)
 
@@ -32,3 +32,5 @@ plt.show()
 
 # Reference:
 # 1. https://stackoverflow.com/questions/10960463/non-ascii-characters-in-matplotlib
+# 2. Google Translate
+# 3. https://ja.wikipedia.org/wiki/高雄市#出典
