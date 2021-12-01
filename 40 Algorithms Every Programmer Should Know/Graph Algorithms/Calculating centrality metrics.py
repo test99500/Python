a6063179b0992d239b81edaaf33726290aa9644a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-12-01 18:50:29 +0800

    Committed 2021/12/01

--- 40 Algorithms Every Programmer Should Know/Graph Algorithms/Calculating centrality metrics.py
+++ 40 Algorithms Every Programmer Should Know/Graph Algorithms/Calculating centrality metrics.py
@@ -3,4 +3,12 @@ import matplotlib.pyplot as plt
 
 vertices = range(1, 10)
 
+edges = [(7, 2), (2, 3), (7, 4), (4, 5), (7, 3), (7, 5), (1, 6), (1, 7), (2, 8), (2, 9)]
 
+graph = nx.Graph()
+graph.add_nodes_from(vertices)
+graph.add_edges_from(edges)
+
+nx.draw(graph, with_labels=True, node_color='y', node_size=800)
+
+plt.show()
