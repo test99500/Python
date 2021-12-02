199c9605d282a77eff810a218a0abdf9fd6acf59
Ceiling_roof <ted10014@hotmail.com>
2021-12-03 02:47:22 +0800

    Committed 2021/12/02

--- Chapter5 - Some More Python Essentials/Analytically.py
+++ Chapter5 - Some More Python Essentials/Analytically.py
@@ -7,4 +7,4 @@ f_expr = 5*x**3 + 2*x**2 - 1    # Symbolic expression for f(x)
 dfdx_expr = sym.diff(f_expr, x)   # Compute f'(x) symbolically
 
 # turn symbolic expressions into functions
-f = sym.lambdify([x])
+f = sym.lambdify([x], f_expr)  # f = lambda x: 5*x**3 + 2*x**2 - 1
