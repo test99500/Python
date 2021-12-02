34772fe01b524b803e29d551aee18fdf564451f3
Ceiling_roof <ted10014@hotmail.com>
2021-12-02 20:14:09 +0800

    Committed 2021/12/2

--- Chapter5 - Some More Python Essentials/example_symbolic.py
+++ Chapter5 - Some More Python Essentials/example_symbolic.py
@@ -10,4 +10,5 @@ print(2 * x + 3*x - y)               # Algebraic computation
 print(sym.diff(x**2, x))             # Differentiate x**2 wrt. x
 print(sym.integrate(sym.cos(x), x))  # Integrates cos(x) wrt. x
 print(sym.simplify((x ** 2 + x ** 3) / x ** 2))  # Simplifies expression
-
+print(sym.limit(sym.sin(x) / x, x, 0))  # lim of sin(x)/x as x -> 0
+print(sym.solve(5 * x - 15, x))         # Solve 5 * x = 15
