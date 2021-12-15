4442876eb1037d0c6ed22c78f9083b9243e50572
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-12-15 14:34:24 +0800

    Committed 2021/12/15

---
+++ NetworkX Altas/Bipartite.py
@@ -0,0 +1,15 @@
+from networkx.algorithms import bipartite
+import networkx as nx
+import matplotlib.pyplot as plt
+
+B = nx.Graph()
+
+# Add nodes with the node attribute "bipartite"
+B.add_nodes_from([1, 2, 3, 4], bipartite=0)
+B.add_nodes_from(["a", "b", "c"], bipartite=1)
+
+# Add edges only between nodes of opposite node sets
+B.add_edges_from([(1, "a"), (1, "b"), (2, "b"), (2, "c"), (3, "c"), (4, "a")])
+
+c = bipartite.color(G=B)
+print(c)
