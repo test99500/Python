ab00792d15259011ede2c856521d18dc5c487697
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-12-20 20:47:08 +0800

    Committed 2021/12/20

--- Hands on Machine Learning2/Chapter18 - Reinforcement Learning/CartPole_example.py
+++ Hands on Machine Learning2/Chapter18 - Reinforcement Learning/CartPole_example.py
@@ -1,4 +1,5 @@
 import gym
+import matplotlib.pyplot as plt
 
 environment = gym.make('CartPole-v1')
 
@@ -7,3 +8,26 @@ environment.seed(42)
 observation = environment.reset()
 
 print(observation)
+
+image = environment.render(mode="rgb_array")
+print(image.shape)
+
+def plot_environment(environment, figsize=(5, 4)):
+    plt.figure(figsize=figsize)
+    image = environment.render(mode="rgb_array")
+    plt.imshow(image)
+    plt.axis("off")
+
+    return image
+
+
+plot_environment(environment)
+
+plt.show()
+
+print(environment.action_space)
+
+action = 1 # accelerate right
+observation, reward, done, info = environment.step(action)
+
+print(observation, '\n', reward, '\n', done, '\n', info)
