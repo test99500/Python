d3568929fa98a117ef07197968f7310a9f7b65ac
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-12-26 13:50:25 +0800

    Committed 2021/12/26

--- NetworkX Altas/cut_size_example.py
+++ NetworkX Altas/cut_size_example.py
@@ -1,5 +1,10 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 graph = nx.barbell_graph(m1=3, m2=0)
 
+nx.draw_planar(G=graph, with_labels=True, node_color='g', node_size=800, font_size=14, width=0.8)
 
+plt.show()
+
+print(nx.algorithms.is_directed_acyclic_graph(graph))
