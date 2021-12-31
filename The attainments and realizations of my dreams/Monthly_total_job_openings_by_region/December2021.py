fb62190343fb6a8a3e6b147fdf803e2c429586d5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-12-31 16:36:03 +0800

    Committed 2021/12/31

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/December2021.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/December2021.py
@@ -16,7 +16,7 @@ for rect in plot:
     ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
             '%d' % int(height), ha='center', va='bottom')
 
-plt.title("2021/11 the number of job openings in Taiwan by region")
+plt.title("2021/12 the number of job openings in Taiwan by region")
 
 # plt.ylabel("")
 plt.xlabel("Reference: https://web.archive.org/web/20211231064231/https://www.104.com.tw/jb/category/?cat=2")
