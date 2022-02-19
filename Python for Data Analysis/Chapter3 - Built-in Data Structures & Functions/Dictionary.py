ffba55f46d77776107dc06991acb5bf22b8d489e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-02-19 15:51:52 +0800

    Committed 2022/02/19

--- Python for Data Analysis/Chapter3 - Built-in Data Structures & Functions/Dictionary.py
+++ Python for Data Analysis/Chapter3 - Built-in Data Structures & Functions/Dictionary.py
@@ -4,6 +4,8 @@ words = ['apple', 'bat', 'bar', 'atom', 'book']
 
 by_letter = defaultdict(list)
 
+print(by_letter)
+
 for word in words:
     by_letter[word[0]].append(word)
 
@@ -11,3 +13,6 @@ for word in words:
 print(by_letter)
 print('\n')
 print(words)
+
+print(by_letter[0])
+print(by_letter[1])
