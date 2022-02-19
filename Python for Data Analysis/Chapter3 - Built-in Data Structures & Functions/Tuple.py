13df74a6052a900472cc0c6eee32530d3460abb7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-02-19 15:03:02 +0800

    Committed 2022/02/19

--- Python for Data Analysis/Chapter3 - Built-in Data Structures & Functions/Tuple.py
+++ Python for Data Analysis/Chapter3 - Built-in Data Structures & Functions/Tuple.py
@@ -1,5 +1,10 @@
 immutable = tuple(['foo', [1, 2], True])
 print(immutable)
 
-immutable[1] = [1, 2, 3]
+# immutable[1] = [1, 2, 3]
+
+# immutable[1] = immutable[1].append(3)
+
+immutable[1].append(3)
+
 print(immutable)
