80fc0d77dab32978c83e6d783885c38d5d678d8e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-02-27 00:35:32 +0800

    Committed 2022/02/27

---
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/February2022_metro.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+region_num = [1, 2, 3, 4, 5]
+position_vacancies = [173152, 86229, 74105, 38453, 38373]
+
+label = ["Greater Taipei", "Taoyuan\nHsinchu\nMiaoli", "Taichung\nChanghua\nNantou",
+         "Yunlin\nChiayi\nTainan", "Kaohsiung\nPingtung"]
+
+fig, ax = plt.subplots(figsize=(9, 8))
+plt.xticks(region_num, labels=label, rotation=7)
+# plt.yticks([0, 250, 300, 350, 400, 450, 500, 550])
+
+plot = ax.bar(region_num, position_vacancies)
+
+for rect in plot:
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
+            '%d' % int(height), ha='center', va='bottom')
+
+plt.title("2022/02 the number of job openings in Taiwan by metro")
+
+# plt.ylabel("")
+plt.xlabel("Reference: https://web.archive.org/web/20220226163214/https://www.104.com.tw/jb/category/?cat=2")
+
+plt.ylim(30000, 150000)
+
+plt.show()
