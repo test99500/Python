947834a343d6a536967a4c573a8b1cc7aaed6060
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-03-27 15:10:12 +0800

    Committed 2022/03/27

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/March2022_metro.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/March2022_metro.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 region_num = [1, 2, 3, 4, 5]
-position_vacancies = [173152, 86229, 74105, 38453, 38373]
+position_vacancies = [178995, 88885, 76159, 39243, 39416]
 
 label = ["Greater Taipei", "Taoyuan\nHsinchu\nMiaoli", "Taichung\nChanghua\nNantou",
          "Yunlin\nChiayi\nTainan", "Kaohsiung\nPingtung"]
@@ -17,10 +17,10 @@ for rect in plot:
     ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
             '%d' % int(height), ha='center', va='bottom')
 
-plt.title("2022/02 the number of job openings in Taiwan by metro")
+plt.title("2022/03 the number of job openings in Taiwan by metro")
 
 # plt.ylabel("")
-plt.xlabel("Reference: https://web.archive.org/web/20220226163214/https://www.104.com.tw/jb/category/?cat=2")
+plt.xlabel("Reference: https://web.archive.org/web/20220327070711/https://www.104.com.tw/jb/category/?cat=2")
 
 plt.ylim(30000, 160000)
 
