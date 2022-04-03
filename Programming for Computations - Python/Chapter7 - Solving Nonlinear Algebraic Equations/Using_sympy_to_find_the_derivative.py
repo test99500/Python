5d4e5b07bbaeaed51826bf2fa06bcb1c1df96830
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-03 16:53:07 +0800

    Committed 2022/04/03

---
+++ Programming for Computations - Python/Chapter7 - Solving Nonlinear Algebraic Equations/Using_sympy_to_find_the_derivative.py
@@ -0,0 +1,11 @@
+import sympy as sym
+
+x = sym.symbols('x')
+f_expression = x**2 - 9   # symbolic expression for f(x)
+dfdx_expression = sym.diff(f_expression, x)  # compute f'(x) symbolically
+
+# turn f_expression and dfdx_expression into plan Python functions
+f = sym.lambdify([x], f_expression)
+dfdx = sym.lambdify([x], dfdx_expression)
+
+print(f(3), dfdx(3))
