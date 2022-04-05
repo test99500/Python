162d16d3dd28eef9f50ff5a73478fb09fe4244d6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-05 23:05:01 +0800

    Committed 2022/04/05

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/March2022_ZH.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/March2022_ZH.py
@@ -24,10 +24,10 @@ for rect in plot:
     ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
             '%d' % int(height), ha='center', va='bottom')
 
-plt.title("2021/11 台灣各地區職缺數統計\nthe number of job openings in Taiwan by region", fontproperties="MS Gothic")
+plt.title("2022/03 台灣各地區職缺數統計\nthe number of job openings in Taiwan by region", fontproperties="MS Gothic")
 
 # plt.ylabel("")
-plt.xlabel("Reference: https://web.archive.org/web/20211128163303/https://www.104.com.tw/jb/category/?cat=2")
+plt.xlabel("Reference: https://web.archive.org/web/20220130184455/https://www.104.com.tw/jb/category/?cat=2")
 
 plt.ylim(20000, 90000)
 
