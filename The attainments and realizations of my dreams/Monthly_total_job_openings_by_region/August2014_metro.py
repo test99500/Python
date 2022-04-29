ebb2e2b06442626c45b50605f7381ff5eb4d52db
Ceiling_roof <ted10014@hotmail.com>
2022-04-30 00:40:08 +0800

    Committed 2022/04/29

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/August2014_metro.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/August2014_metro.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 region_num = [1, 2, 3, 4, 5]
-position_vacancies = [103968, 42281, 28171+5630+1821, 1903+2806+12377, 16746+1861]
+position_vacancies = [102374, 41655, 27355+5579+1760, 1829+2691+11944, 16107+1852]
 
 label = ["Greater Taipei", "Taoyuan\nHsinchu\nMiaoli", "Taichung\nChanghua\nNantou",
          "Yunlin\nChiayi\nTainan", "Kaohsiung\nPingtung"]
@@ -17,10 +17,10 @@ for rect in plot:
     ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
             '%d' % int(height), ha='center', va='bottom')
 
-plt.title("2014/07 the number of job openings in Taiwan by metro")
+plt.title("2014/08 the number of job openings in Taiwan by metro")
 
 # plt.ylabel("")
-plt.xlabel("Reference: https://web.archive.org/web/20140702121714/http://www.104.com.tw:80/jb/category/?cat=2")
+plt.xlabel("Reference: https://web.archive.org/web/20140803070255/http://www.104.com.tw:80/jb/category/?cat=2")
 
 plt.ylim(10000, 100000)
 
