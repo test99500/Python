dd8c32cf1b6cda47ab5a11635daa8ee0819fc31d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-24 14:39:47 +0800

    Committed 2022/05/24

---
+++ Complex Network Analysis in Python/Chapter 11 - Unearthing the Network Structure/make-figures4.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+random.seed(1)
+import numpy as np
+np.random.seed(1)
+
+F = nx.DiGraph()
+F.add_node("C")
+
+F.add_edges_from([("B", "b0"), ("b0", "b1"), ("b1", "B")])
+F.add_edges_from([("A", "a0"), ("a0", "a1"), ("a1", "a2"), ("a1", "a3"), ("a3", "A")])
+
+nx.draw_networkx(G=F, arrows=True, with_labels=True)
+
+plt.show()
+
+print(list(nx.strongly_connected_components(G=F)))
