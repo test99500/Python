7fafc9a5f83c14e09346b23f0438430be3869e35
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-25 15:30:04 +0800

    Committed 2022/05/25

--- Complex Network Analysis in Python/Chapter 11 - Unearthing the Network Structure/Maximum_clique.py
+++ Complex Network Analysis in Python/Chapter 11 - Unearthing the Network Structure/Maximum_clique.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 # Generate a 5-clique
 G = nx.complete_graph(n=5, create_using=nx.Graph())
 
+nx.relabel_nodes(G=G, mapping=dict(enumerate(("Alpha", "Bravo", "Charlie", "Delta", "Echo"))), copy=False)
+
+# Attach a pigtail to it
+G.add_edges_from([("Echo", "Foxtrot"), ("Foxtrot", "Golf"), ("Foxtrot", "Hotel"), ("Golf", "Hotel")])
+
+nx.draw_networkx(G=G, arrows=True, with_labels=True)
+
+plt.show()
