5b67f557d3fd738ddbf0f337931adf80e9afd3d4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-25 11:00:51 +0800

    Committed 2022/05/25

--- Complex Network Analysis in Python/Chapter 11 - Unearthing the Network Structure/Separate_Cores, Shells, Coronas, & Crusts.py
+++ Complex Network Analysis in Python/Chapter 11 - Unearthing the Network Structure/Separate_Cores, Shells, Coronas, & Crusts.py
@@ -3,4 +3,11 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph((("Alpha", "Brazo"), ("Brazo", "Charlie"), ("Charlie", "Delta"), ("Charlie", "Echo"),
               ("Charlie", "Foxtrot"), ("Delta", "Echo"), ("Delta", "Foxtrot"), ("Echo", "Foxtrot"),
-              ()))
+              ("Echo", "Golf"), ("Echo", "Hotel"), ("Foxtrot", "Golf"), ("Foxtrot", "Hotel"),
+              ("Delta", "Hotel"), ("Golf", "Hotel"), ("Delta", "India"), ("Charlie", "India"),
+              ("India", "Juliet"), ("Golf", "Kilo"), ("Alpha", "Kilo"), ("Bravo", "Lima")))
+
+nx.draw_networkx(G=G, arrows=True, with_labels=True)
+
+plt.show()
+
