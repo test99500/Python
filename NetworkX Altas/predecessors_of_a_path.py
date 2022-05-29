0ac60a6701dd8bf5b2f4e9f72a33d67aad82a16b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-29 13:26:33 +0800

    Committed 2022/05/29

--- NetworkX Altas/predecessors_of_a_path.py
+++ NetworkX Altas/predecessors_of_a_path.py
@@ -8,3 +8,7 @@ nx.draw(G=G, with_labels=True, node_color='y', node_size=800)
 plt.show()
 
 print(nx.predecessor(G=G, source=0, target=3))
+print(nx.predecessor(G=G, source=0, target=4))
+
+# Reference:
+# 1. https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.shortest_paths.unweighted.predecessor.html
