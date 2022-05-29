d43d14e67fffa829a407b856d5aa04b2a8632436
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-29 13:24:22 +0800

    Committed 2022/05/29

--- NetworkX Altas/predecessors_of_a_path.py
+++ NetworkX Altas/predecessors_of_a_path.py
@@ -3,4 +3,8 @@ import matplotlib.pyplot as plt
 
 G = nx.path_graph(n=4)
 
-print()
+nx.draw(G=G, with_labels=True, node_color='y', node_size=800)
+
+plt.show()
+
+print(nx.predecessor(G=G, source=0, target=3))
