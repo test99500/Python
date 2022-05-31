108b0967963a621cfae3a5b5e5778239de222edb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-31 18:36:40 +0800

    Committed 2022/05/31

---
+++ NetworkX Altas/Independent set & bipartite graphs/is_matching().py
@@ -0,0 +1,12 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph(incoming_graph_data=[(1, 2), (1, 3), (2, 3), (2, 4), (3, 5), (4, 5)])
+
+nx.draw(G=G, with_labels=True, node_color='y', node_size=800)
+
+plt.show()
+
+print(nx.maximal_matching(G=G))
+
+print(nx.is_matching(G=G, matching={(2, 4), (3, 5)}))
