ba30da153986773904834cd7e99326dab4d2d2b7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-05-31 14:24:36 +0800

    Committed 2022/05/31

--- NetworkX Altas/Independent set & bipartite graphs/Independent set.py
+++ NetworkX Altas/Independent set & bipartite graphs/Independent set.py
@@ -10,5 +10,8 @@ plt.show()
 print(nx.maximal_independent_set(G=G))
 print(nx.maximal_independent_set(G=G))
 print(nx.maximal_independent_set(G=G))
-print(nx.maximal_independent_set(G=G))
-print(nx.maximal_independent_set(G=G))
+print(nx.maximal_independent_set(G=G, nodes=[1]))
+print(nx.maximal_independent_set(G=G, nodes=[4]))
+
+# Reference:
+# https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.mis.maximal_independent_set.html#networkx.algorithms.mis.maximal_independent_set
