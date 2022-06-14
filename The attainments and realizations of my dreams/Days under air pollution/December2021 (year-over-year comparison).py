0307f2777c2e9ccadeacb4e5a3c7a788be7a9bb1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-06-15 01:02:58 +0800

    Committed 2022/06/14

---
+++ The attainments and realizations of my dreams/Days under air pollution/December2021 (year-over-year comparison).py
@@ -0,0 +1,64 @@
+# Reference: https://airtw.epa.gov.tw/CHT/Query/Bad_Day.aspx
+# Source: https://matplotlib.org/stable/gallery/lines_bars_and_markers/hat_graph.html#sphx-glr-gallery-lines-bars-and-markers-hat-graph-py
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def hat_graph(ax, xlabels, values, group_labels):
+    """
+    Create a hat graph.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The Axes to plot into.
+    xlabels : list of str
+        The category names to be displayed on the x-axis.
+    values : (M, N) array-like
+        The data values.
+        Rows are the groups (len(group_labels) == M).
+        Columns are the categories (len(xlabels) == N).
+    group_labels : list of str
+        The group labels displayed in the legend.
+    """
+
+    def label_bars(heights, rects):
+        """Attach a text label on top of each bar."""
+        for height, rect in zip(heights, rects):
+            ax.annotate(f'{height}',
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 4),  # 4 points vertical offset.
+                        textcoords='offset points',
+                        ha='center', va='bottom')
+
+    values = np.asarray(values)
+    x = np.arange(values.shape[1])
+    ax.set_xticks(x, labels=xlabels)
+    spacing = 0.3  # spacing between hat groups
+    width = (1 - spacing) / values.shape[0]
+    heights0 = values[0]
+    for i, (heights, group_label) in enumerate(zip(values, group_labels)):
+        style = {'fill': False} if i == 0 else {'edgecolor': 'black'}
+        rects = ax.bar(x - spacing/2 + i * width, heights - heights0,
+                       width, bottom=heights0, label=group_label, **style)
+        label_bars(heights, rects)
+
+
+# initialise labels and a numpy array make sure you have
+# N labels of N number of values in the array
+xlabels = ['2018', '\'19', '\'20', '\'21', '\'22']
+Taipei = np.array([5, 15, 22, 20, 25])
+Kaohsiung = np.array([25, 32, 34, 30, 27])
+
+fig, ax = plt.subplots()
+hat_graph(ax, xlabels, [Taipei, Kaohsiung], ['Taipei', 'Kaohsiung'])
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_xlabel('Year\nReference: https://airtw.epa.gov.tw/CHT/Query/Bad_Day.aspx')
+ax.set_ylabel('Days polluted by the unsafe levels of AQI')
+ax.set_ylim(0, 60)
+ax.set_title('The number of Days with Air Quality Index > 100')
+ax.legend()
+
+fig.tight_layout()
+plt.show()
