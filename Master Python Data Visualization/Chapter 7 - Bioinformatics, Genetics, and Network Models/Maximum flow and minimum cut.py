cfc9d7fa9af01830af57e9faacac257fb249f525
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-07-16 15:23:04 +0800

    Committed 2022/07/16

--- Master Python Data Visualization/Chapter 7 - Bioinformatics, Genetics, and Network Models/Maximum flow and minimum cut.py
+++ Master Python Data Visualization/Chapter 7 - Bioinformatics, Genetics, and Network Models/Maximum flow and minimum cut.py
@@ -3,5 +3,15 @@ import matplotlib.pyplot as plt
 
 G = nx.DiGraph()
 G.add_edge('p', 'y', capacity=5.0)
+G.add_edge('p', 's', capacity=4.0)
+G.add_edge('y', 't', capacity=3.0)
+G.add_edge('s', 'h', capacity=5.0)
+G.add_edge('s', 'o', capacity=4.0)
 
+flow_value = nx.maximum_flow_value(flowG=G, _s='p', _t='o')
 
+print("Flow value", flow_value)
+
+nx.draw(G=G, node_color='#a0cbe2')
+
+plt.show()
