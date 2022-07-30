4aae1e34ee0d8bd0ea232718a1af1e9ee94b920b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-07-30 17:58:52 +0800

    Committed 2022/07/30

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2022/July/Metro_area_EN_ZH_JP.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2022/July/Metro_area_EN_ZH_JP.py
@@ -6,7 +6,7 @@ import matplotlib
 matplotlib.rc('font', family="MS Gothic")
 
 region_num = [1, 2, 3, 4, 5]
-position_vacancies = [180263, 89216, 74059, 39413, 40549]
+position_vacancies = [180263, 89334, 74194, 39046, 41424]
 
 label = ["大台北 Greater\nTaipei", "桃園 Taoyuan\n新竹 Hsinchu\n苗栗 Miaoli", "台中 Taichung\n彰化 Changhua\n南投 Nantou",
          "雲林 Yunlin\n嘉義 Chiayi\n台南 Tainan", "高雄 Kaohsiung\n屏東 Pingtung"]
