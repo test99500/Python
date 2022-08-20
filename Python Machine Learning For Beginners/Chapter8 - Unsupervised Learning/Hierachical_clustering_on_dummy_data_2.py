0d687420b79524715678db15bd68a78f517d604f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-08-20 18:42:40 +0800

    Committed 2022/08/20

---
+++ Python Machine Learning For Beginners/Chapter8 - Unsupervised Learning/Hierachical_clustering_on_dummy_data_2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets._samples_generator import make_blobs
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram
+
+# generating dummy data of 10 records with 2 clusters.
+features, labels = make_blobs(n_samples=10, centers=2, cluster_std=2.00)
+
+# plotting the dummy data
+plt.scatter(x=features[:, 0], y=features[:, 1], color='r')
+
+# adding numbers to data points
+annots = range(1, 11)
+
+for label, x, y in zip(annots, features[:, 0], features[:, 1]):
+    plt.annotate(text=label, xy=(x, y), xytext=(-3, 3), textcoords='offset points', ha='right', va='bottom')
+
+
+plt.show()
+
+# Pass features to the constructor of linkage class.
+dendros = linkage(y=features, method='single')
+
+dendrogram(Z=dendros, orientation='top', labels=annots, distance_sort='descending', show_leaf_counts=True)
+
+plt.show()
