85a154fd852ca9e7159288a922e42b8df3554903
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-08-20 16:20:38 +0800

    Committed 2022/08/20

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2022/August/Metro_areas_EN_ZH_JP_watermark.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2022/August/Metro_areas_EN_ZH_JP_watermark.py
@@ -14,7 +14,7 @@ label = ["大台北 Greater\nTaipei", "桃園 Taoyuan\n新竹 Hsinchu\n苗栗 Mi
          "雲林 Yunlin\n嘉義 Chiayi\n台南 Tainan", "高雄 Kaohsiung\n屏東 Pingtung"]
 
 fig, ax = plt.subplots(figsize=(9, 9))
-plt.xticks(region_num, labels=label, rotation=7, fontsize=12) # [1]
+plt.xticks(region_num, labels=label, rotation=7, fontsize=12)
 # plt.yticks([0, 250, 300, 350, 400, 450, 500, 550])
 plt.tick_params(axis='y', labelsize=12) # [2]
 
@@ -23,17 +23,21 @@ plot = ax.bar(region_num, position_vacancies)
 for rect in plot:
     height = rect.get_height()
     ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
-            '%d' % int(height), ha='center', va='bottom', fontsize=12) # [1]
+            '%d' % int(height), ha='center', va='bottom', fontsize=12)
 
-plt.title("2022/08 台灣各生活圈職缺數\n the number of job openings in Taiwan by metro", fontsize=20) # [1]
+plt.title("2022/08 台灣各生活圈職缺數\n the number of job openings in Taiwan by metro", fontsize=20)
 
 # plt.ylabel("")
 plt.xlabel("參考資料 Reference: https://web.archive.org/web/20220820070855/https://www.104.com.tw/jb/category/?cat=2")
 
 plt.ylim(38000, 165000)
 
+with cbook.get_sample_data('CC-BY.png') as file:
+    img = image.imread(file)
+
+
+plt.figimage(X=img, xo=20, yo=20, alpha=0.5)
+
 plt.show()
 
 # Reference:
-# 1. https://stackoverflow.com/a/12444777/
-# 2. https://stackoverflow.com/a/11386056/
