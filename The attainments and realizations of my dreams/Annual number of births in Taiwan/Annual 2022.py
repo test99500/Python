93403c99398f4f5a4d70ce7a2f404021fdd837b2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-08-26 19:42:38 +0800

    Committed 2022/08/26

--- The attainments and realizations of my dreams/Annual number of births in Taiwan/Annual 2022.py
+++ The attainments and realizations of my dreams/Annual number of births in Taiwan/Annual 2022.py
@@ -11,10 +11,14 @@ y_pos = np.arange(len(city))
 births = [8297, 12111, 10216, 9955, 5078, 9013]
 error = np.random.rand(len(city))
 
-ax.barh(y_pos, births, xerr=error, align='center')
+horizontal_bar = ax.barh(y_pos, births, xerr=error, align='center')
 ax.set_yticks(y_pos, labels=city)
 ax.invert_yaxis()  # labels read top-to-bottom
 ax.set_xlabel("# of births\nReference: https://statis.moi.gov.tw/micst/stmain.jsp?sys=100")
-ax.set_title('The number of births by city (2022-01-07) ')
+ax.set_title('The number of births by city (2022/01-07) ')
+
+# Label with given captions, custom padding and annotate options
+ax.bar_label(horizontal_bar, labels=['±%.2f' % e for e in error],
+             padding=8, color='b', fontsize=14)
 
 plt.show()
