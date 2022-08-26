bff586a031eab45bdd89f8aeacd89401d91e77b5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-08-26 19:29:26 +0800

    Committed 2022/08/26

--- The attainments and realizations of my dreams/Annual number of births in Taiwan/Annual 2022.py
+++ The attainments and realizations of my dreams/Annual number of births in Taiwan/Annual 2022.py
@@ -6,15 +6,15 @@ plt.rcdefaults()
 fig, ax = plt.subplots()
 
 # Example data
-city = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+city = ('Taipei', 'New Taipei', 'Taoyuan', 'Taichung', 'Tainan', 'Kaohsiung')
 y_pos = np.arange(len(city))
-performance = 3 + 10 * np.random.rand(len(city))
+births = [8297, 12111, 10216, 9955, 5078, 9013]
 error = np.random.rand(len(city))
 
-ax.barh(y_pos, performance, xerr=error, align='center')
+ax.barh(y_pos, births, xerr=error, align='center')
 ax.set_yticks(y_pos, labels=city)
 ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Performance')
-ax.set_title('How fast do you want to go today?')
+ax.set_xlabel("# of births\nReference: https://statis.moi.gov.tw/micst/stmain.jsp?sys=100")
+ax.set_title('The number of births by city (2022-01-07) ')
 
 plt.show()
