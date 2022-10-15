91194b733ecfa168abb76b19460f2de6b39d374a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-10-15 23:55:54 +0800

    Committed 2022/10/15

--- Supplementary materials/TensorFlow/Back_propagate.py
+++ Supplementary materials/TensorFlow/Back_propagate.py
@@ -6,4 +6,7 @@ np.random.seed(0)
 # Sample random numbers from a normal distribution with mean 1 and standard deviation of 0.1.
 x_values = np.random.normal(1, 0.1, 100).astype(np.float)
 
-print(x_values)
+print("x_values: ", x_values)
+
+y_values = (x_values * (np.random.normal(loc=1, scale=0.05, size=100) - 0.5)).astype(np.float)
+
