d8e05c2f0ae8941ff54295f48f23b6d7aaa01673
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-10-16 00:00:26 +0800

    Committed 2022/10/15

--- Supplementary materials/TensorFlow/Back_propagate.py
+++ Supplementary materials/TensorFlow/Back_propagate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
@@ -11,3 +12,8 @@ print("x_values: ", x_values)
 y_values = (x_values * (np.random.normal(loc=1, scale=0.05, size=100) - 0.5)).astype(np.float)
 
 print("y_values: ", y_values)
+
+# In order to get assurance that the target and input have a good correlation, plot a scatter plot
+# of the two variables:
+plt.scatter(x=x_values, y=y_values)
+plt.show()
