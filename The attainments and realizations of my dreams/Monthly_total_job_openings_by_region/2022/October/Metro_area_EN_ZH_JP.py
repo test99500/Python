92813700be327cfc7b419303d4ca62b5c7d67302
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-10-25 15:04:23 +0800

    Committed 2022/10/25

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2022/October/Metro_area_EN_ZH_JP.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2022/October/Metro_area_EN_ZH_JP.py
@@ -9,7 +9,7 @@ from matplotlib.transforms import IdentityTransform
 matplotlib.rc('font', family="MS Gothic")
 
 region_num = [1, 2, 3, 4, 5]
-position_vacancies = [1973+109860+62930+4205, 45938+33983+6435, 57369+3550+10081, 4346+6630+27509, 37149+4361]
+position_vacancies = [1973+109860+62930+4205, 45938+33983+6435, 57369+3550+10081, 4296+6530+27058, 36903+4374]
 
 label = ["大台北 Greater\nTaipei", "桃園 Taoyuan\n新竹 Hsinchu\n苗栗 Miaoli", "台中 Taichung\n彰化 Changhua\n南投 Nantou",
          "雲林 Yunlin\n嘉義 Chiayi\n台南 Tainan", "高雄 Kaohsiung\n屏東 Pingtung"]
