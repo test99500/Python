ef464191c22b7564cafbc7af0aaae0b0c8ddfad2
Ceiling_roof <ted10014@hotmail.com>
2022-11-07 20:01:06 +0800

    Committed 2022/11/07

--- NetworkX Altas/Independent set & bipartite graphs/Bipartite2.py
+++ NetworkX Altas/Independent set & bipartite graphs/Bipartite2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame(index=["Bob", "Carl", "Diane", "Eve", "Frank", "George"],
                   columns=["Bob", "Carl", "Diane", "Eve", "Frank", "George"],
@@ -14,5 +15,16 @@ df = pd.DataFrame(index=["Bob", "Carl", "Diane", "Eve", "Frank", "George"],
 
 print(df)
 
-
 Bipartite = nx.Graph()
+Bipartite.add_nodes_from(df.index, bipartite=0)
+Bipartite.add_nodes_from(df.columns, bipartite=1)
+
+s = df.stack()
+Bipartite.add_edges_from(s[s == 1].index)
+
+top = nx.bipartite.sets(G=Bipartite)[0]
+pos = nx.bipartite_layout(G=Bipartite, nodes=top)
+
+nx.draw(Bipartite, pos=pos, node_color='lightgreen', node_size=2500, with_labels=True)
+
+plt.show()
