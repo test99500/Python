b91070c5f953540b1f583e8d52932f59c89eaea9
Ceiling_roof <ted10014@hotmail.com>
2022-11-07 19:40:39 +0800

    Committed 2022/11/07

---
+++ NetworkX Altas/Independent set & bipartite graphs/Bipartite.py
@@ -0,0 +1,7 @@
+import networkx as nx
+
+Bipartite = nx.Graph()
+
+Bipartite.add_nodes_from(["Bob", "Carl", "Diane", "Eve", "Frank", "George"], bipartite=0)
+Bipartite.add_nodes_from(["George", "Frank", "Eve", "Diane", "Carl", "Bob"], bipartite=1)
+Bipartite.add_edges_from([("Bob", "George"), ("Bob", "Frank"), ("Bob", "Eve")])
