7b4c08b06503b1911c84c6de65057da6728f2820
Ceiling_roof <ted10014@hotmail.com>
2022-12-04 03:46:37 +0800

    Committed 2022/12/04

---
+++ The attainments and realizations of my dreams/Sewage System Coverage/2021.py
@@ -0,0 +1,1 @@
+# https://statis.moi.gov.tw/micst/stmain.jsp?sys=220&ym=9800&ymt=11000&kind=21&type=1&funid=c0830101&cycle=4&outmode=0&compmode=0&outkind=1&fld3=1&codspc0=0,2,3,2,6,1,9,1,12,1,&rdm=yuilWyVc
