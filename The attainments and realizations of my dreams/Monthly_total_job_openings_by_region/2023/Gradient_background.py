6959d4fc05acb3c4b82ccc9719561f5c099d60b5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-01-21 16:26:13 +0800

    Committed 2023/01/21

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2023/Gradient_background.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2023/Gradient_background.py
@@ -15,19 +15,19 @@ position_vacancies = [107229, 59148, 43509, 54374, 24843, 34872]
 
 label = ["台北市\nTaipei", "新北市\nNew Taipei", "桃園\nTaoyuan", "台中\nTaichung", "台南\nTainan", "高雄\n Kaohsiung"]
 
-fig, ax = plt.subplots(figsize=(9, 9), facecolor=(.18, .31, .31))
-ax.set_facecolor('#eafff5')
+fig, ax = plt.subplots(figsize=(9, 9), facecolor=(.18, .31, .31)) # [1]
+ax.set_facecolor('#eafff5')  # [1]
 
 plt.xticks(region_num, labels=label, rotation=7, fontsize=12)
 # plt.yticks([0, 250, 300, 350, 400, 450, 500, 550])
-plt.tick_params(axis='y', labelsize=12) # [2]
+plt.tick_params(axis='y', labelsize=12)
 
 plot = ax.bar(region_num, position_vacancies, edgecolor='cyan', color=[colour.CSS4_COLORS.get('deepskyblue'),
                                                                        colour.CSS4_COLORS.get('deepskyblue'),
                                                                        colour.CSS4_COLORS.get('deepskyblue'),
                                                                        colour.CSS4_COLORS.get('deepskyblue'),
                                                                        colour.CSS4_COLORS.get('palegreen'),
-                                                                       colour.CSS4_COLORS.get('palegreen')]) #[4][5][6]
+                                                                       colour.CSS4_COLORS.get('palegreen')])
 
 for rect in plot:
     height = rect.get_height()
@@ -48,23 +48,10 @@ plt.figimage(X=img, xo=800, yo=800, alpha=0.9)
 # Insert text watermark [1]
 plt.text(x=0.6, y=0.7, s="CC-BY 4.0", fontsize=40, color='grey', alpha=0.9,
          ha='center', va='center', rotation=30,
-         transform=ax.transAxes) # data coordinates [2] [Note1] [3] [Note2]
+         transform=ax.transAxes)
 
 
 plt.show()
 
 # Reference:
-# 1. https://matplotlib.org/stable/gallery/text_labels_and_annotations/watermark_text.html
-# 2. https://matplotlib.org/stable/tutorials/advanced/transforms_tutorial.html
-# 3. https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.text.html#matplotlib.axes.Axes.text
-# 4. https://www.google.com/search?q=matplotlib+bar+color
-# 5. https://www.python-graph-gallery.com/3-control-color-of-barplots
-# 6. https://matplotlib.org/stable/gallery/color/named_colors.html
-
-# Notes:
-# 1. pixel coordinate system of the display window; (0, 0) is bottom left of the window,
-# and (width, height) is top right of the display window in pixels.[2]
-#
-# 2. default transform specifies that text is in data coords,
-# alternatively, you can specify text in axis coords ((0, 0) is lower-left and (1, 1) is upper-right).
-# The example below places text in the center of the Axes:[3]
+# 1. https://matplotlib.org/stable/gallery/color/color_demo.html#sphx-glr-gallery-color-color-demo-py
