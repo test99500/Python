6ccce51b4b358e0a61cc7e3cb7f7167ef27abbbc
Ceiling_roof <ted10014@hotmail.com>
2023-01-28 01:11:26 +0800

    Committed 2023/01/28

---
+++ 40 Algorithms Every Programmer Should Know/Graph Algorithms/Breadth-first search.py
@@ -0,0 +1,9 @@
+
+
+# Adjacency list
+Graph = {"Amin": ["Wasim", "Nick", "Mike"],
+         "Wasim": ["Imran", "Amin"],
+         "Nick": "Amin",
+         "Mike": "Amin",
+         "Imran": ["Wasim", "Faras"]
+         }
