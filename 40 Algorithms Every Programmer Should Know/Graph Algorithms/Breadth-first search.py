82e09332861c51e4498008120ff3ae6ef0499318
Ceiling_roof <ted10014@hotmail.com>
2023-01-28 01:24:35 +0800

    Committed 2023/01/28

--- 40 Algorithms Every Programmer Should Know/Graph Algorithms/Breadth-first search.py
+++ 40 Algorithms Every Programmer Should Know/Graph Algorithms/Breadth-first search.py
@@ -9,6 +9,12 @@ Graph = {"Amin": ["Wasim", "Nick", "Mike"],
          "Faras": "Imran"
          }
 
+def bfs(graph, start):
+    visited = []  # This contains all the vertices that have been visited. Initially, it will be empty.
+    queue = [start]  # This contains all the vertices that we have want to visit in next iterations.
 
+
+    # Pop the first node from the queue and chose that node as the current node.
+    node = queue.pop(0)
 
 
