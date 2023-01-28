06163da68e2cd476090fd9af0ebd99955f19e2a1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-01-28 23:49:20 +0800

    Committed 2023/01/28

--- A Beginner's Guide to Python 3/35 ADTs, Queues & Stacks/35.4.1.py
+++ A Beginner's Guide to Python 3/35 ADTs, Queues & Stacks/35.4.1.py
@@ -11,3 +11,8 @@ element1 = queue.pop(0)
 print("element retrieved from queue: ", element1)
 
 print("queue after the pop: ", queue)
+
+element2 = queue.pop()
+print("element2 retrieved from queue: ", element2)
+
+print("queue after the pop: ", queue)
