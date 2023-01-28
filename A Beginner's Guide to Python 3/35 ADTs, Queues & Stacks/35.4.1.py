14f583fd29a3bc4965e744e21c66f30d203cee75
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-01-28 23:31:07 +0800

    Committed 2023/01/28

--- A Beginner's Guide to Python 3/35 ADTs, Queues & Stacks/35.4.1.py
+++ A Beginner's Guide to Python 3/35 ADTs, Queues & Stacks/35.4.1.py
@@ -2,3 +2,8 @@ queue = []  # Create an empty queue.
 queue.append('task1')
 
 print("Initial queue: ", queue)
+
+queue.append('task2')
+queue.append('task3')
+print("queue after additions: ", queue)
+
