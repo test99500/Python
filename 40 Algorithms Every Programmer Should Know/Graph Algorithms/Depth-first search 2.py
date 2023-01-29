f1435349c89545ec6f638a3847bfe772c9bad704
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-01-29 15:18:50 +0800

    Committed 2023/01/29

---
+++ 40 Algorithms Every Programmer Should Know/Graph Algorithms/Depth-first search 2.py
@@ -0,0 +1,24 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = []
+
+    visited.add(start)
+    print(start)
+
+    for next in graph[start] - visited:
+        dfs(graph=graph, start=next, visited=visited)
+
+    return visited
+
+
+# Adjacency list
+graph = {"Amin": ['Wasim', 'Nick', "Mike"],
+         "Wasim": ['Amin', 'Imran'],
+         "Nick": ['Amin'],
+         "Mike": ['Amin'],
+         'Imran': ['Wasim', 'Faras'],
+         'Faras': ['Imran']
+         }
+
+
+print(dfs(graph=graph, start='Amin', visited=None))
