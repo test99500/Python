4ae97ecb9ebda831fb4495fc25751c6aa6c190bc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-01-29 16:59:20 +0800

    Committed 2023/01/29

---
+++ 40 Algorithms Every Programmer Should Know/Graph Algorithms/Depth-first search 3.py
@@ -0,0 +1,35 @@
+# Acclimatizing the breadth-first search to the depth-first search
+# by replacing queue with stack
+def dfs(graph, start):
+    visited = []  # This contains all the vertices that have been visited. Initially, it will be empty.
+    visited.append(start)
+
+    queue = graph[start]  # This contains all the vertices that we have want to visit in next iterations.
+
+    # Check if there exists even a single element in the queue.
+    while queue:
+        # Pop the first node from the queue and chose that node as the current node of this iteration.
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            neighbours = graph[node]  # Use neighbors to represent the node's directly connected nodes.
+
+            # Now, add the node's neighbours one by one to the queue.
+            for neighbour in neighbours:
+                if neighbour not in visited:  # To prevent graph[start] from being added again.
+                    queue.append(neighbour)
+
+    return visited
+
+
+# Adjacency list
+graph = {"Amin": ['Wasim', 'Nick', "Mike"],
+         "Wasim": ['Amin', 'Imran'],
+         "Nick": ['Amin'],
+         "Mike": ['Amin'],
+         'Imran': ['Wasim', 'Faras'],
+         'Faras': ['Imran']
+         }
+
+
+print()
