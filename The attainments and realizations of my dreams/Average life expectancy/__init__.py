dd2f530ff82d93782ada8db83d6fe408f0f97fe6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-06-18 00:19:14 +0800

    Committed on 2023/06/18

--- The attainments and realizations of my dreams/Average life expectancy/__init__.py
+++ The attainments and realizations of my dreams/Average life expectancy/__init__.py
@@ -59,19 +59,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data = {'Barton LLC': 109438.50,
-        'Frami, Hills and Schmidt': 103569.59,
-        'Fritsch, Russel and Anderson': 112214.71,
-        'Jerde-Hilpert': 112591.43,
-        'Keeling LLC': 100934.30,
-        'Koepp Ltd': 103660.54,
-        'Kulas Inc': 137351.96,
-        'Trantow-Barrows': 123381.38,
-        'White-Trantow': 135841.99,
-        'Will LLC': 104437.60}
+data = {'Taipei': 84.14,
+        'Kaohsiung': 79.96,
+        }
+
 group_data = list(data.values())
 group_names = list(data.keys())
-group_mean = np.mean(group_data)
+group_mean = 80.86
 
 ###############################################################################
 # Getting started
@@ -168,8 +162,8 @@ fig, ax = plt.subplots()
 ax.barh(group_names, group_data)
 labels = ax.get_xticklabels()
 plt.setp(labels, rotation=45, horizontalalignment='right')
-ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
-       title='Company Revenue')
+ax.set(xlim=[-10, 100], xlabel='Average life expectancy', ylabel='City',
+       title='Average Life Expectancy in 2021')
 
 ###############################################################################
 # We can also adjust the size of this plot using the :func:`pyplot.subplots`
@@ -186,8 +180,8 @@ fig, ax = plt.subplots(figsize=(8, 4))
 ax.barh(group_names, group_data)
 labels = ax.get_xticklabels()
 plt.setp(labels, rotation=45, horizontalalignment='right')
-ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
-       title='Company Revenue')
+ax.set(xlim=[-10, 100], xlabel='Average life expectancy', ylabel='City',
+       title='Average Life Expectancy in 2021')
 
 ###############################################################################
 # For labels, we can specify custom formatting guidelines in the form of
@@ -219,8 +213,8 @@ ax.barh(group_names, group_data)
 labels = ax.get_xticklabels()
 plt.setp(labels, rotation=45, horizontalalignment='right')
 
-ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
-       title='Company Revenue')
+ax.set(xlim=[-10, 100], xlabel='Average life expectancy', ylabel='City',
+       title='Average Life Expectancy in 2021')
 ax.xaxis.set_major_formatter(currency)
 
 ###############################################################################
@@ -237,20 +231,21 @@ labels = ax.get_xticklabels()
 plt.setp(labels, rotation=45, horizontalalignment='right')
 
 # Add a vertical line, here we set the style in the function call
-ax.axvline(group_mean, ls='--', color='r')
+ax.axvline(x=group_mean, ls='--', color='r')
 
 # Annotate new companies
-for group in [3, 5, 8]:
-    ax.text(145000, group, "New Company", fontsize=10,
-            verticalalignment="center")
+# for group in [3, 5, 8]:
+#     ax.text(145000, group, "New Company", fontsize=10,
+#            verticalalignment="center")
 
 # Now we move our title up since it's getting a little cramped
 ax.title.set(y=1.05)
 
-ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
-       title='Company Revenue')
+ax.set(xlim=[-10, 100], xlabel='Average life expectancy', ylabel='City',
+       title='Average Life Expectancy in 2021')
+
 ax.xaxis.set_major_formatter(currency)
-ax.set_xticks([0, 25e3, 50e3, 75e3, 100e3, 125e3])
+ax.set_xticks([0, 20, 40, 60, 80, 100])
 fig.subplots_adjust(right=.1)
 
 plt.show()
