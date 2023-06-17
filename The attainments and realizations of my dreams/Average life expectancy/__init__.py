85fcd0b51ecde967d5945fe2667bcbc272b6f374
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-06-18 00:42:52 +0800

    Committed on 2023/06/18

--- The attainments and realizations of my dreams/Average life expectancy/__init__.py
+++ The attainments and realizations of my dreams/Average life expectancy/__init__.py
@@ -232,7 +232,9 @@ labels = ax.get_xticklabels()
 plt.setp(labels, rotation=45, horizontalalignment='right')
 
 # Add a vertical line, here we set the style in the function call
-ax.axvline(x=group_mean, ls='--', color='r', url="Average")
+ax.axvline(x=group_mean, ls='--', color='r')
+ax.text(group_mean, 0.5, "Nationwide average",
+             horizontalalignment='center', fontweight='bold', color='red')
 
 # Annotate new companies
 # for group in [3, 5, 8]:
