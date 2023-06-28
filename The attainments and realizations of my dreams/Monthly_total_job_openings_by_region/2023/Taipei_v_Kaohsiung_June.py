5db46adcde344450867b8d75d3355b07b944559c
Ceiling_roof <ted10014@hotmail.com>
2023-06-29 01:14:47 +0800

    Committed on 2023/06/29

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2023/Taipei_v_Kaohsiung_June.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2023/Taipei_v_Kaohsiung_June.py
@@ -39,7 +39,7 @@ plt.ylim(30000, 130000)
 
 img = image.imread('CC-BY.png')
 
-plt.figimage(X=img, xo=800, yo=800, alpha=0.9)
+plt.figimage(X=img, xo=600, yo=700, alpha=0.9)
 
 # Insert text watermark [1]
 plt.text(x=0.6, y=0.7, s="CC-BY 4.0", fontsize=40, color='grey', alpha=0.9,
