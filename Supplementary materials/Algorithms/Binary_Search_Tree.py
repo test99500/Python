b9d704c68a4ff501e63d3f9f9dd9fc163ddd7fe2
Ceiling_roof <ted10014@hotmail.com>
2023-08-15 13:17:48 +0800

    Committed on 2023/08/15

---
+++ Supplementary materials/Algorithms/Binary_Search_Tree.py
@@ -0,0 +1,8 @@
+class binaryTree:
+    def __init__(self, nodeData, left=None, right=None):
+        self.nodeData = nodeData
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.nodeData)
