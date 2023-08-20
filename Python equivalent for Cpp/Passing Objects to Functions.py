c1f6a210f5e611f7128c528550b9b80c0e3cb6dd
Ceiling_roof <ted10014@hotmail.com>
2023-08-20 13:56:08 +0800

    Committed on 2023/08/20

--- Python equivalent for Cpp/Passing Objects to Functions.py
+++ Python equivalent for Cpp/Passing Objects to Functions.py
@@ -9,3 +9,16 @@ class MyClass:
     def set_value(self, value):
         self.value = value
 
+    @staticmethod
+    def display(ob):
+        print(ob.get_value())
+        print("\n")
+    @staticmethod
+    def change(ob):
+        ob.set_value(100)
+        print("Value of ob inside: ")
+        MyClass.display(ob)
+
+
+
+
