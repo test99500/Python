77a03ea82e6d64c4cd7d6db205d401b2ee0f4dba
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-29 14:21:30 +0800

    Committed on or around 2023/10/29

--- Grokking Algorithms/CH5-Hash Tables/grocery clerk.py
+++ Grokking Algorithms/CH5-Hash Tables/grocery clerk.py
@@ -1,9 +1,12 @@
 # book is a new hash table
 book = dict()
 
+# Let's add some prices to book
+
 book["apple"] = 0.67   # An apple costs 67 cents.
 book["milk"] = 1.49    # Milk costs 1.49
 book["avocado"] = 1.49
 
 print(book)
+
 
